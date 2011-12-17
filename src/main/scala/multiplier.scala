92a40cd4cd14aa1690de7f0a8b66902282e85879
Andrew Waterman <waterman@eecs.berkeley.edu>
2011-12-17 07:20:00 -0800

    fix multiplier for rv32

--- src/main/scala/multiplier.scala
+++ src/main/scala/multiplier.scala
@@ -7,7 +7,8 @@ import Constants._;
 class ioMultiplier(width: Int) extends Bundle {
   // requests
   val mul_val = Bool('input);
-  val mul_fn  = UFix(3, 'input);
+  val dw      = UFix(1, 'input);
+  val mul_fn  = UFix(2, 'input);
   val mul_tag = UFix(5, 'input);
   val in0     = Bits(width, 'input);
   val in1     = Bits(width, 'input);
@@ -22,30 +23,38 @@ class rocketMultiplier extends Component {
   val io = new ioMultiplier(64);
   
   val r_val = Reg(resetVal = Bool(false));
+  val r_dw  = Reg(resetVal = UFix(0,1));
   val r_fn  = Reg(resetVal = UFix(0,3));
   val r_tag = Reg(resetVal = UFix(0,5));
-  val r_in0 = Reg(resetVal = Bits(0,64));
-  val r_in1 = Reg(resetVal = Bits(0,64));
+  val r_lhs = Reg(resetVal = Bits(0,65));
+  val r_rhs = Reg(resetVal = Bits(0,65));
+
+  val lhs_msb = Mux(io.dw === DW_64, io.in0(63), io.in0(31)).toBool
+  val lhs_sign = ((io.mul_fn === MUL_HS) || (io.mul_fn === MUL_HSU)) && lhs_msb
+  val lhs_hi = Mux(io.dw === DW_64, io.in0(63,32), Fill(32, lhs_sign))
+  val lhs = Cat(lhs_sign, lhs_hi, io.in0(31,0))
+
+  val rhs_msb = Mux(io.dw === DW_64, io.in1(63), io.in1(31)).toBool
+  val rhs_sign = (io.mul_fn === MUL_HS) && rhs_msb
+  val rhs_hi = Mux(io.dw === DW_64, io.in1(63,32), Fill(32, rhs_sign))
+  val rhs = Cat(rhs_sign, rhs_hi, io.in1(31,0))
   
   r_val <== io.mul_val;
   when (io.mul_val) {
+    r_dw  <== io.dw
     r_fn  <== io.mul_fn;
     r_tag <== io.mul_tag;
-    r_in0 <== io.in0;
-    r_in1 <== io.in1;
+    r_lhs <== lhs;
+    r_rhs <== rhs;
   }
   
-  val sxl64 = (r_fn === MUL_64H) || (r_fn === MUL_64HSU);
-  val sxr64 = (r_fn === MUL_64H);
+  val mul_result = r_lhs.toFix * r_rhs.toFix;
 
-  val lhs = Cat(r_in0(63) & sxl64, r_in0);
-  val rhs = Cat(r_in1(63) & sxr64, r_in1);
+  val mul_output64 = Mux(r_fn === MUL_LO, mul_result(63,0), mul_result(127,64))
+  val mul_output32 = Mux(r_fn === MUL_LO, mul_result(31,0), mul_result(63,31))
+  val mul_output32_ext = Cat(Fill(32, mul_output32(31)), mul_output32)
   
-  val mul_result = lhs.toFix * rhs.toFix;
-  
-  val mul_output = MuxCase(mul_result(63,0), Array(
-    ((r_fn === MUL_64H) || (r_fn === MUL_64HU) || (r_fn === MUL_64HSU)) -> mul_result(127,64),
-    (r_fn === MUL_32) -> Cat(Fill(32, mul_result(31)), mul_result(31, 0))));
+  val mul_output = Mux(r_dw === DW_64, mul_output64, mul_output32_ext)
   
   // just a hack for now, this should be a parameterized number of stages
   val r_result     = Reg(Reg(Reg(mul_output)));
