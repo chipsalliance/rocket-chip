3a2b305ddfd4b248764d3232fdc1636cbb45be73
Yunsup Lee <yunsup@cs.berkeley.edu>
2012-07-25 17:25:50 -0700

    change htif width to 16

--- rocket/src/main/scala/consts.scala
+++ rocket/src/main/scala/consts.scala
@@ -12,7 +12,7 @@ object Constants
 
   val MAX_THREADS = hwacha.Constants.NUM_PVFB * hwacha.Constants.WIDTH_PVFB / hwacha.Constants.SZ_BANK
 
-  val HTIF_WIDTH = 8
+  val HTIF_WIDTH = 16
   val MEM_BACKUP_WIDTH = HTIF_WIDTH
 
   val BR_X    = Bits("b????", 4)
