f7b1e23eadebdc956b68dbd6f5e8eedb04bb6241
Henry Cook <hcook@eecs.berkeley.edu>
2014-07-10 16:59:48 -0700

    functional style on MuxBundle

--- uncore/src/main/scala/coherence.scala
+++ uncore/src/main/scala/coherence.scala
@@ -5,11 +5,7 @@ import Chisel._
 
 object MuxBundle {
   def apply[T <: Data] (default: T, mapping: Seq[(Bool, T)]): T = {
-    var res = default;
-    for ((t, v) <- mapping.reverse){
-      res = Mux(t, v, res);
-    }
-    res
+    mapping.reverse.foldLeft(default)((b, a) => Mux(a._1, a._2, b))
   }
 }
 
