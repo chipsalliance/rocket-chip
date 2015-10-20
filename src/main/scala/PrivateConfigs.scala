afd3dab2f9c67faca17bc75b15086f01de2bdfbe
Yunsup Lee <yunsup@cs.berkeley.edu>
2015-10-19 20:43:43 -0700

    work-in-progress still observing lockups

--- src/main/scala/PrivateConfigs.scala
+++ src/main/scala/PrivateConfigs.scala
@@ -17,7 +17,8 @@ object HwachaTestSuites {
     "vld", "vlw", "vlwu", "vlh", "vlhu", "vlb", "vlbu", "vlxd", "vlxw", "vlxwu", "vlxh", "vlxhu", "vlxb", "vlxbu", 
     "vsd", "vsw", "vsh", "vsb", "vsxd", "vsxw", "vsxh", "vsxb",
     "eidx", "imul", "fcvt", "vvadd_d", "vvadd_w", "vvadd_fd", "vvadd_fw", "vvmul_d",
-    "overlap", "sched_sreg_xbar", "sched_fadd", "sched_waw", "sched_war", "pointer") ++
+    "overlap", "sched_sreg_xbar", "sched_fadd", "sched_waw", "sched_war", "pointer",
+    "vld_pred", "vsd_pred", "eidx_pred") ++
     rv32uaNames ++ rv64uaNames 
   val rv64uvBasic = new AssemblyTestSuite("rv64uv", "rv64uv", rv64uvNames)(_)
   
