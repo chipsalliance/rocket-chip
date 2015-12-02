cdc476a370dfbaf5aac7b50d9a4f55fd5c2d2692
Howard Mao <zhehao.mao@gmail.com>
2015-12-01 17:55:07 -0800

    change Rocc parameterization

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -133,9 +133,7 @@ class DefaultConfig extends Config (
         }
       }
       case BuildRoCC => Nil
-      case RoccAcceleratorMemChannels => site(BuildRoCC).map(_ => 1)
-      case RoccOpcodes => site(BuildRoCC).map(_ => OpcodeSet.all)
-      case RoccNMemChannels => site(RoccAcceleratorMemChannels).fold(0)(_ + _)
+      case RoccNMemChannels => site(BuildRoCC).map(_.nMemChannels).foldLeft(0)(_ + _)
       //Rocket Core Constants
       case FetchWidth => 1
       case RetireWidth => 1
@@ -389,14 +387,17 @@ class FancyMemtestConfig extends Config(
 class WithRoccExample extends Config(
   (pname, site, here) => pname match {
     case BuildRoCC => Seq(
-      (p: Parameters) => Module(new AccumulatorExample()(p)),
-      (p: Parameters) => Module(new TranslatorExample()(p)),
-      (p: Parameters) => Module(new CharacterCountExample()(p)))
+      RoccParameters(
+        opcodes = OpcodeSet.custom0,
+        generator = (p: Parameters) => Module(new AccumulatorExample()(p))),
+      RoccParameters(
+        opcodes = OpcodeSet.custom1,
+        generator = (p: Parameters) => Module(new TranslatorExample()(p))),
+      RoccParameters(
+        opcodes = OpcodeSet.custom2,
+        generator = (p: Parameters) => Module(new CharacterCountExample()(p))))
+
     case RoccMaxTaggedMemXacts => 1
-    case RoccOpcodes => Seq(
-      OpcodeSet.custom0,
-      OpcodeSet.custom1,
-      OpcodeSet.custom2)
   })
 
 class RoccExampleConfig extends Config(new WithRoccExample ++ new DefaultConfig)
