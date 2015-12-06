0c91e0067657a8b37373cf17a64639251ada1df5
Howard Mao <zhehao.mao@gmail.com>
2015-12-06 03:01:05 -0800

    move GroundTest configs to a separate file

--- src/main/scala/Configs.scala
+++ src/main/scala/Configs.scala
@@ -297,56 +297,6 @@ class WithZscale extends Config(
 
 class ZscaleConfig extends Config(new WithZscale ++ new DefaultConfig)
 
-class WithGroundTest extends Config(
-  (pname, site, here) => pname match {
-    case TLKey("L1toL2") =>
-      TileLinkParameters(
-        coherencePolicy = new MESICoherence(site(L2DirectoryRepresentation)),
-        nManagers = site(NBanksPerMemoryChannel)*site(NMemoryChannels),
-        nCachingClients = site(NTiles),
-        nCachelessClients = site(NTiles) + 1,
-        maxClientXacts = 1,
-        maxClientsPerPort = site(GroundTestMaxXacts),
-        maxManagerXacts = site(NAcquireTransactors) + 2,
-        dataBits = site(CacheBlockBytes)*8)
-    case BuildTiles => {
-      (0 until site(NTiles)).map { i =>
-        (r: Bool, p: Parameters) =>
-          Module(new GroundTestTile(i, r)
-            (p.alterPartial({case TLId => "L1toL2"})))
-      }
-    }
-    case GroundTestMaxXacts => 1
-  })
-
-class WithMemtest extends Config(
-  (pname, site, here) => pname match {
-    case NGenerators => site(NTiles)
-    case GenerateUncached => true
-    case GenerateCached => true
-    case MaxGenerateRequests => 128
-    case GeneratorStartAddress => 0
-    case BuildGroundTest =>
-      (id: Int, p: Parameters) => Module(new GeneratorTest(id)(p))
-  })
-
-class WithCacheFillTest extends Config(
-  (pname, site, here) => pname match {
-    case BuildGroundTest =>
-      (id: Int, p: Parameters) => Module(new CacheFillTest()(p))
-  },
-  knobValues = {
-    case "L2_WAYS" => 4
-    case "L2_CAPACITY_IN_KB" => 4
-  })
-
-class GroundTestConfig extends Config(new WithGroundTest ++ new DefaultConfig)
-class MemtestConfig extends Config(new WithMemtest ++ new GroundTestConfig)
-class MemtestL2Config extends Config(
-  new WithMemtest ++ new WithL2Cache ++ new GroundTestConfig)
-class CacheFillTestConfig extends Config(
-  new WithCacheFillTest ++ new WithL2Cache ++ new GroundTestConfig)
-
 class FPGAConfig extends Config (
   (pname,site,here) => pname match {
     case NAcquireTransactors => 4
@@ -388,10 +338,6 @@ class DualChannelDualBankConfig extends Config(
 class DualChannelDualBankL2Config extends Config(
   new With2MemoryChannels ++ new With2BanksPerMemChannel ++
   new WithL2Cache ++ new DefaultConfig)
-
-class FancyMemtestConfig extends Config(
-  new With2Cores ++ new With2MemoryChannels ++ new With2BanksPerMemChannel ++
-  new WithMemtest ++ new WithL2Cache ++ new GroundTestConfig)
 
 class WithRoccExample extends Config(
   (pname, site, here) => pname match {
