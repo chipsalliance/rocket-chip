0ff33a31a42f4d99aca67828a10f63d623d541b2
Wesley W. Terpstra <wesley@sifive.com>
2016-08-26 14:16:17 -0700

    tilelink2: add a stub SRAM manager

---
+++ uncore/src/main/scala/tilelink2/SRAM.scala
@@ -0,0 +1,24 @@
+// See LICENSE for license details.
+
+package uncore.tilelink2
+
+import Chisel._
+
+class TLRAM(address: AddressSet, beatBytes: Int = 4) extends TLFactory
+{
+  val node = TLManagerNode(beatBytes, TLManagerParameters(
+    address            = List(address),
+    regionType         = RegionType.UNCACHED,
+    supportsGet        = TransferSizes(1, beatBytes),
+    supportsPutPartial = TransferSizes(1, beatBytes),
+    supportsPutFull    = TransferSizes(1, beatBytes),
+    fifoId             = Some(0))) // requests are handled in order
+
+  lazy val module = Module(new TLModule(this) {
+    val io = new Bundle {
+      val in = node.bundleIn
+    }
+    
+    // do stuff
+  })
+}
