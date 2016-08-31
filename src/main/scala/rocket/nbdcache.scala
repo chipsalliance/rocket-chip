bb578494d8e70f9a8984f855a5b0e701b4b9623b
Howard Mao <zhehao.mao@gmail.com>
2016-08-31 09:44:16 -0700

    don't override req.bits.phys in SimpleHellaCacheIF

--- src/main/scala/rocket/nbdcache.scala
+++ src/main/scala/rocket/nbdcache.scala
@@ -1221,7 +1221,6 @@ class SimpleHellaCacheIF(implicit p: Parameters) extends Module
 
   io.cache.invalidate_lr := io.requestor.invalidate_lr
   io.cache.req <> req_arb.io.out
-  io.cache.req.bits.phys := Bool(true)
   io.cache.s1_kill := io.cache.s2_nack
   io.cache.s1_data := RegEnable(req_arb.io.out.bits.data, s0_req_fire)
 
