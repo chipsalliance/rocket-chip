ee3e31cb23dce19daf576eb575f9ad3aae2909b0
Wesley W. Terpstra <wesley@sifive.com>
2016-08-31 13:30:06 -0700

    tilelink2: refactor TLNodes into a seperate file

--- uncore/src/main/scala/tilelink2/Operations.scala
+++ uncore/src/main/scala/tilelink2/Operations.scala
@@ -431,26 +431,3 @@ class TLEdgeIn(
     d
   }
 }
-
-object TLImp extends NodeImp[TLClientPortParameters, TLManagerPortParameters, TLEdgeOut, TLEdgeIn, TLBundle]
-{
-  def edgeO(po: TLClientPortParameters, pi: TLManagerPortParameters): TLEdgeOut = new TLEdgeOut(po, pi)
-  def edgeI(po: TLClientPortParameters, pi: TLManagerPortParameters): TLEdgeIn  = new TLEdgeIn(po, pi)
-  def bundleO(eo: Seq[TLEdgeOut]): Vec[TLBundle] = {
-    require (!eo.isEmpty)
-    Vec(eo.size, TLBundle(eo.map(_.bundle).reduce(_.union(_))))
-  }
-  def bundleI(ei: Seq[TLEdgeIn]): Vec[TLBundle] = {
-    require (!ei.isEmpty)
-    Vec(ei.size, TLBundle(ei.map(_.bundle).reduce(_.union(_)))).flip
-  }
-
-  def connect(bo: TLBundle, eo: TLEdgeOut, bi: TLBundle, ei: TLEdgeIn)(implicit sourceInfo: SourceInfo): Unit = {
-    TLMonitor.legalize(bo, eo, bi, ei, sourceInfo)
-    bi <> bo
-    val mask = ~UInt(ei.manager.beatBytes - 1)
-    bi.a.bits.address := (mask & bo.a.bits.address)
-    bo.b.bits.address := (mask & bi.b.bits.address)
-    bi.c.bits.address := (mask & bo.c.bits.address)
-  }
-}
