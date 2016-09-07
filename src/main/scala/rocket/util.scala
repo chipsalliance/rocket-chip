511cc6c5c5fb086840922210f0e7ad5b9b944746
Andrew Waterman <waterman@cs.berkeley.edu>
2016-09-06 23:36:41 -0700

    Evaluate arg to Boolean.option lazily

--- src/main/scala/rocket/util.scala
+++ src/main/scala/rocket/util.scala
@@ -30,7 +30,7 @@ object Util {
     def toInt: Int = if (x) 1 else 0
 
     // this one's snagged from scalaz
-    def option[T](z: T): Option[T] = if (x) Some(z) else None
+    def option[T](z: => T): Option[T] = if (x) Some(z) else None
   }
 }
 
