56fe49e961038e5948641e79d73d1bf56f49e7d3
Howard Mao <zhehao.mao@gmail.com>
2016-09-21 21:44:54 -0700

    Allow coreplex to be on separate clock domain in ExampleMultiClockTop

--- src/main/scala/coreplex/Coreplex.scala
+++ src/main/scala/coreplex/Coreplex.scala
@@ -5,11 +5,18 @@ import cde.{Parameters, Field}
 import junctions._
 import uncore.tilelink._
 import uncore.util._
+import uncore.devices.{DebugBusIO, AsyncDebugBusFrom}
 import rocket._
 
 trait DirectConnection {
   val tiles: Seq[Tile]
   val uncoreTileIOs: Seq[TileIO]
+
+  val io: BaseCoreplexBundle
+  val uncoreExtMem: Vec[ClientUncachedTileLinkIO]
+  val uncoreExtMMIO: ClientUncachedTileLinkIO
+  val uncoreExtSlave: Vec[ClientUncachedTileLinkIO]
+  val uncoreExtDebug: DebugBusIO
 
   val tlBuffering = TileLinkDepths(1,1,2,2,0)
   val ultBuffering = UncachedTileLinkDepths(1,2)
@@ -24,6 +31,11 @@ trait DirectConnection {
     tile.io.hartid := uncore.hartid
     tile.io.resetVector := uncore.resetVector
   }
+
+  io.master.mem <> uncoreExtMem
+  io.master.mmio <> uncoreExtMMIO
+  uncoreExtSlave <> io.slave
+  uncoreExtDebug <> io.debug
 }
 
 class DefaultCoreplex(c: CoreplexConfig)(implicit p: Parameters) extends BaseCoreplex(c)(p) {
@@ -32,26 +44,36 @@ class DefaultCoreplex(c: CoreplexConfig)(implicit p: Parameters) extends BaseCor
 
 class DefaultCoreplexBundle(c: CoreplexConfig)(implicit p: Parameters) extends BaseCoreplexBundle(c)(p)
 
-class DefaultCoreplexModule[+L <: DefaultCoreplex, +B <: DefaultCoreplexBundle](
-    c: CoreplexConfig, l: L, b: => B)(implicit p: Parameters) extends BaseCoreplexModule(c, l, b)(p)
-    with DirectConnection
+class DefaultCoreplexModule[+L <: DefaultCoreplex, +B <: DefaultCoreplexBundle]
+  (c: CoreplexConfig, l: L, b: => B)(implicit p: Parameters)
+  extends BaseCoreplexModule(c, l, b)(p) with DirectConnection
 
 /////
 
-trait TileClockResetBundle {
+trait HasExtraClockResets {
   val c: CoreplexConfig
   val tcrs = Vec(c.nTiles, new Bundle {
     val clock = Clock(INPUT)
     val reset = Bool(INPUT)
   })
+  val extcr = new Bundle {
+    val clock = Clock(INPUT)
+    val reset = Bool(INPUT)
+  }
 }
 
 trait AsyncConnection {
-  val io: TileClockResetBundle
+  val io: BaseCoreplexBundle
+  val cio = io.asInstanceOf[HasExtraClockResets]
+
   val tiles: Seq[Tile]
   val uncoreTileIOs: Seq[TileIO]
+  val uncoreExtMem: Vec[ClientUncachedTileLinkIO]
+  val uncoreExtMMIO: ClientUncachedTileLinkIO
+  val uncoreExtSlave: Vec[ClientUncachedTileLinkIO]
+  val uncoreExtDebug: DebugBusIO
 
-  (tiles, uncoreTileIOs, io.tcrs).zipped foreach { case (tile, uncore, tcr) =>
+  (tiles, uncoreTileIOs, cio.tcrs).zipped foreach { case (tile, uncore, tcr) =>
     tile.clock := tcr.clock
     tile.reset := tcr.reset
 
@@ -61,24 +83,36 @@ trait AsyncConnection {
 
     val ti = tile.io.interrupts
     val ui = uncore.interrupts
-    ti.debug := LevelSyncTo(tcr.clock, ui.debug)
-    ti.mtip := LevelSyncTo(tcr.clock, ui.mtip)
-    ti.msip := LevelSyncTo(tcr.clock, ui.msip)
+    // These two come from outside the coreplex
+    ti.mtip := LevelSyncCrossing(cio.extcr.clock, tcr.clock, ui.mtip)
+    ti.msip := LevelSyncCrossing(cio.extcr.clock, tcr.clock, ui.msip)
+    // These come from inside the coreplex
     ti.meip := LevelSyncTo(tcr.clock, ui.meip)
+    ti.debug := LevelSyncTo(tcr.clock, ui.debug)
     ti.seip.foreach { _ := LevelSyncTo(tcr.clock, ui.seip.get) }
 
     tile.io.hartid := uncore.hartid
     tile.io.resetVector := uncore.resetVector
   }
+
+
+  io.master.mem.zip(uncoreExtMem).foreach { case (ext, uncore) =>
+    ext <> AsyncUTileLinkTo(cio.extcr.clock, cio.extcr.reset, uncore)
+  }
+  io.master.mmio <> AsyncUTileLinkTo(cio.extcr.clock, cio.extcr.reset, uncoreExtMMIO)
+  uncoreExtSlave.zip(io.slave).foreach { case (uncore, ext) =>
+    uncore <> AsyncUTileLinkFrom(cio.extcr.clock, cio.extcr.reset, ext)
+  }
+  uncoreExtDebug <> AsyncDebugBusFrom(cio.extcr.clock, cio.extcr.reset, io.debug)
 }
 
 class MultiClockCoreplex(c: CoreplexConfig)(implicit p: Parameters) extends BaseCoreplex(c)(p) {
   override lazy val module = Module(new MultiClockCoreplexModule(c, this, new MultiClockCoreplexBundle(c)(p))(p))
 }
 
-class MultiClockCoreplexBundle(c: CoreplexConfig)(implicit p: Parameters) extends BaseCoreplexBundle(c)(p)
-    with TileClockResetBundle
+class MultiClockCoreplexBundle(c: CoreplexConfig)(implicit p: Parameters)
+  extends BaseCoreplexBundle(c)(p) with HasExtraClockResets
 
-class MultiClockCoreplexModule[+L <: MultiClockCoreplex, +B <: MultiClockCoreplexBundle](
-    c: CoreplexConfig, l: L, b: => B)(implicit p: Parameters) extends BaseCoreplexModule(c, l, b)(p)
-    with AsyncConnection
+class MultiClockCoreplexModule[+L <: MultiClockCoreplex, +B <: MultiClockCoreplexBundle]
+  (c: CoreplexConfig, l: L, b: => B)(implicit p: Parameters)
+  extends BaseCoreplexModule(c, l, b)(p) with AsyncConnection
