270011b76809e59b8e73dfc5339c8d3b0c89f7cb
Henry Cook <henry@sifive.com>
2016-09-21 17:11:57 -0700

    [unittest] more Config cleanup

--- src/main/scala/unittest/Configs.scala
+++ src/main/scala/unittest/Configs.scala
@@ -2,8 +2,6 @@
 
 package unittest
 
-import scala.collection.mutable.LinkedHashSet
-
 import Chisel._
 import cde.{Parameters, Config, CDEMatchError}
 import coreplex._
@@ -11,7 +9,6 @@ import rocketchip._
 
 class WithJunctionsUnitTests extends Config(
   (pname, site, here) => pname match {
-    case RegressionTestNames => LinkedHashSet("rv64ui-p-simple")
     case UnitTests => (p: Parameters) => {
       TestGeneration.addSuite(DefaultTestSuites.groundtest64("p")) // TODO why
       TestGeneration.addSuite(DefaultTestSuites.emptyBmarks)
@@ -27,16 +24,17 @@ class WithUncoreUnitTests extends Config(
   (pname, site, here) => pname match {
     case NCoreplexExtClients => 0
     case uncore.tilelink.TLId => "L1toL2"
-    case RegressionTestNames => LinkedHashSet("rv64ui-p-simple")
     case UnitTests => (p: Parameters) => {
       TestGeneration.addSuite(DefaultTestSuites.groundtest64("p")) // TODO why
       TestGeneration.addSuite(DefaultTestSuites.emptyBmarks)
       Seq(
         Module(new uncore.devices.ROMSlaveTest()(p)),
         Module(new uncore.devices.TileLinkRAMTest()(p)),
-        Module(new uncore.tilelink2.TLFuzzRAMTest))
+        Module(new uncore.tilelink2.TLFuzzRAMTest)
+      )
     }
     case _ => throw new CDEMatchError
-  })
+  }
+)
 
 class UnitTestConfig extends Config(new WithUncoreUnitTests ++ new WithJunctionsUnitTests ++ new BaseConfig)
