cfdb8ca797c7d56fd6f151b6e8d3a10888a158d8
Wesley W. Terpstra <wesley@sifive.com>
2016-09-29 14:33:54 -0700

    tilelink2 LazyModule: remove obsolete connect method

--- src/main/scala/uncore/tilelink2/LazyModule.scala
+++ src/main/scala/uncore/tilelink2/LazyModule.scala
@@ -16,11 +16,6 @@ abstract class LazyModule
   LazyModule.stack = this :: LazyModule.stack
   parent.foreach(p => p.children = this :: p.children)
 
-  // Use as: connect(source -> sink, source2 -> sink2, ...)
-  def connect[PO, PI, EO, EI, B <: Data](edges: (BaseNode[PO, PI, EO, EI, B], BaseNode[PO, PI, EO, EI, B])*)(implicit sourceInfo: SourceInfo) = {
-    edges.foreach { case (source, sink) => sink := source }
-  }
-
   def name = getClass.getName.split('.').last
   def line = sourceLine(info)
 
