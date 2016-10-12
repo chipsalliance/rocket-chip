a6c6d9984845dfbf9c7ee6b31000452b20d5ab09
Wesley W. Terpstra <wesley@sifive.com>
2016-10-11 22:36:40 -0700

    axi4: prototype Fragmenter

--- src/main/scala/uncore/axi4/Parameters.scala
+++ src/main/scala/uncore/axi4/Parameters.scala
@@ -42,6 +42,10 @@ case class AXI4SlavePortParameters(
   // Check that the link can be implemented in AXI4
   require (maxTransfer <= beatBytes * (1 << AXI4Parameters.lenBits))
 
+  lazy val routingMask = AddressDecoder(slaves.map(_.address))
+  def findSafe(address: UInt) = Vec(slaves.map(_.address.map(_.contains(address)).reduce(_ || _)))
+  def findFast(address: UInt) = Vec(slaves.map(_.address.map(_.widen(~routingMask)).distinct.map(_.contains(address)).reduce(_ || _)))
+
   // Require disjoint ranges for addresses
   slaves.combinations(2).foreach { case Seq(x,y) =>
     x.address.foreach { a => y.address.foreach { b =>
