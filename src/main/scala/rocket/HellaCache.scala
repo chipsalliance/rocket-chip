32a1c274413f65147b9951105264852d4ea466a6
Wesley W. Terpstra <wesley@sifive.com>
2016-11-18 19:55:24 -0800

    rocket: disable nbdcache until it's fully ported

--- src/main/scala/rocket/HellaCache.scala
+++ src/main/scala/rocket/HellaCache.scala
@@ -161,7 +161,8 @@ class HellaCacheModule(outer: HellaCache)(implicit val p: Parameters) extends La
 
 object HellaCache {
   def apply(cfg: DCacheConfig)(implicit p: Parameters) = {
-    if (cfg.nMSHRs == 0) LazyModule(new DCache(cfg))
-    else LazyModule(new NonBlockingDCache(cfg))
+    //if (cfg.nMSHRs == 0) LazyModule(new DCache(cfg))
+    //else LazyModule(new NonBlockingDCache(cfg))
+    LazyModule(new DCache(cfg))
   }
 }
