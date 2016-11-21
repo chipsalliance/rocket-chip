95536aef7f94d091fc5bebb83808752bbebc26df
Andrew Waterman <andrew@sifive.com>
2016-11-20 16:35:27 -0800

    Per ABI, only x1 and x5 should be treated as function returns
    We were doing so for x3 and x7, as well, which could reduce performance
    for compilers that happen to perform indirect jumps via t2 (x7).

--- src/main/scala/rocket/rocket.scala
+++ src/main/scala/rocket/rocket.scala
@@ -602,7 +602,7 @@ class Rocket(implicit p: Parameters) extends CoreModule()(p) {
   io.imem.btb_update.valid := (mem_reg_replay && mem_reg_btb_hit) || (mem_reg_valid && !take_pc_wb && (((mem_cfi_taken || !mem_cfi) && mem_wrong_npc) || (Bool(fastJAL) && mem_ctrl.jal && !mem_reg_btb_hit)))
   io.imem.btb_update.bits.isValid := !mem_reg_replay && mem_cfi
   io.imem.btb_update.bits.isJump := mem_ctrl.jal || mem_ctrl.jalr
-  io.imem.btb_update.bits.isReturn := mem_ctrl.jalr && mem_reg_inst(19,15) === BitPat("b00??1")
+  io.imem.btb_update.bits.isReturn := mem_ctrl.jalr && mem_reg_inst(19,15) === BitPat("b00?01")
   io.imem.btb_update.bits.target := io.imem.req.bits.pc
   io.imem.btb_update.bits.br_pc := (if (usingCompressed) mem_reg_pc + Mux(mem_reg_rvc, UInt(0), UInt(2)) else mem_reg_pc)
   io.imem.btb_update.bits.pc := ~(~io.imem.btb_update.bits.br_pc | (coreInstBytes*fetchWidth-1))
