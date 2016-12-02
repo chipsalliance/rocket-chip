be23189f77a4c482066dedbbb0520a1b66953263
Jacob Chang <jchang@.sifive.com>
2016-12-01 18:35:43 -0800

    Removed val from case class for Parameters

--- src/main/scala/uncore/tilelink2/Parameters.scala
+++ src/main/scala/uncore/tilelink2/Parameters.scala
@@ -7,9 +7,9 @@ import diplomacy._
 import scala.math.max
 
 case class TLManagerParameters(
-  val address:            Seq[AddressSet],
+  address:            Seq[AddressSet],
   regionType:         RegionType.T  = RegionType.GET_EFFECTS,
-  val executable:         Boolean       = false, // processor can execute from this memory
+  executable:         Boolean       = false, // processor can execute from this memory
   nodePath:           Seq[BaseNode] = Seq(),
   // Supports both Acquire+Release+Finish of these sizes
   supportsAcquire:    TransferSizes = TransferSizes.none,
