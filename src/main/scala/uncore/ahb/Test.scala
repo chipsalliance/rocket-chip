7545c7d28018413491c6bdc99d06cad813ec0dc4
Wesley W. Terpstra <wesley@sifive.com>
2016-12-07 20:56:28 -0800

    ahb: include a unit test

---
+++ src/main/scala/uncore/ahb/Test.scala
@@ -0,0 +1,36 @@
+// See LICENSE.SiFive for license details.
+
+package uncore.ahb
+
+import Chisel._
+import config._
+import diplomacy._
+import uncore.tilelink2._
+import unittest._
+
+class RRTest0(address: BigInt)(implicit p: Parameters) extends AHBRegisterRouter(address, 0, 32, 0, 4)(
+  new AHBRegBundle((), _)    with RRTest0Bundle)(
+  new AHBRegModule((), _, _) with RRTest0Module)
+
+class RRTest1(address: BigInt)(implicit p: Parameters) extends AHBRegisterRouter(address, 0, 32, 1, 4, false)(
+  new AHBRegBundle((), _)    with RRTest1Bundle)(
+  new AHBRegModule((), _, _) with RRTest1Module)
+
+class AHBFuzzBridge()(implicit p: Parameters) extends LazyModule
+{
+  val fuzz  = LazyModule(new TLFuzzer(5000))
+  val model = LazyModule(new TLRAMModel("AHBFuzzMaster"))
+  val ram   = LazyModule(new AHBRAM(AddressSet(0x0, 0xff)))
+
+  model.node := fuzz.node
+  ram.node := TLToAHB()(model.node)
+
+  lazy val module = new LazyModuleImp(this) with HasUnitTestIO {
+    io.finished := fuzz.module.io.finished
+  }
+}
+
+class AHBBridgeTest()(implicit p: Parameters) extends UnitTest(500000) {
+  val dut = Module(LazyModule(new AHBFuzzBridge).module)
+  io.finished := dut.io.finished
+}
