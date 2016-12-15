16febe7e949ea8c89cab395c730644d5ec00ac5a
Wesley W. Terpstra <wesley@sifive.com>
2016-12-15 14:16:01 -0800

    apb: add a TileLink to APB bridge and unittest it

--- src/main/scala/uncore/apb/Test.scala
+++ src/main/scala/uncore/apb/Test.scala
@@ -25,7 +25,7 @@ class APBFuzzBridge()(implicit p: Parameters) extends LazyModule
   val gpio  = LazyModule(new RRTest0(0x100))
 
   model.node := fuzz.node
-//  xbar.node := TLToAPB()(model.node)
+  xbar.node := TLToAPB()(model.node)
   ram.node  := xbar.node
   gpio.node := xbar.node
 
