618468a06b0635f1fa20e5c63dbc4fc11ac9ac7e
Andrew Waterman <aswaterman@gmail.com>
2017-05-24 21:38:56 -0700

    Make plusarg_reader default args work with VCS (#765)
    Resolves #764

--- vsrc/plusarg_reader.v
+++ vsrc/plusarg_reader.v
@@ -2,7 +2,7 @@
 
 // No default parameter values are intended, nor does IEEE 1800-2012 require them (clause A.2.4 param_assignment),
 // but Incisive demands them. These default values should never be used.
-module plusarg_reader #(string FORMAT="borked", int DEFAULT=0) (
+module plusarg_reader #(string FORMAT="borked=%d", int DEFAULT=0) (
    output [31:0] out
 );
 
