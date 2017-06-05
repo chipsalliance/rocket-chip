7afd5e60704daaa13402a5f2eb53e493bc0a0a3b
Megan Wachs <megan@sifive.com>
2017-06-05 16:18:57 -0700

    remove unnecessary whitespace. Fix grammar.

--- src/main/scala/util/PlusArg.scala
+++ src/main/scala/util/PlusArg.scala
@@ -9,7 +9,6 @@ class plusarg_reader(val format: String, val default: Int, val docstring: String
   val io = new Bundle {
     val out = UInt(OUTPUT, width = 32)
   }
-
 }
 
 object PlusArg
@@ -17,7 +16,7 @@ object PlusArg
   // PlusArg("foo") will return 42.U if the simulation is run with +foo=42
   // Do not use this as an initial register value. The value is set in an
   // initial block and thus accessing it from another initial is racey.
-  // Add a docstring to document the arg, which can be dumped in elaboration
+  // Add a docstring to document the arg, which can be dumped in an elaboration
   // pass.
 
   def apply(name: String, default: Int = 0, docstring: String = ""): UInt =
