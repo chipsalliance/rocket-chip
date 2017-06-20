f6e0dd12c8add3104c424611151a1e647ab38c07
Wesley W. Terpstra <wesley@sifive.com>
2017-06-20 13:58:30 -0700

    TLSplitter: ManagerUnification is not used in Xbars
    Track the change made in 5994714970c7c0ee5687e6c4c6701b5ad4372bd7

--- src/main/scala/uncore/tilelink2/Splitter.scala
+++ src/main/scala/uncore/tilelink2/Splitter.scala
@@ -21,14 +21,14 @@ class TLSplitter(policy: TLArbiter.Policy = TLArbiter.lowestIndexFirst)(implicit
         seq(0).copy(
           minLatency = seq.map(_.minLatency).min,
           endSinkId = outputIdRanges.map(_.map(_.end).getOrElse(0)).max,
-          managers = ManagerUnification(seq.zipWithIndex.flatMap { case (port, i) =>
+          managers = seq.zipWithIndex.flatMap { case (port, i) =>
             require (port.beatBytes == seq(0).beatBytes,
               s"Splitter data widths don't match: ${port.managers.map(_.name)} has ${port.beatBytes}B vs ${seq(0).managers.map(_.name)} has ${seq(0).beatBytes}B")
             val fifoIdMapper = fifoIdFactory()
             port.managers map { manager => manager.copy(
               fifoId = manager.fifoId.map(fifoIdMapper(_))
             )}
-          })
+          }
         )
       }
     })
