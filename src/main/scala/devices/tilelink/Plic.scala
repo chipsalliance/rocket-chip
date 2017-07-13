0800fd3ed99c1ca5fcde73905162df863911d73e
Megan Wachs <megan@sifive.com>
2017-07-13 13:18:50 -0700

    PLIC: Add maxPri as well as ndev in DTS

--- src/main/scala/devices/tilelink/Plic.scala
+++ src/main/scala/devices/tilelink/Plic.scala
@@ -68,6 +68,7 @@ class TLPLIC(params: PLICParams)(implicit p: Parameters) extends LazyModule
       val extra = Map(
         "interrupt-controller" -> Nil,
         "riscv,ndev" -> Seq(ResourceInt(nDevices)),
+        "riscv,maxpri" -> Seq(ResourceInt(maxPriorities)),        
         "#interrupt-cells" -> Seq(ResourceInt(1)))
       Description(name, mapping ++ extra)
     }
