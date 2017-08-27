e7a8f0fe0cc023b81bd3120855ef252b2d682a97
Henry Styles <hes@sifive.com>
2017-08-26 22:13:03 -0700

    4 TLBuffers from SBus to PBus

--- src/main/scala/coreplex/SystemBus.scala
+++ src/main/scala/coreplex/SystemBus.scala
@@ -12,7 +12,7 @@ case class SystemBusParams(
   beatBytes: Int,
   blockBytes: Int,
   masterBuffering: BufferParams = BufferParams.default,
-  slaveBuffering: BufferParams = BufferParams.flow, // TODO should be BufferParams.none on BCE
+  slaveBuffering: BufferParams = BufferParams.default, // TODO should be BufferParams.none on BCE
   splitSlavesBuffering : BufferParams = BufferParams.default
 ) extends TLBusParams
 
@@ -46,7 +46,15 @@ class SystemBus(params: SystemBusParams)(implicit p: Parameters) extends TLBusWr
 
   def toSplitSlaves: TLOutwardNode = outwardSplitNode
 
-  val toPeripheryBus: TLOutwardNode = outwardWWNode
+  private val pbusBuffer0 = LazyModule(new TLBuffer(BufferParams.default))
+  private val pbusBuffer1 = LazyModule(new TLBuffer(BufferParams.default))
+  private val pbusBuffer2 = LazyModule(new TLBuffer(BufferParams.default))
+  private val pbusBuffer3 = LazyModule(new TLBuffer(BufferParams.default))
+  pbusBuffer0.node :*= outwardWWNode
+  pbusBuffer1.node :*= pbusBuffer0.node
+  pbusBuffer2.node :*= pbusBuffer1.node
+  pbusBuffer3.node :*= pbusBuffer2.node
+  val toPeripheryBus: TLOutwardNode = pbusBuffer3.node
 
   val toMemoryBus: TLOutwardNode = outwardNode
 
