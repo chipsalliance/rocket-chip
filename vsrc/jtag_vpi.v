19eabb672897e4f07683bdc2966f4242ebdd84f9
Megan Wachs <megan@sifive.com>
2017-09-06 18:13:04 -0700

    jtag_vpi: add some hysterisis for waiting for init_done

--- vsrc/jtag_vpi.v
+++ vsrc/jtag_vpi.v
@@ -87,10 +87,10 @@ begin
 	data_out	<= 32'h0;
 	data_in		<= 32'h0;
 
-	// Insert a #delay here because we need to
-	// wait until the PC isn't pointing to flash anymore
-	// (this is around 20k ns if the flash_crash boot code
-	// is being booted from, else much bigger, around 10mil ns)
+        // Small delay to get past reset instability
+        //  before checking for init_done
+        #200 
+          
 	wait(init_done)
 		if($test$plusargs("jtag_vpi_enable")) main;
 end
