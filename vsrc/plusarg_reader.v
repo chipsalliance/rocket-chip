226c34c14d9b0470b2dcd1f0be927e27aea406aa
Bernd Beuster <bernd.beuster@gmail.com>
2017-09-25 17:18:49 +0200

    Fix omitted parameter

--- vsrc/plusarg_reader.v
+++ vsrc/plusarg_reader.v
@@ -2,7 +2,7 @@
 
 // No default parameter values are intended, nor does IEEE 1800-2012 require them (clause A.2.4 param_assignment),
 // but Incisive demands them. These default values should never be used.
-module plusarg_reader #(FORMAT="borked=%d", DEFAULT=0) (
+module plusarg_reader #(parameter FORMAT="borked=%d", DEFAULT=0) (
    output [31:0] out
 );
 
