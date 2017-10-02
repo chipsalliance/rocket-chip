9c9cb68462525fe124689484131a0be0c56f2d64
Megan Wachs <megan@sifive.com>
2017-10-02 11:07:55 -0700

    JTAG Coverage: Add reset coverage points

--- src/main/scala/jtag/JtagStateMachine.scala
+++ src/main/scala/jtag/JtagStateMachine.scala
@@ -147,6 +147,7 @@ class JtagStateMachine(implicit val p: Parameters) extends Module(override_reset
   JtagState.State.all.foreach { s => 
     cover (currState === s.U && io.tms === true.B,  s"${s.toString}_tms_1", "JTAG; ${s.toString} with TMS = 1; State Transition from ${s.toString} with TMS = 1")
     cover (currState === s.U && io.tms === false.B, s"${s.toString}_tms_0", "JTAG; ${s.toString} with TMS = 0; State Transition from ${s.toString} with TMS = 0")
+    cover (currState === s.U && jtag_reset === true.B, s"${s.toString}_reset", "JTAG; ${s.toString} with reset; JTAG Reset asserted during ${s.toString")
   }
 
 }
