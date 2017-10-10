57c2b18077f07f0905409c87ac6dd8ae153de9cd
Andrew Waterman <andrew@sifive.com>
2017-10-10 14:34:52 -0700

    Conditionalize some covers that are sometimes impossible

--- src/main/scala/rocket/PTW.scala
+++ src/main/scala/rocket/PTW.scala
@@ -279,8 +279,10 @@ class PTW(n: Int)(implicit edge: TLEdgeOut, p: Parameters) extends CoreModule()(
     count := pgLevels-1
   }
 
-  ccover(io.mem.s2_nack, "NACK", "D$ nacked page-table access")
-  ccover(io.mem.resp.valid && io.mem.s2_xcpt.ae.ld, "AE", "access exception while walking page table")
+  if (usingVM) {
+    ccover(io.mem.s2_nack, "NACK", "D$ nacked page-table access")
+    ccover(state === s_wait2 && io.mem.s2_xcpt.ae.ld, "AE", "access exception while walking page table")
+  }
 
   def ccover(cond: Bool, label: String, desc: String)(implicit sourceInfo: SourceInfo) =
     cover(cond, s"PTW_$label", "MemorySystem;;" + desc)
