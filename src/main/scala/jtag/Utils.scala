f7411d55b18f2d211b8f5cccbdeefa4d3eb7d82f
Megan Wachs <megan@sifive.com>
2017-10-26 17:48:21 -0700

    JTAG: use withClock instead of override clock

--- src/main/scala/jtag/Utils.scala
+++ src/main/scala/jtag/Utils.scala
@@ -13,32 +13,18 @@ class Tristate extends Bundle {
   val driven = Bool()  // active high, pin is hi-Z when driven is low
 }
 
-class NegativeEdgeLatch[T <: Data](dataType: T) extends Module {
-  class IoClass extends Bundle {
-    val next = Input(dataType)
-    val enable = Input(Bool())
-    val output = Output(dataType)
-  }
-  val io = IO(new IoClass)
-
-  val reg = Reg(dataType)
-  when (io.enable) {
-    reg := io.next
-  }
-  io.output := reg
-}
-
 /** Generates a register that updates on the falling edge of the input clock signal.
   */
 object NegativeEdgeLatch {
   def apply[T <: Data](clock: Clock, next: T, enable: Bool=true.B, name: Option[String] = None): T = {
-    // TODO better init passing once in-module multiclock support improves
-    withClock(clock) {
-     val latch_module = Module(new NegativeEdgeLatch((!clock.asUInt).asClock, next.cloneType))
-     name.foreach(latch_module.suggestName(_))
-     latch_module.io.next := next
-     latch_module.io.enable := enable
-     latch_module.io.output
+    // TODO pass in initial value as well
+    withClock((!clock.asUInt).asClock) {
+      val reg = Reg(dataType)
+      name.foreach{reg.suggestName(_)}
+      when (enable) {
+        reg := next
+      }
+      reg
     }
   }
 }
@@ -46,8 +32,7 @@ object NegativeEdgeLatch {
 /** A module that counts transitions on the input clock line, used as a basic sanity check and
   * debug indicator clock-crossing designs.
   */
-class ClockedCounter(modClock: Clock, counts: BigInt, init: Option[BigInt])
-    extends Module(override_clock=Some(modClock)) {
+class ClockedCounter(counts: BigInt, init: Option[BigInt]) {
   require(counts > 0, "really?")
 
   val width = log2Ceil(counts)
@@ -66,19 +51,23 @@ class ClockedCounter(modClock: Clock, counts: BigInt, init: Option[BigInt])
   } .otherwise {
     count := count + 1.U
   }
-
-  io.count := count
+ io.count := count
 }
 
 /** Count transitions on the input bit by specifying it as a clock to a counter.
   */
 object ClockedCounter {
   def apply (data: Bool, counts: BigInt, init: BigInt): UInt = {
-    val counter = Module(new ClockedCounter(data.asClock, counts, Some(init)))
-    counter.io.count
+    withClock(data.asClock) {
+      val counter = Module(new ClockedCounter(counts, Some(init)))
+      counter.io.count
+    }
   }
+
   def apply (data: Bool, counts: BigInt): UInt = {
-    val counter = Module(new ClockedCounter(data.asClock, counts, None))
-    counter.io.count
+    withClock(data.asClock) {
+      val counter = Module(new ClockedCounter(counts, None))
+      counter.io.count
+    }
   }
 }
