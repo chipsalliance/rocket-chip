a31ba2ea2e6d23def28a3da3a0cf47bd87e7a9d8
Henry Cook <henry@sifive.com>
2017-12-18 15:40:30 -0800

    diplomacy: LazyModule factory uses ValName (#1159)
    * diplomacy: LazyModule factory uses ValName

--- src/main/scala/diplomacy/LazyModule.scala
+++ src/main/scala/diplomacy/LazyModule.scala
@@ -141,7 +141,7 @@ object LazyModule
   protected[diplomacy] var scope: Option[LazyModule] = None
   private var index = 0
 
-  def apply[T <: LazyModule](bc: T)(implicit sourceInfo: SourceInfo): T = {
+  def apply[T <: LazyModule](bc: T)(implicit valName: ValName, sourceInfo: SourceInfo): T = {
     // Make sure the user put LazyModule around modules in the correct order
     // If this require fails, probably some grandchild was missing a LazyModule
     // ... or you applied LazyModule twice
