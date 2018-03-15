d58a575111c878b77fa95aa1463f4471c9a70a32
Henry Cook <henry@sifive.com>
2018-03-15 14:04:20 -0700

    annotations: first cut at two different mapping annotations

--- src/main/scala/util/Annotations.scala
+++ src/main/scala/util/Annotations.scala
@@ -11,6 +11,7 @@ import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods.{pretty, render}
 
 import freechips.rocketchip.diplomacy.AddressMapEntry
+import freechips.rocketchip.tilelink.TLToAXI4IdMapEntry
 
 case class ParamsAnnotation(target: Named, paramsClassName: String, params: Map[String,Any]) extends SingleTargetAnnotation[Named] {
   def duplicate(n: Named) = this.copy(n)
@@ -21,7 +22,12 @@ case class ParamsChiselAnnotation[T <: Product](target: InstanceId, params: T) e
   def toFirrtl = ParamsAnnotation(target.toNamed, params.getClass.getName, paramMap)
 }
 
+// TODO, figure out how to type parameterize these, such that they can be deserialized
 case class AddressMapAnnotation(target: Named, mapping: Seq[AddressMapEntry]) extends SingleTargetAnnotation[Named] {
+  def duplicate(n: Named) = this.copy(n)
+}
+
+case class TLToAXI4IdMapAnnotation(target: Named, mapping: Seq[TLToAXI4IdMapEntry]) extends SingleTargetAnnotation[Named] {
   def duplicate(n: Named) = this.copy(n)
 }
 
@@ -31,8 +37,13 @@ object annotated {
     params
   }
 
-  def addressMap(component: InstanceId, mapping: Seq[AddressMapEntry]): Seq[AddressMapEntry] = {
+  def addressMapping(component: InstanceId, mapping: Seq[AddressMapEntry]): Seq[AddressMapEntry] = {
     annotate(new ChiselAnnotation { def toFirrtl = AddressMapAnnotation(component.toNamed, mapping) })
+    mapping
+  }
+
+  def idMapping(component: InstanceId, mapping: Seq[TLToAXI4IdMapEntry]): Seq[TLToAXI4IdMapEntry] = {
+    annotate(new ChiselAnnotation { def toFirrtl = TLToAXI4IdMapAnnotation(component.toNamed, mapping) })
     mapping
   }
 }
