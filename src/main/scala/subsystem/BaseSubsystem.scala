1da36f872c9ddd99469ea5620d9428fe27a8d8e7
Henry Cook <henry@sifive.com>
2018-03-15 14:04:20 -0700

    annotations: first cut at two different mapping annotations

--- src/main/scala/subsystem/BaseSubsystem.scala
+++ src/main/scala/subsystem/BaseSubsystem.scala
@@ -99,11 +99,11 @@ abstract class BaseSubsystem(implicit p: Parameters) extends BareSubsystem {
 
 
 abstract class BaseSubsystemModuleImp[+L <: BaseSubsystem](_outer: L) extends BareSubsystemModuleImp(_outer) {
-  private val mapping: Seq[AddressMapEntry] = {
+  private val mapping: Seq[AddressMapEntry] = annotated.addressMapping(this, {
     outer.collectResourceAddresses.groupBy(_._2).toList.flatMap { case (key, seq) =>
       AddressRange.fromSets(key.address).map { r => AddressMapEntry(r, key.permissions, seq.map(_._1)) }
     }.sortBy(_.range)
-  }
+  })
 
   println("Generated Address Map")
   mapping.map(entry => println(entry.toString((outer.sbus.busView.bundle.addressBits-1)/4 + 1)))
