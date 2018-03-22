e3b432a0f1a982dd27d658e24eb21a007a8685b7
Paul Rigge <rigge@berkeley.edu>
2018-03-21 17:22:53 -0700

    Add HasBlackBoxResource to some black boxes.

--- src/main/scala/devices/debug/Periphery.scala
+++ src/main/scala/devices/debug/Periphery.scala
@@ -4,6 +4,7 @@ package freechips.rocketchip.devices.debug
 
 import Chisel._
 import chisel3.core.{IntParam, Input, Output}
+import chisel3.util.HasBlackBoxResource
 import freechips.rocketchip.config.{Field, Parameters}
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.devices.tilelink._
@@ -87,7 +88,7 @@ trait HasPeripheryDebugModuleImp extends LazyModuleImp with HasPeripheryDebugBun
   outer.debug.module.io.ctrl.debugUnavail.foreach { _ := Bool(false) }
 }
 
-class SimDTM(implicit p: Parameters) extends BlackBox {
+class SimDTM(implicit p: Parameters) extends BlackBox with HasBlackBoxResource {
   val io = new Bundle {
     val clk = Clock(INPUT)
     val reset = Bool(INPUT)
@@ -108,9 +109,13 @@ class SimDTM(implicit p: Parameters) extends BlackBox {
       stop(1)
     }
   }
+
+  setResource("/vsrc/SimDTM.v")
+  setResource("/csrc/SimDTM.cc")
 }
 
-class SimJTAG(tickDelay: Int = 50) extends BlackBox(Map("TICK_DELAY" -> IntParam(tickDelay))) {
+class SimJTAG(tickDelay: Int = 50) extends BlackBox(Map("TICK_DELAY" -> IntParam(tickDelay)))
+  with HasBlackBoxResource {
   val io = new Bundle {
     val clock = Clock(INPUT)
     val reset = Bool(INPUT)
@@ -137,5 +142,10 @@ class SimJTAG(tickDelay: Int = 50) extends BlackBox(Map("TICK_DELAY" -> IntParam
       stop(1)
     }
   }
+
+  setResource("/vsrc/SimJTAG.v")
+  setResource("/csrc/SimJTAG.cc")
+  setResource("/csrc/remote_bitbang.h")
+  setResource("/csrc/remote_bitbang.cc")
 }
 
