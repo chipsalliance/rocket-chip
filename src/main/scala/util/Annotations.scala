ed8caa9268a63e3474571d3f0d3a67730553e711
Henry Cook <henry@sifive.com>
2018-04-16 14:20:19 -0700

    annotations: more comments

--- src/main/scala/util/Annotations.scala
+++ src/main/scala/util/Annotations.scala
@@ -13,6 +13,7 @@ import org.json4s.jackson.JsonMethods.{pretty, render}
 import freechips.rocketchip.diplomacy.AddressMapEntry
 import freechips.rocketchip.tilelink.TLToAXI4IdMapEntry
 
+/** Record a case class that was used to parameterize this target. */
 case class ParamsAnnotation(target: Named, paramsClassName: String, params: Map[String,Any]) extends SingleTargetAnnotation[Named] {
   def duplicate(n: Named) = this.copy(n)
 }
@@ -22,13 +23,19 @@ case class ParamsChiselAnnotation[T <: Product](target: InstanceId, params: T) e
   def toFirrtl = ParamsAnnotation(target.toNamed, params.getClass.getName, paramMap)
 }
 
-// TODO, figure out how to type parameterize these, such that they can be deserialized
+/** Record an address map. */
 case class AddressMapAnnotation(target: Named, mapping: Seq[AddressMapEntry]) extends SingleTargetAnnotation[Named] {
   def duplicate(n: Named) = this.copy(n)
 }
 
+/** Record a conversion of TL source ids to AXI4 ids. */
 case class TLToAXI4IdMapAnnotation(target: Named, mapping: Seq[TLToAXI4IdMapEntry]) extends SingleTargetAnnotation[Named] {
   def duplicate(n: Named) = this.copy(n)
+}
+
+/** Marks this module as a candidate for register retiming */
+case class RetimeModuleAnnotation(target: ModuleName) extends SingleTargetAnnotation[ModuleName] {
+  def duplicate(n: ModuleName) = this.copy(n)
 }
 
 object annotated {
@@ -48,9 +55,10 @@ object annotated {
   }
 }
 
-// TODO: replace this with an implicit class when @chisel unprotects dontTouchPorts
+/** Mix this into a Module class or instance to mark its ports as untouchable */
 trait DontTouch { self: RawModule =>
-
+  // TODO: replace this with an implicit class from UserModule that uses getPorts
+  // TODO: this is a workaround for firrtl #756
   def dontTouch(data: Data): Unit = data match {
      case agg: Aggregate => agg.getElements.foreach(dontTouch)
      case elt: Element => chisel3.core.dontTouch(elt)
@@ -70,11 +78,6 @@ trait DontTouch { self: RawModule =>
     self.getModulePorts.filterNot(f).foreach(dontTouch(_))
     self
   }
-}
-
-/** Marks this module as a candidate for register retiming */
-case class RetimeModuleAnnotation(target: ModuleName) extends SingleTargetAnnotation[ModuleName] {
-  def duplicate(n: ModuleName) = this.copy(n)
 }
 
 /** Mix this into a Module class or instance to mark it for register retiming */
