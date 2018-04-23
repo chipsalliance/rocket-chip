f82690dde37b326a7285a47e8ea1ec077eb28b4b
Derek Pappas <depappas@sifive.com>
2018-04-23 16:37:14 -0700

    moved a comment from RegisterRouter to GetMeMyModule

--- src/main/scala/tilelink/RegisterRouter.scala
+++ src/main/scala/tilelink/RegisterRouter.scala
@@ -103,26 +103,6 @@ case class TLRegisterNode(
 
     // TODO This comment serves as a reminder to change this code to use the new def in Chisel and remove the call to
     //  GetMeMyModule.
-
-    /*
-    * GetMeMyModule is a hack to get the current module during elaboration.
-    *
-    *
-    *  A pull request will be issued against Chisel3 to add this function to:
-    *
-    *  chiselFrontend/src/main/scala/chisel3/core/Module.scala:79
-    *
-    *  The pull request adds the following function:
-    *
-    *  /** Returns the current elaborating module */
-    *  def self: Option[BaseModule] = Builder.currentModule
-    *
-    *  When and if the function is added to Chisel3 GetMeMyModule should be deleted and
-    *  replaced with the Chisel:Module.self call
-    *
-    *  */
-
-
     val module = GetMeMyModule.currentModule.get.asInstanceOf[RawModule]
     GenRegDescsAnno.anno(
       module,
