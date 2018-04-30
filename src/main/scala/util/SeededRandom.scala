c0224a0ce9b4141d1c94157779151613ca24dbbc
Henry Cook <henry@sifive.com>
2018-04-30 15:16:06 -0700

    util: add explicitly reproducible Random

---
+++ src/main/scala/util/SeededRandom.scala
@@ -0,0 +1,12 @@
+// See LICENSE.SiFive for license details.
+
+package freechips.rocketchip.util
+
+/** Using e.g. Random.nextInt makes your hardware generation non-repoducible,
+  * which is almost certainly undesirable. Use LCG to increment random numbers in HW,
+  * or use this SeededRandom.fromSeed to make reproducible Scala PRNGs.
+  */
+object SeededRandom {
+  val fromCurrentTime = new scala.util.Random(System.currentTimeMillis())
+  val fromSeed = new scala.util.Random(42)
+}
