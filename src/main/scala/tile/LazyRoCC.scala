cdf3c01dd127d8e7499f77a97bfda40ea5128abc
Henry Cook <henry@sifive.com>
2018-05-07 22:23:09 -0700

    LazyRocc: scala style

--- src/main/scala/tile/LazyRoCC.scala
+++ src/main/scala/tile/LazyRoCC.scala
@@ -106,7 +106,7 @@ trait HasLazyRoCCModule[+L <: BaseTile with HasLazyRoCC] extends CanHavePTWModul
       val nFPUPorts = outer.roccs.filter(_.usesFPU).size
       if (usingFPU && nFPUPorts > 0) {
         val fpArb = Module(new InOrderArbiter(new FPInput()(outer.p), new FPResult()(outer.p), nFPUPorts))
-        val fp_rocc_ios = outer.roccs.filter { _.usesFPU } .map { _.module.io }
+        val fp_rocc_ios = outer.roccs.filter(_.usesFPU).map(_.module.io)
         fpArb.io.in_req <> fp_rocc_ios.map(_.fpu_req)
         fp_rocc_ios.zip(fpArb.io.in_resp).foreach {
           case (rocc, arb) => rocc.fpu_resp <> arb
