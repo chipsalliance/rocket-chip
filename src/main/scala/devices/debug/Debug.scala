3c1635c278d0e2e0f88c8556bb7962face01d4b6
Megan Wachs <megan@sifive.com>
2018-05-11 18:05:30 -0700

    debug: check for unexpected exceptions
    Harts should only be writing to the EXCEPTION location if doing work under the direction of the debugger.
    
    Debug: fix the location of the exception assertion
    
    Debug: fix the assertion comparison
    
    Debug: move assertion to right spot

--- src/main/scala/devices/debug/Debug.scala
+++ src/main/scala/devices/debug/Debug.scala
@@ -1037,9 +1037,10 @@ class TLDebugModuleInner(device: Device, getNComponents: () => Int, beatBytes: I
     }.otherwise {
       ctrlStateReg := ctrlStateNxt
     }
+    assert ((!hartExceptionWrEn || ctrlStateReg === CtrlState(Exec)),
+      "Unexpected EXCEPTION write: should only get it in Debug Module EXEC state")
   }
 }
-
 
 // Wrapper around TL Debug Module Inner and an Async DMI Sink interface.
 // Handles the synchronization of dmactive, which is used as a synchronous reset
