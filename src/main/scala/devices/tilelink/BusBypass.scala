31c7d3faa155e7e11e070cacf3c177cdf16c7839
Wesley W. Terpstra <wesley@sifive.com>
2018-05-12 14:23:02 -0700

    BusBypass: must indicate that it may 'deny' queries

--- src/main/scala/devices/tilelink/BusBypass.scala
+++ src/main/scala/devices/tilelink/BusBypass.scala
@@ -15,7 +15,13 @@ abstract class TLBusBypassBase(beatBytes: Int, deadlock: Boolean = false)(implic
   protected val nodeOut = TLIdentityNode()
   val node = NodeHandle(nodeIn, nodeOut)
 
-  protected val bar = LazyModule(new TLBusBypassBar)
+  protected val bar = LazyModule(new TLBusBypassBar(dFn = { mp =>
+    mp.copy(managers = mp.managers.map { m =>
+      m.copy(
+        mayDenyPut = m.mayDenyPut || !deadlock,
+        mayDenyGet = m.mayDenyGet || !deadlock)
+    })
+  }))
   protected val everything = Seq(AddressSet(0, BigInt("ffffffffffffffffffffffffffffffff", 16))) // 128-bit
   protected val params = ErrorParams(everything, maxAtomic=16, maxTransfer=4096)
   protected val error = if (deadlock) LazyModule(new DeadlockDevice(params, beatBytes))
@@ -37,21 +43,21 @@ class TLBusBypass(beatBytes: Int)(implicit p: Parameters) extends TLBusBypassBas
   }
 }
 
-class TLBypassNode(implicit valName: ValName) extends TLCustomNode
+class TLBypassNode(dFn: TLManagerPortParameters => TLManagerPortParameters)(implicit valName: ValName) extends TLCustomNode
 {
   def resolveStar(iKnown: Int, oKnown: Int, iStars: Int, oStars: Int): (Int, Int) = {
     require (iStars == 0 && oStars == 0, "TLBypass node does not support :=* or :*=")
     require (iKnown == 1, "TLBypass node expects exactly one input")
-    require (oKnown == 2, "TLBypass node expects exactly one output")
+    require (oKnown == 2, "TLBypass node expects exactly two outputs")
     (0, 0)
   }
   def mapParamsD(n: Int, p: Seq[TLClientPortParameters]): Seq[TLClientPortParameters] = { p ++ p }
-  def mapParamsU(n: Int, p: Seq[TLManagerPortParameters]): Seq[TLManagerPortParameters] = { p.tail }
+  def mapParamsU(n: Int, p: Seq[TLManagerPortParameters]): Seq[TLManagerPortParameters] = { Seq(dFn(p.last)) }
 }
 
-class TLBusBypassBar(implicit p: Parameters) extends LazyModule
+class TLBusBypassBar(dFn: TLManagerPortParameters => TLManagerPortParameters)(implicit p: Parameters) extends LazyModule
 {
-  val node = new TLBypassNode()
+  val node = new TLBypassNode(dFn)
 
   lazy val module = new LazyModuleImp(this) {
     val io = IO(new Bundle {
