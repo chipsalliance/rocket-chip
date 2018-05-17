aba681f3bd0d6bdf3e3b989c2e98ca000d1594e3
Wesley W. Terpstra <wesley@sifive.com>
2018-05-17 11:25:18 -0700

    PhysicalFilter: only switch channel A to an error device
    Previously, using the BusBypass, the PhysicalFilter tried to cut all
    traffic (including B+C). This was wrong.
    
    Now, we carefully wait until A=>D is idle, then swallow denied traffic.
    The injected traffic thus respects order and we swallow any GrantAcks.

--- src/main/scala/devices/tilelink/PhysicalFilter.scala
+++ src/main/scala/devices/tilelink/PhysicalFilter.scala
@@ -55,14 +55,14 @@ object DevicePMP
 }
 
 /** PhysicalFilter uses a set of DevicePMP registers to control whether
-  * accesses of certain types are allowed to proceed or bypassed to
-  * a /dev/null device.
+  * accesses of certain types are allowed to proceed or denied. The Filter
+  * will only prevent acquisition of NEW permissions; it will not shoot
+  * down permissions Acquired previously.
   */
 
 case class PhysicalFilterParams(
   controlAddress:   BigInt,
   controlBeatBytes: Int,
-  deviceBeatBytes:  Int = 1, // TODO: This is ignored by the BusBypassBar
   pmpRegisters:     Int = 1)
 {
   val page = 4096
@@ -73,61 +73,111 @@ case class PhysicalFilterParams(
   require (controlAddress > 0)
   require (controlAddress % size == 0)
   require (controlBeatBytes > 0 && isPow2(controlBeatBytes))
-  require (deviceBeatBytes  > 0 && isPow2(deviceBeatBytes))
 }
 
-class PhysicalFilter(params: PhysicalFilterParams)(implicit p: Parameters) extends TLBusBypassBase(params.deviceBeatBytes)
+class PhysicalFilter(params: PhysicalFilterParams)(implicit p: Parameters) extends LazyModule
 {
-  val device = new SimpleDevice("physical-filter", Seq("sifive,physical-filter-v0"))
+  val node = TLAdapterNode(managerFn = { mp => mp.copy(
+    endSinkId  = if (mp.endSinkId == 0) { 0 } else { mp.endSinkId+1 },
+    minLatency = 1 min mp.minLatency)})
 
+  val device = new SimpleDevice("physical-filter", Seq("sifive,physical-filter-v0"))
   val controlNode = TLRegisterNode(
     address   = Seq(AddressSet(params.controlAddress, params.size-1)),
     device    = device,
     beatBytes = params.controlBeatBytes)
 
   lazy val module = new LazyModuleImp(this) {
-    // We need to be able to represent +1 larger than the largest populated address
-    val addressBits = log2Ceil(nodeOut.edges.out(0).manager.maxAddress+1+1)
-    val pmps = RegInit(Vec.fill(params.pmpRegisters) { DevicePMP(addressBits, params.pageBits) })
-    val blocks = pmps.tail.map(_.blockPriorAddress) :+ Bool(false)
-    controlNode.regmap(0 -> (pmps zip blocks).map { case (p, b) => p.fields(b) }.toList.flatten)
+    (node.in zip node.out) foreach { case ((in, edgeIn), (out, edgeOut)) =>
+      out <> in
 
-    val (in, edge) = nodeIn.in(0)
+      // Generally useful wires needed below
+      val mySinkId = UInt(edgeOut.manager.endSinkId)
+      val a_first = edgeIn.first(in.a)
+      val (d_first, d_last, _) = edgeIn.firstlast(in.d)
 
-    // Determine if a request is allowed
-    val needW = in.a.bits.opcode =/= TLMessages.Get &&
-                (in.a.bits.opcode =/= TLMessages.AcquireBlock ||
-                 in.a.bits.param  =/= TLPermissions.NtoB ||
-                 Bool(!edge.manager.anySupportAcquireB))
-    val needR = in.a.bits.opcode =/= TLMessages.PutFullData &&
-                in.a.bits.opcode =/= TLMessages.PutPartialData
-    val lt = Bool(false) +: pmps.map(in.a.bits.address < _.address)
-    val sel = (pmps.map(_.a) zip (lt.init zip lt.tail)) map { case (a, (l, r)) => a(0) && !l && r }
-    val ok = pmps.map(p => (p.r(0) || !needR) && (p.w(0) || !needW))
-    val allow = PriorityMux(sel :+ Bool(true), ok :+ Bool(false)) // no match => deny
+      // We need to be able to represent +1 larger than the largest populated address
+      val addressBits = log2Ceil(edgeOut.manager.maxAddress+1+1)
+      val pmps = RegInit(Vec.fill(params.pmpRegisters) { DevicePMP(addressBits, params.pageBits) })
+      val blocks = pmps.tail.map(_.blockPriorAddress) :+ Bool(false)
+      controlNode.regmap(0 -> (pmps zip blocks).map { case (p, b) => p.fields(b) }.toList.flatten)
 
-    bar.module.io.bypass := !allow
+      // Determine if a request is allowed
+      val needW = in.a.bits.opcode =/= TLMessages.Get &&
+                  (in.a.bits.opcode =/= TLMessages.AcquireBlock ||
+                   in.a.bits.param  =/= TLPermissions.NtoB ||
+                   Bool(!edgeIn.manager.anySupportAcquireB))
+      val needR = in.a.bits.opcode =/= TLMessages.PutFullData &&
+                  in.a.bits.opcode =/= TLMessages.PutPartialData
+      val lt = Bool(false) +: pmps.map(in.a.bits.address < _.address)
+      val sel = (pmps.map(_.a) zip (lt.init zip lt.tail)) map { case (a, (l, r)) => a(0) && !l && r }
+      val ok = pmps.map(p => (p.r(0) || !needR) && (p.w(0) || !needW))
+      val allowFirst = PriorityMux(sel :+ Bool(true), ok :+ Bool(false)) // no match => deny
+      val allow = allowFirst holdUnless a_first // don't change our mind mid-transaction
 
-    // Track when a request is not allowed to be promoted toT
-    if (edge.manager.anySupportAcquireB) {
-      val wSourceVec = Reg(Vec(edge.client.endSourceId, Bool()))
-      val aWOk = PriorityMux(sel, pmps.map(_.w(0)))
-      val dWOk = wSourceVec(in.d.bits.source)
-      val bypass = Bool(edge.manager.minLatency == 0) && in.a.valid && in.a.bits.source === in.d.bits.source
-      val d_grant = in.d.bits.opcode === TLMessages.Grant || in.d.bits.opcode === TLMessages.GrantData
-      val d_first = edge.first(in.d)
-      val dWHeld = Mux(bypass, aWOk, dWOk) holdUnless d_first
+      // Track the progress of transactions from A => D
+      val d_rack  = Bool(edgeIn.manager.anySupportAcquireB) && in.d.bits.opcode === TLMessages.ReleaseAck
+      val flight = RegInit(UInt(0, width = log2Ceil(edgeIn.client.endSourceId+1+1))) // +1 for inclusive range +1 for a_first vs. d_last
+      flight := flight + (a_first && in.a.fire()) - (d_last && !d_rack && in.d.fire())
 
-      when (d_grant && !dWHeld) {
-        in.d.bits.param := TLPermissions.toB
+      // Discard denied A traffic, but first block it until there is nothing in-flight
+      in.a.ready := Mux(allow, out.a.ready, !a_first || flight === UInt(0))
+      out.a.valid := in.a.valid && allow
+
+      // Frame an appropriate deny message
+      val denyValid = RegInit(Bool(false))
+      val deny = Reg(in.d.bits)
+      val d_opcode = TLMessages.adResponse(in.a.bits.opcode)
+      when (in.a.fire() && !allow) {
+        denyValid    := Bool(true)
+        deny.opcode  := d_opcode
+        deny.param   := UInt(0) // toT, but error grants must be handled transiently (ie: you don't keep permissions)
+        deny.size    := in.a.bits.size
+        deny.source  := in.a.bits.source
+        deny.sink    := mySinkId
+        deny.denied  := Bool(true)
+        deny.data    := UInt(0)
+        deny.corrupt := d_opcode(0)
+      }
+      when (denyValid && in.d.ready && d_last) {
+        denyValid := Bool(false)
       }
 
-      when (in.a.fire()) {
-        wSourceVec(in.a.bits.source) := aWOk
-      }
+      val out_d = Wire(in.d.bits)
+      out_d := out.d.bits
 
-      edge.client.unusedSources.foreach { id =>
-        wSourceVec(id) := Bool(true)
+      // Deny can have unconditional priority, because the only out.d message possible is
+      // ReleaseAck, because we waited for all A=>D traffic to complete. ReleaseAck is
+      // single-beat, so it's safe to just arbitrate without counting the responses.
+      in.d.valid := out.d.valid || denyValid
+      out.d.ready := !denyValid && in.d.ready
+      in.d.bits := Mux(denyValid, deny, out_d)
+
+      // Track when a request is not allowed to be promoted toT
+      if (edgeIn.manager.anySupportAcquireB) {
+        val wSourceVec = Reg(Vec(edgeIn.client.endSourceId, Bool()))
+        val aWOk = PriorityMux(sel, pmps.map(_.w(0)))
+        val dWOk = wSourceVec(in.d.bits.source)
+        val bypass = Bool(edgeIn.manager.minLatency == 0) && in.a.valid && in.a.bits.source === in.d.bits.source
+        val d_grant = in.d.bits.opcode === TLMessages.Grant || in.d.bits.opcode === TLMessages.GrantData
+        val dWHeld = Mux(bypass, aWOk, dWOk) holdUnless d_first
+
+        when (d_grant && !dWHeld) {
+          in.d.bits.param := TLPermissions.toB
+        }
+
+        when (in.a.fire() && a_first) {
+          wSourceVec(in.a.bits.source) := aWOk
+        }
+
+        edgeIn.client.unusedSources.foreach { id =>
+          wSourceVec(id) := Bool(true)
+        }
+
+        // Swallow GrantAcks
+        val isMyId = mySinkId === in.e.bits.sink
+        out.e.valid := in.e.valid && !isMyId
+        in.e.ready := out.e.ready || isMyId
       }
     }
   }
