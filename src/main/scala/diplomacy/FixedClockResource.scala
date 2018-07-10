d75cff0eb482fea9207457347db8f766e1d259ca
Wesley W. Terpstra <wesley@sifive.com>
2018-07-09 17:15:03 -0700

    FixedClockResource: use the new DeviceSnippet API

--- src/main/scala/diplomacy/FixedClockResource.scala
+++ src/main/scala/diplomacy/FixedClockResource.scala
@@ -4,17 +4,14 @@ package freechips.rocketchip.diplomacy
 
 class FixedClockResource(val name: String, val freqMHz: Double, val prefix: String = "soc/")
 {
-  val device = new Device {
-    def describe(resources: ResourceBindings) =
+  val device = new DeviceSnippet {
+    def describe() =
       Description(prefix + name, Map(
         "#clock-cells"       -> Seq(ResourceInt(0)),
         "clock-frequency"    -> Seq(ResourceInt(freqMHz * 1000000)),
         "clock-output-names" -> Seq(ResourceString(name)),
         "compatible"         -> Seq(ResourceString("fixed-clock"))))
   }
-
-  // Just bind something to ensure it shows up
-  ResourceBinding { Resource(device, "nil").bind(ResourceInt(42)) }
 
   def bind(dev: Device) {
     ResourceBinding { Resource(dev, "clocks").bind(ResourceReference(device.label)) }
