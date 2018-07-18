e0b72c4249c2fe6cb3552301dd51b90c80da301a
Wesley W. Terpstra <wesley@sifive.com>
2018-07-18 12:40:07 -0700

    diplomacy: desugar BundleBridge -- it was too confusing
    Sometimes more typing is better.

--- src/main/scala/diplomacy/BundleBridge.scala
+++ src/main/scala/diplomacy/BundleBridge.scala
@@ -4,6 +4,7 @@ package freechips.rocketchip.diplomacy
 
 import chisel3._
 import chisel3.internal.sourceinfo.SourceInfo
+import chisel3.experimental.IO
 import freechips.rocketchip.config.{Parameters,Field}
 
 case class BundleBridgeParams[T <: Data](gen: () => T)
@@ -18,40 +19,33 @@ class BundleBridgeImp[T <: Data]() extends SimpleNodeImp[BundleBridgeParams[T], 
 
 case class BundleBridgeSink[T <: Data]()(implicit valName: ValName) extends SinkNode(new BundleBridgeImp[T])(Seq(BundleBridgeNull()))
 {
-  def io: T = in(0)._1
+  def bundle: T = in(0)._1
+
+  def makeIO()(implicit valName: ValName): T = {
+    val io = IO(Output(bundle.cloneType))
+    io.suggestName(valName.name)
+    io <> bundle
+    io
+  }
 }
 
 case class BundleBridgeSource[T <: Data](gen: () => T)(implicit valName: ValName) extends SourceNode(new BundleBridgeImp[T])(Seq(BundleBridgeParams(gen)))
 {
-  def sink(implicit p: Parameters) = {
+  def bundle: T = out(0)._1
+
+  def makeIO()(implicit valName: ValName): T = {
+    val io = IO(Input(bundle.cloneType))
+    io.suggestName(valName.name)
+    bundle <> io
+    io
+  }
+
+  private var doneSink = false
+  def makeSink()(implicit p: Parameters) = {
+    require (!doneSink, "Can only call makeSink() once")
+    doneSink = true
     val sink = BundleBridgeSink[T]()
     sink := this
     sink
   }
-}
-
-class BundleBridge[D <: Data, T <: LazyModule](lm: => T { val module: { val io: D }})(implicit p: Parameters) extends LazyModule
-{
-  val child = LazyModule(lm)
-  val ioNode = BundleBridgeSource(() => child.module.io.cloneType)
-  override lazy val desiredName = s"BundleBridge_${child.desiredName}"
-
-  lazy val module = new LazyModuleImp(this) {
-    val (io, _) = ioNode.out(0)
-    io <> child.module.io
-  }
-}
-
-/* Usage:
- *   // Wrap up SomeDevice's module.io Bundle into an ioNode
- *   val deviceBridge = BundleBridge(new SomeDevice) // BundleBridge() replaces LazyModule()
- *   // Somewhere else in the design in LazyModule scope:
- *   val sink = deviceBridge.ioNode.sink // creates a sink node of matching type
- *   // In LazyModuleImp scope:
- *   val io = sink.io // io is a Wire () connected to device.module.io
- */
-object BundleBridge
-{
-  def apply[D <: Data, T <: LazyModule](lm: => T { val module: { val io: D }})(implicit p: Parameters, valName: ValName) =
-    LazyModule(new BundleBridge(lm))
 }
