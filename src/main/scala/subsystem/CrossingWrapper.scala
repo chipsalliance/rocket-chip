40d949be7126c6301478e1c790dc7d29311d66f2
Henry Cook <henry@sifive.com>
2018-07-19 16:29:46 -0700

    crossingwrapper: update deprecations

--- src/main/scala/subsystem/CrossingWrapper.scala
+++ src/main/scala/subsystem/CrossingWrapper.scala
@@ -8,23 +8,29 @@ import freechips.rocketchip.tilelink._
 import freechips.rocketchip.amba.axi4._
 import freechips.rocketchip.interrupts._
 
-@deprecated("HasCrossing has been replaced by protocol-specific CrossingHelpers", "rocket-chip 1.3")
 trait HasCrossing extends LazyScope { this: LazyModule =>
 
   def crossing: ClockCrossingType
-  protected lazy val tl_xing = new TLCrossingHelper(this, crossing)
-  protected lazy val axi4_xing = new AXI4CrossingHelper(this, crossing)
-  protected lazy val int_xing = new IntCrossingHelper(this, crossing)
+  lazy val tl_xing = new TLCrossingHelper(this, crossing)
+  lazy val axi4_xing = new AXI4CrossingHelper(this, crossing)
+  lazy val int_xing = new IntCrossingHelper(this, crossing)
 
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossTLIn   (implicit p: Parameters): TLNode  = tl_xing.crossTLIn
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossTLOut  (implicit p: Parameters): TLNode  = tl_xing.crossTLOut
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossAXI4In (implicit p: Parameters): AXI4Node= axi4_xing.crossAXI4In
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossAXI4Out(implicit p: Parameters): AXI4Node= axi4_xing.crossAXI4Out
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossIntIn  (implicit p: Parameters): IntNode = int_xing.crossIntIn
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossIntOut (implicit p: Parameters): IntNode = int_xing.crossIntOut
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossIntIn (alreadyRegistered: Boolean)(implicit p: Parameters): IntNode = int_xing.crossIntIn (alreadyRegistered)
+  @deprecated("Make a CrossingHelper and call a method on it instead", "rocket-chip 1.3")
   def crossIntOut(alreadyRegistered: Boolean)(implicit p: Parameters): IntNode = int_xing.crossIntOut(alreadyRegistered)
 }
 
-@deprecated("CrossingWrapper has been replaced by protocol-specific CrossingHelpers", "rocket-chip 1.3")
 class CrossingWrapper(val crossing: ClockCrossingType)(implicit p: Parameters) extends SimpleLazyModule with HasCrossing
