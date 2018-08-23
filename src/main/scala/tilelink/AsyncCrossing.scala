886ae3a0261584231ea650822e875e72fa7c5a13
Wesley W. Terpstra <wesley@sifive.com>
2018-08-22 17:21:25 -0700

    AsyncCrossing: add narrow crossing to unit tests

--- src/main/scala/tilelink/AsyncCrossing.scala
+++ src/main/scala/tilelink/AsyncCrossing.scala
@@ -125,10 +125,10 @@ class TLAsyncCrossing(params: AsyncQueueParams = AsyncQueueParams())(implicit p:
 /** Synthesizeable unit tests */
 import freechips.rocketchip.unittest._
 
-class TLRAMAsyncCrossing(txns: Int)(implicit p: Parameters) extends LazyModule {
+class TLRAMAsyncCrossing(txns: Int, params: AsynchronousCrossing = AsynchronousCrossing())(implicit p: Parameters) extends LazyModule {
   val model = LazyModule(new TLRAMModel("AsyncCrossing"))
   val fuzz = LazyModule(new TLFuzzer(txns))
-  val island = LazyModule(new CrossingWrapper(AsynchronousCrossing(8)))
+  val island = LazyModule(new CrossingWrapper(params))
   val ram  = island { LazyModule(new TLRAM(AddressSet(0x0, 0x3ff))) }
 
   island.crossTLIn(ram.node) := TLFragmenter(4, 256) := TLDelayer(0.1) := model.node := fuzz.node
@@ -143,6 +143,7 @@ class TLRAMAsyncCrossing(txns: Int)(implicit p: Parameters) extends LazyModule {
 }
 
 class TLRAMAsyncCrossingTest(txns: Int = 5000, timeout: Int = 500000)(implicit p: Parameters) extends UnitTest(timeout) {
-  val dut = Module(LazyModule(new TLRAMAsyncCrossing(txns)).module)
-  io.finished := dut.io.finished
+  val dut_wide   = Module(LazyModule(new TLRAMAsyncCrossing(txns)).module)
+  val dut_narrow = Module(LazyModule(new TLRAMAsyncCrossing(txns, AsynchronousCrossing(safe = false, narrow = true))).module)
+  io.finished := dut_wide.io.finished && dut_narrow.io.finished
 }
