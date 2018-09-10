060d851b2c88cf66344dc7cf0f6f51dfca0adbd9
Zihao Yu <yuzihao@ict.ac.cn>
2018-09-10 10:21:35 +0000

    tile,BaseTile: generate 'd' according to fpu.fLen

--- src/main/scala/tile/BaseTile.scala
+++ src/main/scala/tile/BaseTile.scala
@@ -86,7 +86,7 @@ trait HasTileParameters {
     val m = if (tileParams.core.mulDiv.nonEmpty) "m" else ""
     val a = if (tileParams.core.useAtomics) "a" else ""
     val f = if (tileParams.core.fpu.nonEmpty) "f" else ""
-    val d = if (tileParams.core.fpu.nonEmpty && p(XLen) > 32) "d" else ""
+    val d = if (tileParams.core.fpu.nonEmpty && tileParams.core.fpu.get.fLen > 32) "d" else ""
     val c = if (tileParams.core.useCompressed) "c" else ""
     s"rv${p(XLen)}i$m$a$f$d$c"
   }
