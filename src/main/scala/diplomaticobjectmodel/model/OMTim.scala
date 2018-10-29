ec02c69604b1db8eb985244700573213f1df8caf
Derek Pappas <depappas@sifive.com>
2018-10-28 22:56:26 -0700

    updating

--- src/main/scala/diplomaticobjectmodel/model/OMTim.scala
+++
@@ -1,9 +0,0 @@
-// See LICENSE for license details.
-
-package freechips.rocketchip.diplomaticobjectmodel.model
-
-case class OMTim(
-  memoryRegions: List[OMMemoryRegion],
-  interrupts: List[OMInterrupt]
-)  extends OMDevice
-
