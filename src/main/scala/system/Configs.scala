598bf3717b2de132b3579352a3dc07030c7db7f7
Henry Cook <henry@sifive.com>
2019-01-21 15:09:54 -0800

    add missing new

--- src/main/scala/system/Configs.scala
+++ src/main/scala/system/Configs.scala
@@ -24,7 +24,7 @@ class BaseConfig extends Config(
 
 class DefaultConfig extends Config(new WithNBigCores(1) ++ new BaseConfig)
 
-class DefaultBufferlessConfig extends Config(new WithBufferlessBroadcastHub ++ DefaultConfig)
+class DefaultBufferlessConfig extends Config(new WithBufferlessBroadcastHub ++ new DefaultConfig)
 class DefaultSmallConfig extends Config(new WithNSmallCores(1) ++ new BaseConfig)
 class DefaultRV32Config extends Config(new WithRV32 ++ new DefaultConfig)
 
