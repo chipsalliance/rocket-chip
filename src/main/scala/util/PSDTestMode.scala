2ff44a37aca88cfa9d0eb48abe33bd439d7a9b48
Megan Wachs <megan@sifive.com>
2019-01-22 12:30:16 -0800

    PSDTestMode: Add a Config knob for the PSD Bundle Broadcast

--- src/main/scala/util/PSDTestMode.scala
+++ src/main/scala/util/PSDTestMode.scala
@@ -6,6 +6,12 @@ import Chisel._
 import freechips.rocketchip.config._
 
 case object IncludePSDTest extends Field[Boolean](false)
+//Note: one should never have `IncludePSDTest without
+// PSDTestModeBroadcastKey defined everywhere one wants to use it.
+// One could just use the PSDTestModeBroadcastKey.isDefined directly,
+// but it's a more error prone process to instantiate it and pass it in an
+// altered Parameters so the API is to have both.
+case object PSDTestModeBroadcastKey extends Field[Option[BundleBridgeNexus]](None)
 
 class PSDTestMode extends Bundle {
   val test_mode       = Bool()
