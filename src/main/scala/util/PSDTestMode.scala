4d3da8d281b2d4b707292720c7b2e77072637b40
Henry Cook <henry@sifive.com>
2019-01-23 18:15:01 -0800

    PSDTestModeBroadcastKey: update field type

--- src/main/scala/util/PSDTestMode.scala
+++ src/main/scala/util/PSDTestMode.scala
@@ -4,6 +4,7 @@ package freechips.rocketchip.util
 
 import Chisel._
 import freechips.rocketchip.config._
+import freechips.rocketchip.diplomacy.BundleBridgeNexus
 
 case object IncludePSDTest extends Field[Boolean](false)
 //Note: one should never have `IncludePSDTest without
@@ -11,7 +12,7 @@ case object IncludePSDTest extends Field[Boolean](false)
 // One could just use the PSDTestModeBroadcastKey.isDefined directly,
 // but it's a more error prone process to instantiate it and pass it in an
 // altered Parameters so the API is to have both.
-case object PSDTestModeBroadcastKey extends Field[Option[BundleBridgeNexus]](None)
+case object PSDTestModeBroadcastKey extends Field[Option[BundleBridgeNexus[PSDTestMode]]](None)
 
 class PSDTestMode extends Bundle {
   val test_mode       = Bool()
