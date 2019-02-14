4a2798e3c25b95fdd46538f8309e2a083ed0369d
Derek Pappas <depappas@sifive.com>
2019-02-13 18:39:57 -0800

    fix

--- src/main/scala/LogicalTreeRegistry.scala
+++ src/main/scala/LogicalTreeRegistry.scala
@@ -3,20 +3,68 @@
 package freechips.rocketchip.tile
 
 import chisel3.experimental.RawModule
+import freechips.rocketchip.diplomacy.ResourceBindingsMap
+import freechips.rocketchip.diplomaticobjectmodel.model.OMComponent
 
-class LogicalTreeRegistry {
-  val fList: List[() => (RawModule, RawModule)] = List[() => (RawModule, RawModule)]()
+import scala.collection.mutable
 
-  def register(f: () => (RawModule, RawModule)): Unit = {
-    fList.+:(f)
+case class OMEdge(
+  parent: RawModule,
+  child: RawModule
+)
+
+trait OMRegistrar {
+  def parent: RawModule
+  def child: RawModule
+  def getEdge(f: () => OMEdge): OMEdge
+  def getOMComponents(): Seq[OMComponent]
+}
+
+/**
+  * Used to construct th logical tree from the Edge's
+  * @param components
+  * @param children
+  */
+
+case class OMVertex(
+  components: List[OMComponent],
+  children: List[OMVertex]
+)
+
+case object LogicalTreeRegistry {
+  private var resourceBindingsMap: Option[ResourceBindingsMap] = None
+
+  private val registryList: List[() => OMEdge] = List[() => OMEdge]()
+
+  def setResourceBindingsMap(rbm: ResourceBindingsMap): Unit = resourceBindingsMap = Some(rbm)
+  def getResourceBindingsMap(): ResourceBindingsMap = resourceBindingsMap.get
+
+  def register(lr: OMRegistrar): Unit = {
+    registryList.+:(lr)
   }
 
-  def makePairs(): List[(RawModule, RawModule)] = {
-    fList.map{
-      case f => f()
+  def cycleCheck(): Boolean = false
+
+  def makeTree(): Unit = {
+    require(resourceBindingsMap != None)
+
+  }
+
+  def getObjectModel(): OMComponent = {
+
+      registryList.map{
+        case lr: OMRegistrar => lr.getOMComponents()
+      }
     }
   }
 
-  def makeTree():
+}
+
+class TopologicalSort {
+  val stack = mutable.MutableList[OMVertex]()
+  val visited = mutable.Set[OMVertex]()
+
+
+
 
 }
