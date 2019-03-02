44138e3417ca4650022e21c9612302325fe5b703
Derek Pappas <35471038+derekpappas@users.noreply.github.com>
2019-03-01 21:48:04 -0800

    adding rangefilter to the getOMMemoryRegions gating expression (#1859)
    * adding rangefilter to the getOMMemoryRegions gating expression

--- src/main/scala/diplomaticobjectmodel/DiplomaticObjectModelUtils.scala
+++ src/main/scala/diplomaticobjectmodel/DiplomaticObjectModelUtils.scala
@@ -168,7 +168,7 @@ object DiplomaticObjectModelAddressing {
 
   def getOMMemoryRegions(name: String, resourceBindings: ResourceBindings, omRegMap: Option[OMRegisterMap] = None): Seq[OMMemoryRegion]= {
     resourceBindings.map.collect {
-      case (x: String, seq: Seq[Binding]) if (DiplomacyUtils.regFilter(x)) =>
+      case (x: String, seq: Seq[Binding]) if (DiplomacyUtils.regFilter(x) || DiplomacyUtils.rangeFilter(x)) =>
         seq.map {
           case Binding(device: Option[Device], value: ResourceValue) => omMemoryRegion(name, DiplomacyUtils.regName(x).getOrElse(""), value, omRegMap)
         }
@@ -177,7 +177,7 @@ object DiplomaticObjectModelAddressing {
 
   def getOMPortMemoryRegions(name: String, resourceBindings: ResourceBindings, omRegMap: Option[OMRegisterMap] = None): Seq[OMMemoryRegion]= {
     resourceBindings.map.collect {
-      case (x: String, seq: Seq[Binding]) if (DiplomacyUtils.rangeFilter(x)) =>
+      case (x: String, seq: Seq[Binding]) if (DiplomacyUtils.regFilter(x) || DiplomacyUtils.rangeFilter(x)) =>
         seq.map {
           case Binding(device: Option[Device], value: ResourceValue) => omMemoryRegion(name, "port memory region", value, omRegMap)
         }
