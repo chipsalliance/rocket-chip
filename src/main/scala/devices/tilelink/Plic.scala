ca855f1dfffb38b5418ed63e0d8a7364db1c5271
Derek Pappas <depappas@sifive.com>
2019-03-08 01:37:27 -0800

    restore import order

--- src/main/scala/devices/tilelink/Plic.scala
+++ src/main/scala/devices/tilelink/Plic.scala
@@ -2,20 +2,20 @@
 
 package freechips.rocketchip.devices.tilelink
 
-import Chisel.ImplicitConversions._
 import Chisel._
-import chisel3.internal.sourceinfo.SourceInfo
+import Chisel.ImplicitConversions._
 import freechips.rocketchip.config.{Field, Parameters}
-import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.diplomaticobjectmodel.DiplomaticObjectModelAddressing
-import freechips.rocketchip.diplomaticobjectmodel.model._
-import freechips.rocketchip.interrupts._
-import freechips.rocketchip.regmapper._
 import freechips.rocketchip.subsystem.BaseSubsystem
-import freechips.rocketchip.tile.LogicalTree
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.regmapper._
 import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
 import freechips.rocketchip.util._
 import freechips.rocketchip.util.property._
+import chisel3.internal.sourceinfo.SourceInfo
+import freechips.rocketchip.diplomaticobjectmodel.DiplomaticObjectModelUtils
+import freechips.rocketchip.diplomaticobjectmodel.DiplomaticObjectModelAddressing
+import freechips.rocketchip.diplomaticobjectmodel.model._
 
 import scala.math.min
 
