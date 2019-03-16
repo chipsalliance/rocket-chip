1ecf3b7154fc62969afc9bf76e7ac8df7c33e768
Derek Pappas <35471038+derekpappas@users.noreply.github.com>
2019-03-16 11:47:44 -0700

    Add customExtension to OMISA (#1877)
    * added customExtension to OMISA

--- src/main/scala/diplomaticobjectmodel/model/OMSpecification.scala
+++ src/main/scala/diplomaticobjectmodel/model/OMSpecification.scala
@@ -2,8 +2,20 @@
 
 package freechips.rocketchip.diplomaticobjectmodel.model
 
+trait OMSpec {
+  def name: String
+  def version: String
+  def _types: Seq[String]
+}
+
 case class OMSpecification(
   name: String,
   version: String,
-  _types: Seq[String] = Seq("OMSpecification")
-)
+  _types: Seq[String] = Seq("OMISASpecification", "OMSpecification")
+) extends OMSpec
+
+trait OMCustomExtensionSpecification extends OMSpec {
+  def name: String
+  def version: String
+  def _types: Seq[String] = Seq("OMCustomExtensionSpecification", "OMSpecification")
+}
