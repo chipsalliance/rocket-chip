1496d5c740f71c0806efae00c982c50d8b673afa
Henry Cook <henry@sifive.com>
2019-05-07 18:10:09 -0700

    diplomacy: tweak region comments

--- src/main/scala/diplomacy/Parameters.scala
+++ src/main/scala/diplomacy/Parameters.scala
@@ -6,7 +6,7 @@ import Chisel._
 import freechips.rocketchip.util.{ShiftQueue, RationalDirection, FastToSlow, AsyncQueueParams}
 import scala.reflect.ClassTag
 
-/** Options for memory regions */
+/** Options for describing the attributes of memory regions */
 object RegionType {
   // Define the 'more relaxed than' ordering
   val cases = Seq(CACHED, TRACKED, UNCACHED, IDEMPOTENT, VOLATILE, PUT_EFFECTS, GET_EFFECTS)
@@ -14,9 +14,9 @@ object RegionType {
     def compare(that: T): Int = cases.indexOf(that) compare cases.indexOf(this)
   }
 
-  case object CACHED      extends T // an intermediate agent may take ownership on your behalf
-  case object TRACKED     extends T // another master may have taken ownership of the address
-  case object UNCACHED    extends T // not cached yet, but should be cached when possible
+  case object CACHED      extends T // an intermediate agent may have cached a copy of the region for you
+  case object TRACKED     extends T // the region may have been cached by another master, but coherence is being provided
+  case object UNCACHED    extends T // the region has not been cached yet, but should be cached when possible
   case object IDEMPOTENT  extends T // gets return most recently put content, but content should not be cached
   case object VOLATILE    extends T // content may change without a put, but puts and gets have no side effects
   case object PUT_EFFECTS extends T // puts produce side effects and so must not be combined/delayed
