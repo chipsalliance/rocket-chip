5123489973fc3197ab69f4dfbe84d179eb42af27
Derek Pappas <depappas@sifive.com>
2019-05-19 15:00:33 -0700

    handle condition when the l1_fuzzer does not make a Logical Tree

--- src/main/scala/diplomaticobjectmodel/logicaltree/LogicalTreeNode.scala
+++ src/main/scala/diplomaticobjectmodel/logicaltree/LogicalTreeNode.scala
@@ -58,11 +58,14 @@ object LogicalModuleTree {
   def cache() = BindingScope.bindingScopes.map(_.getResourceBindingsMap)
 
   def bind(): Seq[OMComponent] = {
-    val resourceBindingsMaps = cache()
-    def getOMComponentTree(node: LogicalTreeNode): Seq[OMComponent] = {
-      node.getOMComponents(resourceBindings(node.getDevice, resourceBindingsMaps), tree.get(node).getOrElse(Nil).flatMap(getOMComponentTree))
-    }
+    if (tree.size == 0) Nil
+    else {
+      val resourceBindingsMaps = cache()
+      def getOMComponentTree(node: LogicalTreeNode): Seq[OMComponent] = {
+        node.getOMComponents(resourceBindings(node.getDevice, resourceBindingsMaps), tree.get(node).getOrElse(Nil).flatMap(getOMComponentTree))
+      }
 
-    getOMComponentTree(rootLogicalTreeNode)
+      getOMComponentTree(rootLogicalTreeNode)
+    }
   }
 }
