43fb1b90668ff9001cfd28116a2c62aaa5a70006
Richard Xia <richardxia@richardxia.com>
2019-05-26 16:16:23 -0700

    Revert "sbus: actually use policy param (#1915)"
    This reverts commit 70854327e8cdfd3d2601918f54a057ca60a29941.

--- src/main/scala/subsystem/SystemBus.scala
+++ src/main/scala/subsystem/SystemBus.scala
@@ -22,12 +22,7 @@ class SystemBus(params: SystemBusParams)(implicit p: Parameters)
     with CanHaveBuiltInDevices
     with CanAttachTLSlaves
     with CanAttachTLMasters
-{
-  private val system_bus_xbar = LazyModule(new TLXbar(policy = params.policy))
-  def inwardNode: TLInwardNode = system_bus_xbar.node
-  def outwardNode: TLOutwardNode = system_bus_xbar.node
-  def busView: TLEdge = system_bus_xbar.node.edges.in.head
-
+    with HasTLXbarPhy {
   attachBuiltInDevices(params)
 
   def fromTile
