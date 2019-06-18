6852af9f92109608e179ddc9caed12f0e7e242bd
Derek Pappas <depappas@sifive.com>
2019-06-18 11:56:00 -0700

    rm private from omMemoryRegion

--- src/main/scala/diplomaticobjectmodel/DiplomaticObjectModelUtils.scala
+++ src/main/scala/diplomaticobjectmodel/DiplomaticObjectModelUtils.scala
@@ -122,7 +122,7 @@ object DiplomaticObjectModelAddressing {
     }
   }
 
-  private def omMemoryRegion(name: String, description: String, value: ResourceValue, omRegMap: Option[OMRegisterMap]): OMMemoryRegion = {
+  def omMemoryRegion(name: String, description: String, value: ResourceValue, omRegMap: Option[OMRegisterMap]): OMMemoryRegion = {
     val (omRanges, permissions) = value match {
       case rm: ResourceMapping =>
         (omAddressSets(rm.address, name),rm.permissions)
