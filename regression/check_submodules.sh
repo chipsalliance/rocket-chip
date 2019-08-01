2b7112a1f149a2347e0eb8214d8ee9d59595a9c3
Abert Chen <albert.chen@sifive.com>
2019-08-01 16:16:30 -0700

    add submodule check to travis

---
+++ regression/check_submodules.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+filter=".[]"
+filter+="|select(.name == \\\"\$path\\\")"
+filter+="|select(.name == \\\"\$name\\\")"
+filter+="|select(.commit == \\\"\$sha1\\\")"
+filter="\"$filter\""
+
+git submodule foreach --quiet "jq -e $filter ../wit-manifest.json" || { \
+  echo "git submodules do not match wit dependencies!" && exit 1; \
+}
