c081324ca28bc57989b978b04386c3b68868508e
Abert Chen <albert.chen@sifive.com>
2019-08-01 22:13:39 -0700

    use single quotes

--- regression/check_submodules.sh
+++ regression/check_submodules.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-filter=".[]"
-filter+="|select(.name == \\\"\$path\\\")"
-filter+="|select(.name == \\\"\$name\\\")"
-filter+="|select(.commit == \\\"\$sha1\\\")"
+filter='.[]'
+filter+='|select(.name == \"$path\")'
+filter+='|select(.name == \"$name\")'
+filter+='|select(.commit == \"$sha1\")'
 filter="\"$filter\""
 
 git submodule foreach --quiet "jq -e $filter ../wit-manifest.json" || { \
