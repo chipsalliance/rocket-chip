36c39f40db94939f7e3fee91a9a62bf876156d48
Abert Chen <albert.chen@sifive.com>
2019-08-02 11:33:53 -0700

    add comments, move check_submodules script

--- regression/check_submodules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-filter='.[]|select(.commit == "\1")|select(.name == "\2")'
-
-git submodule status \
-| sed -r "s/\-([a-zA-Z0-9]+)\s([a-zA-Z0-9\-]+)/$filter/g" \
-| xargs -d '\n' -I % sh -c "jq -e '%' wit-manifest.json"
