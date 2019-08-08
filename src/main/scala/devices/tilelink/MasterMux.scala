969abed73136f0eb2917b92f1b8465de31cc3ec4
Kevin <kevin.liu@sifive.com>
2019-08-08 10:33:49 +0800

    change import issue of LFSR64 in /devices

--- src/main/scala/devices/tilelink/MasterMux.scala
+++ src/main/scala/devices/tilelink/MasterMux.scala
@@ -6,6 +6,7 @@ import Chisel._
 import freechips.rocketchip.config.{Field, Parameters}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
+import freechips.rocketchip.util.LFSR64
 import freechips.rocketchip.util._
 
 class MasterMuxNode(uFn: Seq[TLClientPortParameters] => TLClientPortParameters)(implicit valName: ValName) extends TLCustomNode
