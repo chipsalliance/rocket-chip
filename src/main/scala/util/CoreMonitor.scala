7e1f0805cf2b3de6cabdaa07791db4ddc9d40121
Jerry Zhao <jerry.zhao@sifive.com>
2019-10-01 16:27:03 -0700

    added new case class for for collecting desired signals from a core

--- src/main/scala/util/CoreMonitor.scala
+++ src/main/scala/util/CoreMonitor.scala
@@ -7,6 +7,12 @@ import chisel3._
 
 // this bundle is used to expose some internal core signals
 // to verification monitors which sample instruction commits
+case class CoreMonitorBundleTest(
+  val xLen: Int,
+  val cease: Bool,
+  val reg_mscratch: UInt,
+  val hartid: UInt)
+
 class CoreMonitorBundle(val xLen: Int) extends Bundle with Clocked {
   val cease = Bool()
   val reg_mscratch = UInt(width = xLen.W)
@@ -15,5 +21,5 @@ class CoreMonitorBundle(val xLen: Int) extends Bundle with Clocked {
 
 // mark a module that has cores with CoreMonitorBundles
 trait HasCoreMonitorBundles {
-    def coreMonitorBundles: List[CoreMonitorBundle]
+    def coreMonitorBundles: List[CoreMonitorBundleTest]
 }
