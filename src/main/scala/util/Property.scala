f9da3240f8cbe9fd336fc25087dec547dd6974c2
Bradley Morrell <brrmorre@gmail.com>
2019-11-05 11:43:36 -0800

    added formalutils into property.scala

--- src/main/scala/util/Property.scala
+++ src/main/scala/util/Property.scala
@@ -8,12 +8,18 @@ import chisel3.util.{ReadyValidIO}
 
 sealed abstract class PropertyType(name: String) {
   override def toString: String = name
+  def flip: PropertyType
 }
 
 object PropertyType {
-  object Assert extends PropertyType("Assert")
-  object Assume extends PropertyType("Assume")
-  object Cover extends PropertyType("Cover")
+  // Also known as master, effectively contains assumes
+  object Driver  extends PropertyType("Driver") { override def flip: PropertyType = Receiver }
+
+  // Also known as slave, effectively contains asserts
+  object Receiver extends PropertyType("Receiver") { override def flip: PropertyType = Driver }
+
+  object Monitor extends PropertyType("Monitor") { override def flip: PropertyType = Monitor }
+  object Cover extends PropertyType("Cover") { override def flip: PropertyType = Cover }
 }
 
 trait BasePropertyParameters {
@@ -21,6 +27,27 @@ trait BasePropertyParameters {
   val cond: Bool
   val label: String
   val message: String
+}
+
+case class DriverPropertyParameters(
+    cond: Bool,
+    label: String = "",
+    message: String = "") extends BasePropertyParameters {
+  val pType = PropertyType.Driver
+}
+
+case class ReceiverPropertyParameters(
+    cond: Bool,
+    label: String = "",
+    message: String = "") extends BasePropertyParameters {
+  val pType = PropertyType.Receiver
+}
+
+case class MonitorPropertyParameters(
+    cond: Bool,
+    label: String = "",
+    message: String = "") extends BasePropertyParameters {
+  val pType = PropertyType.Monitor
 }
 
 case class CoverPropertyParameters(
@@ -46,6 +73,63 @@ abstract class BaseProperty {
 }
 
 case class CoverBoolean(cond: Bool, labels: Seq[String]) {
+}
+
+object Property {
+  var prop_name_set = collection.mutable.Set[String]()
+  def reset_prop_name: Unit = prop_name_set = collection.mutable.Set[String]()
+
+  def apply(dir: MonitorDirection, cond: Bool, message: String,
+            prop_type: PropertyClass=PropertyClass.Default,
+            idx: String = "")(implicit sourceInfo: SourceInfo): Unit = {
+    val line_info = sourceInfo match {
+      case SourceLine(filename, line, col) => s"${filename}_L${line}_C${col}_I${idx}".replace('.', '_')
+      case _ => ""
+    }
+    val proposed_src = prop_type.toString + "_" + line_info
+
+//    var max_rand = 15
+//    val random_src = scala.util.Random
+//    while (!prop_name_set.add(proposed_src)) {
+//      proposed_src = src + "_" + "%x".format(random_src.nextInt(max_rand))
+//      max_rand = max_rand * 2
+//    }
+    val src_wrap = s"@[${proposed_src}]"
+    if (dir==MonitorDirection.Monitor) {
+//      assert(cond, s"Assert: ${prop_type.toString} ${message} ${src_wrap}")
+      when(!cond) {
+        printf(s"assert:${proposed_src}:${prop_type.toString} ${message}")
+      }
+    } else if (dir==MonitorDirection.Receiver) {
+//      assert(cond, s"Assert: ${prop_type.toString} ${message} ${src_wrap}")
+      when(!cond) {
+        printf(s"assert:${proposed_src}:${prop_type.toString} ${message}")
+      }
+    } else if (dir==MonitorDirection.Driver) {
+//      assert(cond, s"Assume: ${prop_type.toString} ${message} ${src_wrap}")
+      when(!cond) {
+        printf(s"assume:${proposed_src}:${prop_type.toString} ${message}")
+      }
+    } else if (dir==MonitorDirection.Cover) {
+        if (prop_type==PropertyClass.CoverDisableMonitor) {
+//          assert(!cond, s"Assert: ${prop_type.toString} ${message} ${src_wrap}")
+          when(cond) { //We want to assert that the condition is never true, which is opposite of a normal assertion
+            printf(s"assert:${proposed_src}:${prop_type.toString} ${message}")
+          }
+        } else {
+//        chisel3.core.withReset(false.B) {
+//          assert(!cond, s"Cover: ${prop_type.toString} ${message} ${src_wrap}")
+            when(cond) {
+              printf(s"cover:${proposed_src}:${prop_type.toString} ${message}")
+            }
+//        }
+        }
+    }
+  }
+
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Unit ={
+    Property(MonitorDirection.Monitor, cond, "Sanity Property", PropertyClass.LocalRTL, "")
+  }
 }
 
 // CrossProperty.generateProperties() will generate Boolean crosses for the cond sequence
@@ -122,6 +206,93 @@ class CrossProperty(cond: Seq[Seq[CoverBoolean]], exclude: Seq[Seq[String]], mes
 
 }
 
+object driver {
+  private var propLib: BasePropertyLibrary = new DefaultPropertyLibrary
+  def setPropLib(lib: BasePropertyLibrary): Unit = this.synchronized {
+    propLib = lib
+  }
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond))
+  }
+  def apply(cond: Bool, label: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label))
+  }
+  def apply(cond: Bool, label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label, message))
+  }
+  def apply(prop: BaseProperty)(implicit sourceInfo: SourceInfo): Unit = {
+    prop.generateProperties().foreach( (pp: BasePropertyParameters) => {
+      if (pp.pType == PropertyType.Cover) {
+        propLib.generateProperty(CoverPropertyParameters(pp.cond, pp.label, pp.message))
+      }
+    })
+  }
+  def apply[T <: Data](rv: ReadyValidIO[T], label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    apply( rv.valid &&  rv.ready, label + "_FIRE",  message + ": valid and ready")
+    apply( rv.valid && !rv.ready, label + "_STALL", message + ": valid and not ready")
+    apply(!rv.valid &&  rv.ready, label + "_IDLE",  message + ": not valid and ready")
+    apply(!rv.valid && !rv.ready, label + "_FULL",  message + ": not valid and not ready")
+  }
+}
+
+object receiver {
+  private var propLib: BasePropertyLibrary = new DefaultPropertyLibrary
+  def setPropLib(lib: BasePropertyLibrary): Unit = this.synchronized {
+    propLib = lib
+  }
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond))
+  }
+  def apply(cond: Bool, label: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label))
+  }
+  def apply(cond: Bool, label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label, message))
+  }
+  def apply(prop: BaseProperty)(implicit sourceInfo: SourceInfo): Unit = {
+    prop.generateProperties().foreach( (pp: BasePropertyParameters) => {
+      if (pp.pType == PropertyType.Cover) {
+        propLib.generateProperty(CoverPropertyParameters(pp.cond, pp.label, pp.message))
+      }
+    })
+  }
+  def apply[T <: Data](rv: ReadyValidIO[T], label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    apply( rv.valid &&  rv.ready, label + "_FIRE",  message + ": valid and ready")
+    apply( rv.valid && !rv.ready, label + "_STALL", message + ": valid and not ready")
+    apply(!rv.valid &&  rv.ready, label + "_IDLE",  message + ": not valid and ready")
+    apply(!rv.valid && !rv.ready, label + "_FULL",  message + ": not valid and not ready")
+  }
+}
+
+object monitor {
+  private var propLib: BasePropertyLibrary = new DefaultPropertyLibrary
+  def setPropLib(lib: BasePropertyLibrary): Unit = this.synchronized {
+    propLib = lib
+  }
+  def apply(cond: Bool)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond))
+  }
+  def apply(cond: Bool, label: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label))
+  }
+  def apply(cond: Bool, label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    propLib.generateProperty(CoverPropertyParameters(cond, label, message))
+  }
+  def apply(prop: BaseProperty)(implicit sourceInfo: SourceInfo): Unit = {
+    prop.generateProperties().foreach( (pp: BasePropertyParameters) => {
+      if (pp.pType == PropertyType.Cover) {
+        propLib.generateProperty(CoverPropertyParameters(pp.cond, pp.label, pp.message))
+      }
+    })
+  }
+  def apply[T <: Data](rv: ReadyValidIO[T], label: String, message: String)(implicit sourceInfo: SourceInfo): Unit = {
+    apply( rv.valid &&  rv.ready, label + "_FIRE",  message + ": valid and ready")
+    apply( rv.valid && !rv.ready, label + "_STALL", message + ": valid and not ready")
+    apply(!rv.valid &&  rv.ready, label + "_IDLE",  message + ": not valid and ready")
+    apply(!rv.valid && !rv.ready, label + "_FULL",  message + ": not valid and not ready")
+  }
+}
+
 // The implementation using a setable global is bad, but removes dependence on Parameters
 // This change was made in anticipation of a proper cover library
 object cover {
@@ -150,5 +321,134 @@ object cover {
     apply( rv.valid && !rv.ready, label + "_STALL", message + ": valid and not ready")
     apply(!rv.valid &&  rv.ready, label + "_IDLE",  message + ": not valid and ready")
     apply(!rv.valid && !rv.ready, label + "_FULL",  message + ": not valid and not ready")
+  }
+}
+
+object SourceGet {
+  def get_line_num(implicit sourceInfo: SourceInfo): String = {
+    val line_info = sourceInfo match {
+      case SourceLine(filename, line, col) => line.toString
+      case _ => ""
+    }
+    line_info
+  }
+}
+
+object ResetUtils {
+  def inactive_output_override[T <: Data](inactive_length: Int)
+                                         (sigs: T,
+                                          override_assn: (T)=>Unit) {
+    require(inactive_length >= 0)
+
+    if(inactive_length>0) {
+      val inactive_counter = RegInit(inactive_length.U)
+      when(inactive_counter =/= 0.U) {
+        override_assn(sigs)
+        inactive_counter := inactive_counter - 1.U
+      }
+    }
+
+  }
+}
+
+object OneHot0Prop {
+  def apply(in: Seq[Bool]): Bool = {
+    if (in.size==0) { true.B }
+    else { apply(Cat(in.reverse)) }
+  }
+  def apply(in: Vec[Bool]): Bool = {
+    if (in.getWidth==0) { true.B }
+    else { apply(in.asUInt) }
+  }
+
+  def apply(in: Bits): Bool = {
+    if(in.getWidth == 0) { true.B }
+    else if(in.getWidth == 1) { true.B }
+    else {
+      val ret_val = Wire(Bool())
+      when (in(0) === true.B) { ret_val := ((in>>1).asUInt===0.U) }
+      .otherwise { ret_val := apply(in>>1) }
+      ret_val
+    }
+  }
+}
+object OneHotProp {
+  def apply(in: Seq[Bool]): Bool = {
+    if (in.size==0) {false.B}
+    else { apply(Cat(in.reverse)) }
+  }
+  def apply(in: Vec[Bool]): Bool = {
+    if (in.getWidth==0) {false.B}
+    else { apply(in.asUInt) }
+  }
+
+  def apply(in: Bits): Bool = {
+    if(in.getWidth == 0) { false.B }
+    else if(in.getWidth == 1) { in(0) }
+    else {
+      val ret_val = Wire(Bool())
+      when (in(0) === true.B) { ret_val := ((in>>1).asUInt===0.U) }
+      .otherwise { ret_val := apply(in>>1) }
+      ret_val
+    }
+  }
+}
+
+object OneHotPriorityEncoder {
+  def apply(in: Vec[Bool]): Vec[Bool] = {
+    VecInit(apply(in.asUInt).toBools)
+  }
+
+  def apply(in: Bits): Bits = {
+    if(in.getWidth==1) { in }
+    else {
+      chisel3.util.Mux1H(chisel3.util.UIntToOH(in(0)===true.B),
+                         Seq(Cat(apply(in>>1), 0.U(1.W)),
+                             1.U(in.getWidth.W)))
+    }
+  }
+}
+
+object IfThen {
+  def apply(if_clause: Bool, then_clause: Bool): Bool = {
+    !(if_clause) || then_clause
+  }
+}
+
+object TernaryIf {
+  def apply[T <: Data](if_clause: Bool, then_clause: T, else_clause: T): T = {
+    val ret_val = Wire(then_clause)
+    when(if_clause) {
+      ret_val := then_clause
+    } .otherwise {
+      ret_val := else_clause
+    }
+    ret_val
+  }
+}
+
+object Case {
+  def apply[T <: Data, R <: Data](case_var: T, sel_ret: Seq[Tuple2[T,R]]): R = {
+    val sel_vec = sel_ret.map( (sel_ret_idv: Tuple2[T,R]) => {
+      val (select, ret_val) = sel_ret_idv
+      TernaryIf(case_var.asUInt===select.asUInt, ret_val.asUInt, 0.U)
+    })
+    sel_vec.reduce( _ | _).asTypeOf(sel_ret(0)._2)
+  }
+}
+
+object InSet {
+  def apply[T <: Data](data: T, set: Seq[T]): Bool = {
+    set.map( _.asUInt === data.asUInt ).reduce(_ || _)
+  }
+}
+
+object ScalaUtils {
+  def repeatfill[T <: Any](size: Int, base_seq: Seq[T]): Seq[T] = {
+    if (size==1) {
+      Seq(base_seq.head)
+    } else {
+      base_seq.head +: ScalaUtils.repeatfill(size-1, base_seq.tail :+ base_seq.head)
+    }
   }
 }
