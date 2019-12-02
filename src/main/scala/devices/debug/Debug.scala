14794ec5881f42300fd0f3afd7de0ecd58c07555
Megan Wachs <megan@sifive.com>
2019-12-02 09:30:07 -0800

    Debug: reduce hardware added to TLError on APB interface

--- src/main/scala/devices/debug/Debug.scala
+++ src/main/scala/devices/debug/Debug.scala
@@ -611,7 +611,7 @@ class TLDebugModuleOuterAsync(device: Device)(implicit p: Parameters) extends La
     val apb2tlBuffer = LazyModule(new TLBuffer(BufferParams.pipe))
     val dmTopAddr = (1 << cfg.nDMIAddrSize) << 2
     val tlErrorParams = DevNullParams(AddressSet.misaligned(dmTopAddr, APBDebugConsts.apbDebugRegBase-dmTopAddr),
-      maxAtomic=0, maxTransfer=4)
+      buffer = false, maxAtomic=0, maxTransfer=4)
     val tlError  = LazyModule(new TLError(tlErrorParams))
     val apbXbar  = LazyModule(new APBFanout())
     val apbRegs  = LazyModule(new APBDebugRegisters())
