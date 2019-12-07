da2b9aa1f0674d76f7528448c455c02e33efc9f1
Ernie Edgar <ernie.edgar@sifive.com>
2019-12-07 10:16:47 -0800

    Chisel3: cast reset
    Add IO() required by chisel3
    
    Revert chisel3 to Chisel for now

--- src/main/scala/system/TestHarness.scala
+++ src/main/scala/system/TestHarness.scala
@@ -9,20 +9,20 @@ import freechips.rocketchip.diplomacy.LazyModule
 import freechips.rocketchip.util.AsyncResetReg
 
 class TestHarness()(implicit p: Parameters) extends Module {
-  val io = new Bundle {
-    val success = Bool(OUTPUT)
-  }
+  val io = IO(new Bundle {
+    val success = Output(Bool())
+  })
 
   val ldut = LazyModule(new ExampleRocketSystem)
   val dut = Module(ldut.module)
 
   // Allow the debug ndreset to reset the dut, but not until the initial reset has completed
-  dut.reset := reset | dut.debug.map { debug => AsyncResetReg(debug.ndreset) }.getOrElse(false.B)
+  dut.reset := (reset.asBool | dut.debug.map { debug => AsyncResetReg(debug.ndreset) }.getOrElse(false.B)).asBool
 
   dut.dontTouchPorts()
   dut.tieOffInterrupts()
   SimAXIMem.connectMem(ldut)
   SimAXIMem.connectMMIO(ldut)
   ldut.l2_frontend_bus_axi4.foreach(_.tieoff)
-  Debug.connectDebug(dut.debug, dut.psd, clock, reset, io.success)
+  Debug.connectDebug(dut.debug, dut.psd, clock, reset.asBool, io.success)
 }
