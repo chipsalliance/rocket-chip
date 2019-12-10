7752f6df9ac1bada0c96596c76a93376384ea17b
Henry Cook <henry@sifive.com>
2019-12-10 13:51:11 -0800

    clocks: ClockGroup.pickGroupFromCrossingType

--- src/main/scala/prci/ClockGroup.scala
+++ src/main/scala/prci/ClockGroup.scala
@@ -30,4 +30,53 @@ object ClockGroup
 {
   def apply()(implicit p: Parameters, valName: ValName) =
     LazyModule(new ClockGroup(valName.name)).node
+
+  def pickGroupFromCrossingType(sync: ClockGroupNode, async: ClockGroupNode)(xType: ClockCrossingType) = {
+    xType match {
+      case _: AsynchronousCrossing => async
+      case _ => sync
+    }
+  }
+}
+
+case class ClockGroupBroadcastNode(groupName: String)(implicit valName: ValName)
+  extends NexusNode(ClockGroupImp)(
+    dFn = { _ => ClockGroupSourceParameters() },
+    uFn = { seq => ClockGroupSinkParameters(name = groupName, members = seq.flatMap(_.members))},
+    outputRequiresInput = false)
+
+class ClockGroupBroadcast(groupName: String)(implicit p: Parameters) extends LazyModule
+{
+  val node = ClockGroupBroadcastNode(groupName)
+
+  lazy val module = new LazyModuleImp(this) {
+    val (in, _) = node.in.unzip
+    val (out, _) = node.out.unzip
+    val outputs = out.flatMap(_.member)
+
+    require (node.in.size <= 1)
+    if (node.in.size == 0) {
+      println(s"Diplomacy found the following clocks:\n${node.out.unzip._2.mkString("\n")}")
+      outputs.foreach { o =>
+        o.clock := clock
+        o.reset := reset
+      }
+    } else {
+      require (in.head.member.size == outputs.size)
+      in.head.member.zip(outputs).foreach { case (i, o) => o := i }
+    }
+  }
+}
+
+object ClockGroupBroadcast
+{
+  def apply()(implicit p: Parameters, valName: ValName) =
+    LazyModule(new ClockGroupBroadcast(valName.name)).node
+
+  def pickGroupFromCrossingType(sync: ClockGroupBroadcastNode, async: ClockGroupBroadcastNode)(xType: ClockCrossingType) = {
+    xType match {
+      case _: AsynchronousCrossing => async
+      case _ => sync
+    }
+  }
 }
