8688e9e21ed1459d1deb69a932b48edcee1e23a1
Henry Cook <henry@sifive.com>
2020-01-06 18:21:56 -0800

    prci: rename ClockGroupBroadcastNode to ClockGroupAggregateNode

--- src/main/scala/prci/ClockGroup.scala
+++ src/main/scala/prci/ClockGroup.scala
@@ -31,32 +31,43 @@ object ClockGroup
   def apply()(implicit p: Parameters, valName: ValName) = LazyModule(new ClockGroup(valName.name)).node
 }
 
-case class ClockGroupBroadcastNode(groupName: String)(implicit valName: ValName)
+case class ClockGroupAggregateNode(groupName: String)(implicit valName: ValName)
   extends NexusNode(ClockGroupImp)(
     dFn = { _ => ClockGroupSourceParameters() },
-    uFn = { seq => ClockGroupSinkParameters(name = groupName, members = seq.flatMap(_.members))},
-    outputRequiresInput = false)
+    uFn = { seq => ClockGroupSinkParameters(name = groupName, members = seq.flatMap(_.members))})
 
-class ClockGroupBroadcast(groupName: String)(implicit p: Parameters) extends LazyModule
+class ClockGroupAggregator(groupName: String)(implicit p: Parameters) extends LazyModule
 {
-  val node = ClockGroupBroadcastNode(groupName)
+  val node = ClockGroupAggregateNode(groupName)
 
   lazy val module = new LazyRawModuleImp(this) {
     val (in, _) = node.in.unzip
     val (out, _) = node.out.unzip
     val outputs = out.flatMap(_.member)
 
-    require (node.in.size <= 1)
-    if (node.in.size == 0) {
-      println(s"Diplomacy found the following clocks for group $groupName:\n${node.out.unzip._2.mkString("\n")}")
-    } else {
-      require (in.head.member.size == outputs.size)
-      in.head.member.zip(outputs).foreach { case (i, o) => o := i }
-    }
+    require (node.in.size == 1)
+    require (in.head.member.size == outputs.size)
+    in.head.member.zip(outputs).foreach { case (i, o) => o := i }
   }
 }
 
-object ClockGroupBroadcast
+object ClockGroupAggregator
 {
-  def apply()(implicit p: Parameters, valName: ValName) = LazyModule(new ClockGroupBroadcast(valName.name)).node
+  def apply()(implicit p: Parameters, valName: ValName) = LazyModule(new ClockGroupAggregator(valName.name)).node
+}
+
+class SimpleClockGroupSource()(implicit p: Parameters) extends LazyModule
+{
+  val node = ClockGroupSourceNode(List(ClockGroupSourceParameters()))
+
+  lazy val module = new LazyModuleImp(this) {
+    val (out, _) = node.out.unzip
+    val outputs = out.flatMap(_.member)
+    outputs.foreach { o => o.clock := clock; o.reset := reset }
+  }
+}
+
+object SimpleClockGroupSource
+{
+  def apply()(implicit p: Parameters, valName: ValName) = LazyModule(new SimpleClockGroupSource()).node
 }
