926abd900444ba4002f85f8679788ddd1390acf3
Henry Cook <henry@sifive.com>
2020-01-21 17:44:20 -0800

    groundtest: delete import that was actually unused

--- src/main/scala/groundtest/GroundTestSubsystem.scala
+++ src/main/scala/groundtest/GroundTestSubsystem.scala
@@ -7,7 +7,6 @@ import freechips.rocketchip.config.{Field, Parameters}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.diplomaticobjectmodel.model.OMInterrupt
 import freechips.rocketchip.interrupts._
-import freechips.rocketchip.prci._
 import freechips.rocketchip.subsystem._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tile._
