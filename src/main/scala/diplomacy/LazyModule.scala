d303652744aac2622476223d244671a0cee93d63
Megan Wachs <megan@sifive.com>
2020-02-08 06:47:06 -0800

    LazyModule: don't assign Reset to Bool type automatically, keep it as chisel3 reset type

--- src/main/scala/diplomacy/LazyModule.scala
+++ src/main/scala/diplomacy/LazyModule.scala
@@ -192,9 +192,13 @@ class LazyRawModuleImp(val wrapper: LazyModule) extends RawModule with LazyModul
   // It is recommended to drive these even if you manually shove most of your children
   // Otherwise, anonymous children (Monitors for example) will not be clocked
   val childClock = Wire(Clock())
-  val childReset = Wire(Reset())
+  // If we use a Chisel.Wire, then this gets WireDefault assigned to a Bool, which
+  // we can't override later to an AsyncReset. Explicitly define this as a chisel3.Wire.
+  // Also, do NOT drive it with a default value here. This will result in a Chisel elab
+  // error if someone does not actually drive it, vs waiting to see a failing sim.
+  val childReset = chisel3.Wire(Reset())
   childClock := Bool(false).asClock
-  childReset := Bool(true)
+  //childReset := Bool(true)
   val (auto, dangles) = withClockAndReset(childClock, childReset) {
     instantiate()
   }
