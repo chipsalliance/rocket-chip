87c738e2d089f59b192305db46762e648bb3f8e0
Megan Wachs <megan@sifive.com>
2020-02-18 23:46:00 -0800

    UserYanker: make Queues inside get chisel3 reset treatment

--- src/main/scala/amba/axi4/UserYanker.scala
+++ src/main/scala/amba/axi4/UserYanker.scala
@@ -2,7 +2,8 @@
 
 package freechips.rocketchip.amba.axi4
 
-import Chisel._
+import Chisel.{defaultCompileOptions => _, _}
+import freechips.rocketchip.util.CompileOptions.NotStrictInferReset
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.util._
