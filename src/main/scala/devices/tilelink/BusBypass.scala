694e1fb18520f994ef4973f4fe54cac0a21af731
Megan Wachs <megan@sifive.com>
2020-02-20 14:38:58 -0800

    Bus Bypass: remove reliance on synchronous reset
    BusBypass: correct removal of sync reset reliance

--- src/main/scala/devices/tilelink/BusBypass.scala
+++ src/main/scala/devices/tilelink/BusBypass.scala
@@ -74,11 +74,13 @@ class TLBusBypassBar(dFn: TLManagerPortParameters => TLManagerPortParameters)(im
       s"BusBypass slave device widths mismatch (${edgeOut0.manager.managers.map(_.name)} has ${edgeOut0.manager.beatBytes}B vs ${edgeOut1.manager.managers.map(_.name)} has ${edgeOut1.manager.beatBytes}B)")
 
     // We need to be locked to the given bypass direction until all transactions stop
-    val bypass = RegInit(io.bypass) // synchronous reset required
+    val in_reset = RegNext(false.B, init = true.B)
+    val bypass_reg = Reg(Bool())
+    val bypass = Mux(in_reset, io.bypass, bypass_reg)
     val (flight, next_flight) = edgeIn.inFlight(in)
 
     io.pending := (flight > 0.U)
-    when (next_flight === UInt(0)) { bypass := io.bypass }
+    when (in_reset || (next_flight === UInt(0))) { bypass_reg := io.bypass }
     val stall = (bypass =/= io.bypass) && edgeIn.first(in.a)
 
     out0.a.valid := !stall && in.a.valid &&  bypass
