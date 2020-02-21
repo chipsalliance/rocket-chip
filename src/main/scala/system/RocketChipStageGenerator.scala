9d997506bffa1971aa692cd304792b969dfeaf47
Deborah Soung <debs@sifive.com>
2020-02-20 16:14:38 -0800

    separate generator behavior into phases (#2274)

---
+++ src/main/scala/system/RocketChipStageGenerator.scala
@@ -0,0 +1,31 @@
+// See LICENSE.SiFive for license details.
+
+package freechips.rocketchip.system
+
+import chisel3.stage.{ChiselCli, ChiselStage}
+import firrtl.options.PhaseManager.PhaseDependency
+import firrtl.options.{Phase, PreservesAll, Shell, StageMain}
+import firrtl.stage.FirrtlCli
+import freechips.rocketchip.stage.RocketChipCli
+
+class RocketChipStage extends ChiselStage with PreservesAll[Phase] {
+
+  override val shell = new Shell("rocket-chip") with RocketChipCli with ChiselCli with FirrtlCli
+  override val targets: Seq[PhaseDependency] = Seq(
+    classOf[freechips.rocketchip.stage.phases.Checks],
+    classOf[freechips.rocketchip.stage.phases.PreElaboration],
+    classOf[chisel3.stage.phases.Checks],
+    classOf[chisel3.stage.phases.Elaborate],
+    classOf[chisel3.stage.phases.MaybeAspectPhase],
+    classOf[freechips.rocketchip.stage.phases.GenerateFirrtl],
+    classOf[freechips.rocketchip.stage.phases.GenerateFirrtlAnnos],
+    classOf[freechips.rocketchip.stage.phases.AddDefaultTests],
+    classOf[freechips.rocketchip.stage.phases.GenerateTestSuiteMakefrags],
+    classOf[freechips.rocketchip.stage.phases.GenerateROMs],
+    classOf[freechips.rocketchip.stage.phases.GenerateArtefacts],
+  )
+
+  // TODO: need a RunPhaseAnnotation to inject phases into ChiselStage
+}
+
+object Generator extends StageMain(new RocketChipStage)
