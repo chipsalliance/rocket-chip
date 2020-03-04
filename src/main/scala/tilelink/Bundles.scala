7de1f1f657272566abec707709f4891c611f9402
Bradley Morrell <brrmorre@gmail.com>
2020-03-04 13:49:00 -0800

    Update Bundles.scala

--- src/main/scala/tilelink/Bundles.scala
+++ src/main/scala/tilelink/Bundles.scala
@@ -178,6 +178,7 @@ final class TLBundleA(params: TLBundleParameters)
   val size    = UInt(width = params.sizeBits)
   val source  = UInt(width = params.sourceBits) // from
   val address = UInt(width = params.addressBits) // to
+  val user    = if (params.aUserBits > 0) Some(UInt(width = params.aUserBits)) else None
   // variable fields during multibeat:
   val mask    = UInt(width = params.dataBits/8)
   val data    = UInt(width = params.dataBits)
@@ -225,6 +226,7 @@ final class TLBundleD(params: TLBundleParameters)
   val source  = UInt(width = params.sourceBits) // to
   val sink    = UInt(width = params.sinkBits)   // from
   val denied  = Bool() // implies corrupt iff *Data
+  val user    = if (params.dUserBits > 0) Some(UInt(width = params.dUserBits)) else None
   // variable fields during multibeat:
   val data    = UInt(width = params.dataBits)
   val corrupt = Bool() // only applies to *Data messages
