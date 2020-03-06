106bc750e340b4356c0548f80694a4b640e4585f
Sultan Ahmed <sultan.ahmed@sifive.com>
2020-03-06 15:34:24 -0800

    For debugging.

--- src/main/scala/monitors/TLMonitorInjToDcache.scala
+++ src/main/scala/monitors/TLMonitorInjToDcache.scala
@@ -18,12 +18,30 @@ import chisel3.internal.firrtl._
 import freechips.rocketchip.util.HasRocketChipStageUtils
 import freechips.rocketchip.tilelink._
 
+case class DummyCode () extends RawModule {
+  val results: Seq[Int] = Seq[Int](0, 1, 2, 3, 4, 5, 6)
+  val values = VecInit(results.map(_.U))
+  val counter = RegInit(0.U(results.length.W))
+  counter := counter + 1.U
+  when(counter >= values.length.U) {
+    stop()
+  }.otherwise {
+//    when(reset.asBool() === false.B) {
+      printf("values(%d) = %d\n", counter, values(counter))
+      assert(counter === values(counter))
+//    }
+  }
+}
+
 
 case object TLMonitorInjToDcache extends InjectorAspect[RawModule, DCacheModule](
   {top: RawModule => Select.collectDeep(top) { case d: DCacheModule => d }},
   {d: DCacheModule => 
     // attach TLMonitor here
     printf("SULTAN from object TLMonitorInjToDcache")
+    val dummyWire = Wire(UInt(3.W)).suggestName("hello")
+    dummyWire := 5.U
+    dontTouch(dummyWire)
   }
 )
 //case object TLMonitorInjToDcache extends InjectorAspect (
