e170f8f8a21ebe8a9e04aed47c2644c06e4a442e
Deborah Soung <debs@sifive.com>
2020-03-10 16:52:06 -0700

    Revert staged generator PRs (#2329)
    * Revert "updating README examples with Make changes (#2322)"
    
    This reverts commit 875dcd3e488da0bf3c3629a0b12962d6edc55c40.
    
    * Revert "fixing aspect generation (#2309)"
    
    This reverts commit bbeb2578c3ba04e16711ea370c28ab1357d8bed0.
    
    * Revert "separate generator behavior into phases (#2274)"
    
    This reverts commit 9d997506bffa1971aa692cd304792b969dfeaf47.

--- src/main/scala/util/GeneratorUtils.scala
+++ src/main/scala/util/GeneratorUtils.scala
@@ -2,14 +2,36 @@
 
 package freechips.rocketchip.util
 
+import Chisel._
+import chisel3.RawModule
+import chisel3.internal.firrtl.Circuit
+// TODO: better job of Makefrag generation for non-RocketChip testing platforms
 import java.io.{File, FileWriter}
 
-import Chisel.throwException
-import chipsalliance.rocketchip.config.{Config, Parameters}
-import chisel3.internal.firrtl.Circuit
+import firrtl.annotations.JsonProtocol
+import freechips.rocketchip.config._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.system.{DefaultTestSuites, TestGeneration}
 
-trait HasRocketChipStageUtils {
+/** Representation of the information this Generator needs to collect from external sources. */
+case class ParsedInputNames(
+    targetDir: String,
+    topModuleProject: String,
+    topModuleClass: String,
+    configProject: String,
+    configs: String,
+    outputBaseName: Option[String]) {
+  val configClasses: Seq[String] = configs.split('_')
+  def prepend(prefix: String, suffix: String) =
+    if (prefix == "" || prefix == "_root_") suffix else (prefix + "." + suffix)
+  val fullConfigClasses: Seq[String] = configClasses.map(x => prepend(configProject, x))
+  val fullTopModuleClass: String = prepend(topModuleProject, topModuleClass)
+}
 
+/** Common utilities we supply to all Generators. In particular, supplies the
+  * canonical ways of building various JVM elaboration-time structures.
+  */
+trait HasGeneratorUtilities {
   def getConfig(fullConfigClassNames: Seq[String]): Config = {
     new Config(fullConfigClassNames.foldRight(Parameters.empty) { case (currentName, config) =>
       val currentConfig = try {
@@ -20,6 +42,22 @@ trait HasRocketChipStageUtils {
       }
       currentConfig ++ config
     })
+  }
+
+  def getParameters(names: Seq[String]): Parameters = getParameters(getConfig(names))
+
+  def getParameters(config: Config): Parameters = config.toInstance
+
+  def elaborate(fullTopModuleClassName: String, params: Parameters): Circuit = {
+    val top = () =>
+      Class.forName(fullTopModuleClassName)
+          .getConstructor(classOf[Parameters])
+          .newInstance(params) match {
+        case m: RawModule => m
+        case l: LazyModule => LazyModule(l).module
+      }
+
+    Driver.elaborate(top)
   }
 
   def enumerateROMs(circuit: Circuit): String = {
@@ -36,6 +74,73 @@ trait HasRocketChipStageUtils {
     }
     res.toString
   }
+}
+
+/** Standardized command line interface for Scala entry point */
+trait GeneratorApp extends App with HasGeneratorUtilities {
+  lazy val names: ParsedInputNames = {
+    require(args.size == 5 || args.size == 6, "Usage: sbt> " +
+      "run TargetDir TopModuleProjectName TopModuleName " +
+      "ConfigProjectName ConfigNameString [OutputFilesBaseName]")
+    val base =
+      ParsedInputNames(
+        targetDir = args(0),
+        topModuleProject = args(1),
+        topModuleClass = args(2),
+        configProject = args(3),
+        configs = args(4),
+        outputBaseName = None)
+
+    if (args.size == 6) {
+      base.copy(outputBaseName = Some(args(5)))
+    } else {
+      base
+    }
+  }
+
+  // Canonical ways of building various JVM elaboration-time structures
+  lazy val td: String = names.targetDir
+  lazy val config: Config = getConfig(names.fullConfigClasses)
+  lazy val params: Parameters = config.toInstance
+  lazy val circuit: Circuit = elaborate(names.fullTopModuleClass, params)
+
+  // Exhaustive name used to interface with external build tool targets
+  lazy val longName: String = names.outputBaseName.getOrElse(names.configProject + "." + names.configs)
+
+  /** Output FIRRTL, which an external compiler can turn into Verilog. */
+  def generateFirrtl {
+    Driver.dumpFirrtl(circuit, Some(new File(td, s"$longName.fir"))) // FIRRTL
+  }
+
+  def generateAnno {
+    val annotationFile = new File(td, s"$longName.anno.json")
+    val af = new FileWriter(annotationFile)
+    af.write(JsonProtocol.serialize(circuit.annotations.map(_.toFirrtl)))
+    af.close()
+  }
+
+  /** Output software test Makefrags, which provide targets for integration testing. */
+  def generateTestSuiteMakefrags {
+    addTestSuites
+    writeOutputFile(td, s"$longName.d", TestGeneration.generateMakefrag) // Subsystem-specific test suites
+  }
+
+  def addTestSuites {
+    TestGeneration.addSuite(DefaultTestSuites.groundtest64("p"))
+    TestGeneration.addSuite(DefaultTestSuites.emptyBmarks)
+    TestGeneration.addSuite(DefaultTestSuites.singleRegression)
+  }
+
+  def generateROMs {
+    writeOutputFile(td, s"$longName.rom.conf", enumerateROMs(circuit))
+  }
+
+  /** Output files created as a side-effect of elaboration */
+  def generateArtefacts {
+    ElaborationArtefacts.files.foreach { case (extension, contents) =>
+      writeOutputFile(td, s"$longName.$extension", contents ())
+    }
+  }
 
   def writeOutputFile(targetDir: String, fname: String, contents: String): File = {
     val f = new File(targetDir, fname)
@@ -44,7 +149,6 @@ trait HasRocketChipStageUtils {
     fw.close
     f
   }
-
 }
 
 object ElaborationArtefacts {
