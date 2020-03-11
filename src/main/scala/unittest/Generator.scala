8a3e9d1645b17a875ec3dbb772ea8ebca1f6d9d2
Deborah Soung <debs@sifive.com>
2020-03-11 13:33:29 -0700

    Revert "Revert "separate generator behavior into phases (#2274)""
    This reverts commit eadc4ec7dc9530aac1adb56b60c27eeae76fd320.

--- src/main/scala/unittest/Generator.scala
+++ src/main/scala/unittest/Generator.scala
@@ -2,9 +2,7 @@
 
 package freechips.rocketchip.unittest
 
-object Generator extends freechips.rocketchip.util.GeneratorApp {
-  generateFirrtl
-  generateAnno
-  generateTestSuiteMakefrags // TODO: Needed only for legacy make targets
-  generateArtefacts
-}
+import firrtl.options.StageMain
+import freechips.rocketchip.system.RocketChipStage
+
+object Generator extends StageMain(new RocketChipStage)
