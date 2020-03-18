595e0e946b174ba8a36dc67aea8a8b0748c0c824
Megan Wachs <megan@sifive.com>
2020-03-17 22:06:54 -0700

    Create OMErrorDevice.scala

---
+++ src/main/scala/diplomaticobjectmodel/model/OMErrorDevice.scala
@@ -0,0 +1,11 @@
+// See LICENSE.SiFive for license details.
+
+package freechips.rocketchip.diplomaticobjectmodel.model
+
+
+case class OMErrorDevice(
+  memoryRegions: Seq[OMMemoryRegion],
+  interrupts: Seq[OMInterrupt],
+  specifications: Seq[OMSpecification],
+  _types: Seq[String] = Seq("OMErrorDevice", "OMDevice", "OMComponent", "OMCompoundType")
+) extends OMDevice
