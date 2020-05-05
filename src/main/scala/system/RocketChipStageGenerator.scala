324d7496a6cc77d808e7f88586f76c6186031552
Edward Wang <edward.c.wang@compdigitec.com>
2020-05-05 01:55:34 -0400

    Update to new Dependency API

--- src/main/scala/system/RocketChipStageGenerator.scala
+++ src/main/scala/system/RocketChipStageGenerator.scala
@@ -3,30 +3,29 @@
 package freechips.rocketchip.system
 
 import chisel3.stage.{ChiselCli, ChiselStage}
-import firrtl.options.PhaseManager.PhaseDependency
-import firrtl.options.{Phase, PreservesAll, Shell, StageMain}
+import firrtl.options.{Dependency, Phase, PreservesAll, Shell, StageMain}
 import firrtl.stage.FirrtlCli
 import freechips.rocketchip.stage.RocketChipCli
 
 class RocketChipStage extends ChiselStage with PreservesAll[Phase] {
 
   override val shell = new Shell("rocket-chip") with RocketChipCli with ChiselCli with FirrtlCli
-  override val targets: Seq[PhaseDependency] = Seq(
-    classOf[freechips.rocketchip.stage.phases.Checks],
-    classOf[freechips.rocketchip.stage.phases.TransformAnnotations],
-    classOf[freechips.rocketchip.stage.phases.PreElaboration],
-    classOf[chisel3.stage.phases.Checks],
-    classOf[chisel3.stage.phases.Elaborate],
-    classOf[freechips.rocketchip.stage.phases.GenerateROMs],
-    classOf[chisel3.stage.phases.AddImplicitOutputFile],
-    classOf[chisel3.stage.phases.AddImplicitOutputAnnotationFile],
-    classOf[chisel3.stage.phases.MaybeAspectPhase],
-    classOf[chisel3.stage.phases.Emitter],
-    classOf[chisel3.stage.phases.Convert],
-    classOf[freechips.rocketchip.stage.phases.GenerateFirrtlAnnos],
-    classOf[freechips.rocketchip.stage.phases.AddDefaultTests],
-    classOf[freechips.rocketchip.stage.phases.GenerateTestSuiteMakefrags],
-    classOf[freechips.rocketchip.stage.phases.GenerateArtefacts],
+  override val targets: Seq[Dependency[Phase]] = Seq(
+    Dependency[freechips.rocketchip.stage.phases.Checks],
+    Dependency[freechips.rocketchip.stage.phases.TransformAnnotations],
+    Dependency[freechips.rocketchip.stage.phases.PreElaboration],
+    Dependency[chisel3.stage.phases.Checks],
+    Dependency[chisel3.stage.phases.Elaborate],
+    Dependency[freechips.rocketchip.stage.phases.GenerateROMs],
+    Dependency[chisel3.stage.phases.AddImplicitOutputFile],
+    Dependency[chisel3.stage.phases.AddImplicitOutputAnnotationFile],
+    Dependency[chisel3.stage.phases.MaybeAspectPhase],
+    Dependency[chisel3.stage.phases.Emitter],
+    Dependency[chisel3.stage.phases.Convert],
+    Dependency[freechips.rocketchip.stage.phases.GenerateFirrtlAnnos],
+    Dependency[freechips.rocketchip.stage.phases.AddDefaultTests],
+    Dependency[freechips.rocketchip.stage.phases.GenerateTestSuiteMakefrags],
+    Dependency[freechips.rocketchip.stage.phases.GenerateArtefacts],
   )
 
   // TODO: need a RunPhaseAnnotation to inject phases into ChiselStage
