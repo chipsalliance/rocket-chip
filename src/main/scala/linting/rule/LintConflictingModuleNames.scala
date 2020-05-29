dedc363803b4fc9f5432a8d794539ef2c3f5c67f
Albert Chen <albert.chen@sifive.com>
2020-05-28 17:37:51 -0700

    split LintConflictingModuleNames into two

--- src/main/scala/linting/rule/LintConflictingModuleNames.scala
+++ src/main/scala/linting/rule/LintConflictingModuleNames.scala
@@ -7,7 +7,7 @@ import firrtl._
 import firrtl.annotations.{CircuitTarget, HasSerializationHints, IsModule, SingleTargetAnnotation, Target}
 import firrtl.ir._
 import firrtl.options.{Dependency, HasShellOptions, PreservesAll, ShellOption}
-import firrtl.stage.Forms
+import firrtl.stage.{Forms, RunFirrtlTransformAnnotation}
 import firrtl.transforms.DedupModules
 
 import chisel3.aop.{Aspect, Select}
@@ -175,14 +175,35 @@ case class NamingStrategyAnnotation(
   }
 }
 
-/** Specifies a desired name for a module
+/** Specifies the original desired name for a module
   */
-case class ModuleNameAnnotation(
+case class DesiredNameAnnotation(
   desiredName: String,
   target: IsModule
 ) extends SingleTargetAnnotation[IsModule] {
-  def duplicate(newTarget: IsModule): ModuleNameAnnotation = {
+  def duplicate(newTarget: IsModule): DesiredNameAnnotation = {
     this.copy(target = newTarget)
+  }
+}
+
+/** Specifies the desired name to rename the module to
+  */
+case class OverrideDesiredNameAnnotation(
+  desiredName: String,
+  target: IsModule
+) extends SingleTargetAnnotation[IsModule] {
+  def duplicate(newTarget: IsModule): OverrideDesiredNameAnnotation = {
+    this.copy(target = newTarget)
+  }
+}
+
+/** Collects [[DesireNameAnnotaion]]s for [[LintConflictingModuleNames]] to lint
+  */
+case object LintConflictingModuleNamesAspect extends Aspect[RawModule] {
+  def toAnnotation(top: RawModule): AnnotationSeq = {
+    Select.collectDeep(top) {
+      case m: chisel3.Module => DesiredNameAnnotation(m.desiredName, m.toTarget)
+    }.toSeq
   }
 }
 
@@ -190,14 +211,15 @@ case class ModuleNameAnnotation(
   */
 case object StabilizeNamesAspect extends Aspect[RawModule] {
   def toAnnotation(top: RawModule): AnnotationSeq = {
+    RunFirrtlTransformAnnotation(new RenameDesiredNames) +:
     Select.collectDeep(top) {
       // annotating all Queues with a more descriptive desired name
       case m: Queue[_] => Seq(
-        new ModuleNameAnnotation(s"Queue_${m.genType.getClass.getSimpleName}_entries_${m.entries}", m.toTarget),
+        new OverrideDesiredNameAnnotation(s"Queue_${m.genType.getClass.getSimpleName}_entries_${m.entries}", m.toTarget),
         new NamingStrategyAnnotation(ContentStructureNamingStrategy, m.toTarget)
       )
       case m: TLMonitor => Seq(
-        new ModuleNameAnnotation(m.desiredName, m.toTarget),
+        new OverrideDesiredNameAnnotation(m.desiredName, m.toTarget),
         new NamingStrategyAnnotation(ContentStructureNamingStrategy, m.toTarget)
       )
 
@@ -205,35 +227,99 @@ case object StabilizeNamesAspect extends Aspect[RawModule] {
       case th: TestHarness =>
         val core = th.ldut.rocketTiles.head.module.core
         Seq(
-          ModuleNameAnnotation("Rocket_Core_0", core.toTarget),
+          OverrideDesiredNameAnnotation("Rocket_Core_0", core.toTarget),
           NamingStrategyAnnotation(ExactNamingStrategy, core.toTarget),
         )
     }.flatten.toSeq
   }
 }
 
-/** This LintRule checks for module name collisions and optionally renames them to stable hash names
+/** This LintRule checks for module name collisions
   *
   * Module name collisions occur when different [[Module]]s are annotated with
-  * [[ModuleNameAnnotation]]s that have the same desiredName. By default all
-  * module name conflicts will cause a [[LintViolation]] (using
-  * [[ExactNamingStrategy]]). If one of the modules is targeted by
-  * [[NamingStrategyAnnotation]] then the transform will attempt to rename
-  * modules according to that naming strategy. If the conflicting modules
-  * cannot be disambiguated by the naming strategy then a [[LintViolation]] is
-  * emitted.
+  * [[DesiredNameAnnotation]]s that have the same desiredName. By default all
+  * module name conflicts will cause a [[LintViolation]]. If one of the modules
+  * is targeted by [[NamingStrategyAnnotation]] then the transform will attempt
+  * to rename modules according to that naming strategy. If the conflicting
+  * modules cannot be disambiguated by the naming strategy then a
+  * [[LintViolation]] is emitted.
   */
 final class LintConflictingModuleNames extends LintRule {
   val recommendedFix: String = "override desiredName based on module parameters"
 
   val lintName: String = "conflicting-module-names"
 
-  private val strategyOrder = Seq(
-    ExactNamingStrategy,
-    PortStructureNamingStrategy,
-    ContentStructureNamingStrategy,
-    ContentNamingStrategy,
-  )
+  override def optionalPrerequisites: Seq[Dependency[Transform]] =
+    Dependency[RenameDesiredNames] +: super.optionalPrerequisites
+
+  override def execute(state: CircuitState): CircuitState = {
+    val violations = new Violations()
+
+    val modMap = state.circuit.modules.collect {
+      case m: Module => m.name -> m
+    }.toMap
+
+    val desiredNameAnnos = state.annotations.collect {
+      case a: DesiredNameAnnotation if a.target.circuit == state.circuit.main => a
+    }
+
+    val moduleToDesiredName: mutable.Map[String, mutable.Set[String]] = mutable.Map()
+
+    val nameMap = desiredNameAnnos.groupBy(_.desiredName).mapValues { annos =>
+      annos.map(a => Target.referringModule(a.target).module).distinct.map { referringModule =>
+        require(modMap.contains(referringModule), "ModuleNameAnnotations may not refer to blackboxes")
+        val desiredNames = moduleToDesiredName.getOrElseUpdate(referringModule, mutable.Set())
+        desiredNames += annos.head.desiredName
+        modMap(referringModule)
+      }
+    }
+
+    val conflictingDesiredNames = moduleToDesiredName.collect {
+      case kv@ (moduleName, desiredName) if desiredName.size > 1 => kv
+    }
+
+    require(conflictingDesiredNames.size == 0, {
+      val explanation = conflictingDesiredNames.map {
+        case (modName, desiredNames) => s"  ${modName}: ${desiredNames.mkString(", ")}"
+      }.mkString("\n")
+      s"Modules may not have more than one desiredName:\n${explanation}"
+    })
+
+    nameMap.foreach {
+      case (desiredName, modules) if modules.size > 1 =>
+        val msg = s"Module conflicts for desired name $desiredName: ${modules.map(_.name).mkString(", ")}"
+        val info = MultiInfo(modules.map(_.info))
+        val mods = violations.getOrElse((info, msg), Set.empty)
+        violations((info, msg)) = mods ++ modules.map(_.name)
+      case _ =>
+    }
+
+    val whitelist = collectWhitelist(state.annotations)
+    val errorList = violations.collect {
+      case ((info, message), mods) if !isWhitelisted(info, whitelist) => Violation(this, info, message, mods)
+    }.toSeq.sortBy { _.toString }
+    val newAnnos = errorList ++ state.annotations
+
+    state.copy(annotations = newAnnos)
+  }
+}
+
+/** Renames modules based on their overrided desired names
+  *
+  * Desired module name overrides are specified by
+  * [[OverrideDesiredNameAnnotation]]. If one of the modules is targeted by a
+  * [[NamingStrategyAnnotation]] then the transform will attempt to rename
+  * modules according to that naming strategy. If the conflicting modules
+  * cannot be disambiguated by the naming strategy then a [[LintViolation]] is
+  * emitted. The default naming strategy is [[ExactNamingStrategy]] which will
+  * throw an exception if there are any module name conflicts.
+  */
+class RenameDesiredNames extends Transform with DependencyAPIMigration {
+
+  override def prerequisites = Seq(Dependency[DedupModules])
+  override def optionalPrerequisiteOf = Seq(Dependency[VerilogEmitter])
+
+  override def invalidates(transform: Transform) = false
 
   private def checkStrategy(
     strategy: NamingStrategy,
@@ -244,27 +330,7 @@ final class LintConflictingModuleNames extends LintRule {
     if (nameMap.values.toSet.size == modules.size) Some(nameMap) else None
   }
 
-  private def pickStrategies(
-    violations: Violations,
-    strategies: Seq[NamingStrategy],
-    desiredName: String,
-    modules: Seq[Module]): Map[String, String] = {
-    val result = strategies.foldLeft(None: Option[Map[String, String]]) {
-      case (None, strategy) => checkStrategy(strategy, desiredName, modules)
-      case (some, _) => some
-    }
-    if (result.isDefined) {
-      result.get
-    } else {
-      val msg = s"No naming strategy resolves module collisions for desired name: $desiredName"
-      val info = MultiInfo(modules.map(_.info))
-      val mods = violations.getOrElse((info, msg), Set.empty)
-      violations((info, msg)) = mods ++ modules.map(_.name)
-      Map.empty
-    }
-  }
-
-  override def execute(state: CircuitState): CircuitState = {
+  def execute(state: CircuitState): CircuitState = {
     val violations = new Violations()
 
     val modMap = state.circuit.modules.collect {
@@ -285,13 +351,13 @@ final class LintConflictingModuleNames extends LintRule {
       module -> strategies.head
     }
 
-    val moduleNameAnnos = state.annotations.collect {
-      case a: ModuleNameAnnotation if a.target.circuit == state.circuit.main => a
+    val overrideDesiredNameAnnos = state.annotations.collect {
+      case a: OverrideDesiredNameAnnotation if a.target.circuit == state.circuit.main => a
     }
 
     val moduleToDesiredName: mutable.Map[String, mutable.Set[String]] = mutable.Map()
 
-    val nameMap = moduleNameAnnos.groupBy(_.desiredName).mapValues { annos =>
+    val nameMap = overrideDesiredNameAnnos.groupBy(_.desiredName).mapValues { annos =>
       annos.map(a => Target.referringModule(a.target).module).distinct.map { referringModule =>
         require(modMap.contains(referringModule), "ModuleNameAnnotations may not refer to blackboxes")
         val desiredNames = moduleToDesiredName.getOrElseUpdate(referringModule, mutable.Set())
@@ -311,21 +377,17 @@ final class LintConflictingModuleNames extends LintRule {
       s"Modules may not have more than one desiredName:\n${explanation}"
     })
 
-    val nameMappings = nameMap.map { case (desiredName, modules) =>
+    val renamedDesiredNames = mutable.Set[String]()
+    val nameMappings = nameMap.flatMap { case (desiredName, modules) =>
       val strategyOpt = modules.collectFirst {
         case m if strategyMap.contains(m.name) => strategyMap(m.name)
       }
       val strategy = strategyOpt.getOrElse(ExactNamingStrategy)
       val result = checkStrategy(strategy, desiredName, modules)
       if (result.isDefined) {
-        result.get
-      } else {
-        val msg = s"Requested naming strategy $strategy does not resolve module collisions for desired name: $desiredName"
-        val info = MultiInfo(modules.map(_.info))
-        val mods = violations.getOrElse((info, msg), Set.empty)
-        violations((info, msg)) = mods ++ modules.map(_.name)
-        Map.empty[String, String]
+        renamedDesiredNames += desiredName
       }
+      result
     }.flatten.toMap
 
     val circuit = RenameModules(nameMappings, state.circuit)
@@ -333,15 +395,14 @@ final class LintConflictingModuleNames extends LintRule {
     val newMain = CircuitTarget(circuit.main)
     val oldMain = CircuitTarget(state.circuit.main)
     val renames = RenameMap()
-    nameMappings.foreach { case (from, to) =>
-      renames.record(oldMain.module(from), newMain.module(to))
-    }
 
-    val whitelist = collectWhitelist(state.annotations)
-    val errorList = violations.collect {
-      case ((info, message), mods) if !isWhitelisted(info, whitelist) => Violation(this, info, message, mods)
-    }.toSeq.sortBy { _.toString }
-    val newAnnos = errorList ++ state.annotations
+    // delete override annotations and rename desired name annotaions for ones that were renamed
+    val newAnnos = state.annotations.flatMap {
+      case a: OverrideDesiredNameAnnotation if renamedDesiredNames(a.desiredName) => None
+      case a: DesiredNameAnnotation if nameMappings.contains(a.desiredName) =>
+        Some(a.copy(desiredName = nameMappings(a.desiredName)))
+      case a => Some(a)
+    }
 
     state.copy(circuit = circuit, annotations = newAnnos, renames = Some(renames))
   }
