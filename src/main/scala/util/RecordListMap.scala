5af6a5aac8543ff65eff2f418de1d634b479f6c8
Megan Wachs <megan@sifive.com>
2020-05-31 15:25:39 -0700

    Create RecordListMap to get signals with better names that HeterogenousBag

---
+++ src/main/scala/util/RecordListMap.scala
@@ -0,0 +1,26 @@
+// See LICENSE.SiFive for license details.
+
+package freechips.rocketchip.util
+
+import Chisel._
+import chisel3.Record
+import scala.collection.immutable.ListMap
+
+final case class RecordListMap[T <: Data](eltMap: ListMap[String, T])
+    extends Record with collection.Map[String, T] {
+
+  // Required by collection.Map[String, T]
+  def get(key: String) = eltMap.get(key)
+  def -(key: String) = eltMap - key
+  def +[V1 >: T](kv: (String, V1)) = eltMap + kv
+  def iterator = eltMap.iterator
+
+  // Required by Record
+  val elements = eltMap
+
+  override def cloneType: this.type = (new RecordMap(eltMap.map{case (k, v) => k -> v.chiselCloneType})).asInstanceOf[this.type]
+
+  // Map has its own hashCode/equals that we must not use
+  override def hashCode: Int = super[Record].hashCode
+  override def equals(that: Any): Boolean = super[Record].equals(that)
+}
