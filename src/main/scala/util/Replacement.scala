e9d48823b0c1aec7443505c3fad5222ccf4d1ce5
John Ingalls <john.ingalls@sifive.com>
2020-06-01 16:24:20 -0700

    PLRU handle power-of-2-plus-1 number of ways (for example: 5)

--- src/main/scala/util/Replacement.scala
+++ src/main/scala/util/Replacement.scala
@@ -179,37 +179,33 @@ class PseudoLRU(n_ways: Int) extends ReplacementPolicy {
     require(state.getWidth == (tree_nways-1),                   s"wrong state bits width ${state.getWidth} for $tree_nways ways")
     require(touch_way.getWidth == (log2Ceil(tree_nways) max 1), s"wrong encoded way width ${touch_way.getWidth} for $tree_nways ways")
 
-    if (tree_nways > 3) {
-      // we are at a branching node in the tree with both left and right sub-trees, so recurse both sub-trees
+    if (tree_nways > 2) {
+      // we are at a branching node in the tree, so recurse
       val right_nways: Int = 1 << (log2Ceil(tree_nways) - 1)  // number of ways in the right sub-tree
       val left_nways:  Int = tree_nways - right_nways         // number of ways in the left sub-tree
       val set_left_older      = !touch_way(log2Ceil(tree_nways)-1)
-      val left_subtree_state  = state(tree_nways-3, right_nways-1)
+      val left_subtree_state  = state.extract(tree_nways-3, right_nways-1)
       val right_subtree_state = state(right_nways-2, 0)
 
-      Cat(set_left_older,
-          Mux(set_left_older,
-              left_subtree_state,  // if setting left sub-tree as older, do NOT recurse into left sub-tree
-              get_next_state(left_subtree_state, touch_way(log2Ceil(left_nways)-1,0), left_nways)),  // recurse left if newer
-          Mux(set_left_older,
-              get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways)-1,0), right_nways),  // recurse right if newer
-              right_subtree_state))  // if setting right sub-tree as older, do NOT recurse into right sub-tree
-
-    } else if (tree_nways == 3) {
-      // we are at a branching node in the tree with only a right sub-tree, so recurse only right sub-tree
-      val right_nways: Int = 1 << (log2Ceil(tree_nways) - 1)  // number of ways in the right sub-tree
-      val set_left_older      = !touch_way(log2Ceil(tree_nways)-1)
-      val right_subtree_state = state(right_nways-2, 0)
-
-      Cat(set_left_older,
-          Mux(set_left_older,
-              get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways)-1,0), right_nways),  // recurse right if newer
-              right_subtree_state))  // if setting right sub-tree as older, do NOT recurse into right sub-tree
-
+      if (left_nways > 1) {
+        // we are at a branching node in the tree with both left and right sub-trees, so recurse both sub-trees
+        Cat(set_left_older,
+            Mux(set_left_older,
+                left_subtree_state,  // if setting left sub-tree as older, do NOT recurse into left sub-tree
+                get_next_state(left_subtree_state, touch_way.extract(log2Ceil(left_nways)-1,0), left_nways)),  // recurse left if newer
+            Mux(set_left_older,
+                get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways)-1,0), right_nways),  // recurse right if newer
+                right_subtree_state))  // if setting right sub-tree as older, do NOT recurse into right sub-tree
+      } else {
+        // we are at a branching node in the tree with only a right sub-tree, so recurse only right sub-tree
+        Cat(set_left_older,
+            Mux(set_left_older,
+                get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways)-1,0), right_nways),  // recurse right if newer
+                right_subtree_state))  // if setting right sub-tree as older, do NOT recurse into right sub-tree
+      }
     } else if (tree_nways == 2) {
       // we are at a leaf node at the end of the tree, so set the single state bit opposite of the lsb of the touched way encoded value
       !touch_way(0)
-
     } else {  // tree_nways <= 1
       // we are at an empty node in an empty tree for 1 way, so return single zero bit for Chisel (no zero-width wires)
       0.U(1.W)
@@ -230,34 +226,30 @@ class PseudoLRU(n_ways: Int) extends ReplacementPolicy {
     require(state.getWidth == (tree_nways-1), s"wrong state bits width ${state.getWidth} for $tree_nways ways")
 
     // this algorithm recursively descends the binary tree, filling in the way-to-replace encoded value from msb to lsb
-    if (tree_nways > 3) {
-      // we are at a branching node in the tree with both left and right sub-trees, so recurse both sub-trees
+    if (tree_nways > 2) {
+      // we are at a branching node in the tree, so recurse
       val right_nways: Int = 1 << (log2Ceil(tree_nways) - 1)  // number of ways in the right sub-tree
       val left_nways:  Int = tree_nways - right_nways         // number of ways in the left sub-tree
       val left_subtree_older  = state(tree_nways-2)
-      val left_subtree_state  = state(tree_nways-3, right_nways-1)
+      val left_subtree_state  = state.extract(tree_nways-3, right_nways-1)
       val right_subtree_state = state(right_nways-2, 0)
 
-      Cat(left_subtree_older,      // return the top state bit (current tree node) as msb of the way-to-replace encoded value
-          Mux(left_subtree_older,  // if left sub-tree is older, recurse left, else recurse right
-              get_replace_way(left_subtree_state,  left_nways),    // recurse left
-              get_replace_way(right_subtree_state, right_nways)))  // recurse right
-
-    } else if (tree_nways == 3) {
-      // we are at a branching node in the tree with only a right sub-tree, so recurse only right sub-tree
-      val right_nways: Int = 1 << (log2Ceil(tree_nways) - 1)  // number of ways in the right sub-tree
-      val left_subtree_older  = state(tree_nways-2)
-      val right_subtree_state = state(right_nways-2, 0)
-
-      Cat(left_subtree_older,      // return the top state bit (current tree node) as msb of the way-to-replace encoded value
-          Mux(left_subtree_older,  // if left sub-tree is older, return and do not recurse right
-              0.U(1.W),
-              get_replace_way(right_subtree_state, right_nways)))  // recurse right
-
+      if (left_nways > 1) {
+        // we are at a branching node in the tree with both left and right sub-trees, so recurse both sub-trees
+        Cat(left_subtree_older,      // return the top state bit (current tree node) as msb of the way-to-replace encoded value
+            Mux(left_subtree_older,  // if left sub-tree is older, recurse left, else recurse right
+                get_replace_way(left_subtree_state,  left_nways),    // recurse left
+                get_replace_way(right_subtree_state, right_nways)))  // recurse right
+      } else {
+        // we are at a branching node in the tree with only a right sub-tree, so recurse only right sub-tree
+        Cat(left_subtree_older,      // return the top state bit (current tree node) as msb of the way-to-replace encoded value
+            Mux(left_subtree_older,  // if left sub-tree is older, return and do not recurse right
+                0.U(1.W),
+                get_replace_way(right_subtree_state, right_nways)))  // recurse right
+      }
     } else if (tree_nways == 2) {
       // we are at a leaf node at the end of the tree, so just return the single state bit as lsb of the way-to-replace encoded value
       state(0)
-
     } else {  // tree_nways <= 1
       // we are at an empty node in an unbalanced tree for non-power-of-2 ways, so return single zero bit as lsb of the way-to-replace encoded value
       0.U(1.W)
