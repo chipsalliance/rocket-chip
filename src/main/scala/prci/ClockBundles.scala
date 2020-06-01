c15cf1f6e1d9fda6f4c4a72b40676785afd8cefe
Megan Wachs <megan@sifive.com>
2020-06-01 02:44:02 -0700

    PRCI: Use RecordListMap.  This compiles but not sure about the foo  in there...

--- src/main/scala/prci/ClockBundles.scala
+++ src/main/scala/prci/ClockBundles.scala
@@ -2,7 +2,9 @@
 package freechips.rocketchip.prci
 
 import chisel3._
-import freechips.rocketchip.util.HeterogeneousBag
+import freechips.rocketchip.util.RecordListMap
+import scala.collection.immutable.ListMap
+
 
 class ClockBundle(val params: ClockBundleParameters) extends Bundle
 {
@@ -10,7 +12,10 @@ class ClockBundle(val params: ClockBundleParameters) extends Bundle
   val reset = Reset()
 }
 
-class ClockGroupBundle(val params: ClockGroupBundleParameters, val names: Option[Seq[String]] = None) extends Bundle
+class ClockGroupBundle(val params: ClockGroupBundleParameters) extends Bundle
 {
-  val member = HeterogeneousBag(params.members.map(p => new ClockBundle(p)), names)
+  val member: RecordListMap[ClockBundle] = {
+    val nameToBundleMap = ListMap(params.members.map{p => p.name -> new ClockBundle(p)}: _*)
+    new RecordListMap(nameToBundleMap)
+  }
 }
