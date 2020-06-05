5d70580b20290da3579c81c12a16afa8279d2a00
Albert Chen <albert.chen@sifive.com>
2020-06-05 13:39:22 -0700

    RenameDesireNames: remove naming strategy

--- src/main/scala/transforms/naming/RenameDesiredNames.scala
+++ src/main/scala/transforms/naming/RenameDesiredNames.scala
@@ -36,41 +36,6 @@ object RenameModules {
   }
 }
 
-/** A strategy for generating new names for modules
-  */
-sealed trait NamingStrategy {
-
-  /** Generates a new stable module name based on the desired name and the module IR node
-    *
-    * called by [[RenameDesiredNames]] to rename modules
-    *
-    * @param desiredName the requested name for the module; generated name should contain this name
-    * @param module the module targeted by the desiredName
-    */
-  def getName(desiredName: String)(module: Module): String
-}
-
-/** A naming strategy that always returns the desired name
-  */
-case object ExactNamingStrategy extends NamingStrategy {
-  def getName(desiredName: String)(module: Module): String = desiredName
-}
-
-/** Specifies a naming strategy to use for a module and the modules that it collides with
-  */
-case class NamingStrategyAnnotation(
-  strategy: NamingStrategy,
-  target: IsModule
-) extends SingleTargetAnnotation[IsModule] with HasSerializationHints {
-  def typeHints: Seq[Class[_]] = Seq(
-    ExactNamingStrategy.getClass
-  )
-
-  def duplicate(newTarget: IsModule): NamingStrategyAnnotation = {
-    this.copy(target = newTarget)
-  }
-}
-
 /** Specifies the desired name to rename the module to, overriding the Module.desiredName
   */
 case class OverrideDesiredNameAnnotation(
@@ -85,10 +50,7 @@ case class OverrideDesiredNameAnnotation(
 /** Renames modules based on their overridden desired names
   *
   * Desired module name overrides are specified by
-  * [[OverrideDesiredNameAnnotation]]. If one of the modules is targeted by a
-  * [[NamingStrategyAnnotation]] then the transform will attempt to rename
-  * modules according to that naming strategy. The default naming strategy is
-  * [[ExactNamingStrategy]].
+  * [[OverrideDesiredNameAnnotation]].
   */
 class RenameDesiredNames extends Transform with DependencyAPIMigration {
 
@@ -97,39 +59,10 @@ class RenameDesiredNames extends Transform with DependencyAPIMigration {
 
   override def invalidates(transform: Transform) = false
 
-  /* Creates a rename mapping using the selected [[NamingStrategy]]
-   *
-   * @return a map of original module name to new name if possible, None if the
-   * strategy fails to result in unique names for all the modules using this
-   * strategy
-   */
-  private def checkStrategy(
-    strategy: NamingStrategy,
-    desiredName: String,
-    modules: Seq[Module]): Option[Map[String, String]] = {
-    val fn = strategy.getName(desiredName)(_)
-    val nameMap = modules.map(m => m.name -> fn(m)).toMap
-    if (nameMap.values.toSet.size == modules.size) Some(nameMap) else None
-  }
-
   def execute(state: CircuitState): CircuitState = {
     val modMap = state.circuit.modules.collect {
       case m: Module => m.name -> m
     }.toMap
-
-    val namingStrategyAnnos = state.annotations.collect {
-      case a: NamingStrategyAnnotation if a.target.circuit == state.circuit.main => a
-    }
-
-    val strategyMap = namingStrategyAnnos.groupBy { a =>
-      val referringModule = Target.referringModule(a.target).module
-      require(modMap.contains(referringModule), "NamingStrategyAnnotation may not refer to blackboxes")
-      referringModule
-    }.map { case (module, annos) =>
-      val strategies = annos.map(_.strategy).distinct
-      require(strategies.size == 1, s"Conflicting naming strategies for module $module: ${strategies.mkString(", ")}")
-      module -> strategies.head
-    }
 
     val overrideDesiredNameAnnos = state.annotations.collect {
       case a: OverrideDesiredNameAnnotation if a.target.circuit == state.circuit.main => a
@@ -159,16 +92,13 @@ class RenameDesiredNames extends Transform with DependencyAPIMigration {
 
     val renamedDesiredNames = mutable.Set[String]()
     val nameMappings = nameMap.flatMap { case (desiredName, modules) =>
-      val strategyOpt = modules.collectFirst {
-        case m if strategyMap.contains(m.name) => strategyMap(m.name)
-      }
-      val strategy = strategyOpt.getOrElse(ExactNamingStrategy)
-      val result = checkStrategy(strategy, desiredName, modules)
-      if (result.isDefined) {
+      if (modules.size == 1) {
         renamedDesiredNames += desiredName
+        Some(modules.head.name -> desiredName)
+      } else {
+        None
       }
-      result
-    }.flatten.toMap
+    }.toMap
 
     val finalNames = state.circuit.modules.map {
       case m: Module if nameMappings.contains(m.name) => nameMappings(m.name)
