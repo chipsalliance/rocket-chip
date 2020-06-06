0c299c745655d8bae94f2309c97313b86265f166
Megan Wachs <megan@sifive.com>
2020-06-05 21:08:59 -0700

    RecordMap: review  feedback

--- src/main/scala/util/RecordMap.scala
+++ src/main/scala/util/RecordMap.scala
@@ -5,23 +5,19 @@ package freechips.rocketchip.util
 import Chisel._
 import chisel3.Record
 import scala.collection.immutable.ListMap
+import chisel3.internal.requireIsChiselType
+import chisel3.experimental.DataMirror.internal.chiselTypeClone
 
-final case class RecordMap[T <: Data](eltMap: ListMap[String, T])
-    extends Record with collection.Seq[T] {
+final class RecordMap[T <: Data](eltMap: ListMap[String, T])
+    extends Record {
+  
+  eltMap.foreach { case (name, elt) => requireIsChiselType(elt, name) }
 
-  // Required by Seq
+  val elements = ListMap() ++ eltMap.mapValues(chiselTypeClone) // mapValues return value is lazy
+
+  override def cloneType: this.type = (new RecordMap(eltMap)).asInstanceOf[this.type]
+
   def apply(x: Int) = eltMap.values.toSeq(x)
-  def length = eltMap.size
-  def iterator = eltMap.values.iterator
-
   def apply(x: String) = eltMap.get(x)
 
-  // Required by Record
-  val elements = eltMap
-
-  override def cloneType: this.type = (new RecordMap(eltMap.map{case (k, v) => k -> v.chiselCloneType})).asInstanceOf[this.type]
-
-  // Map has its own hashCode/equals that we must not use
-  override def hashCode: Int = super[Record].hashCode
-  override def equals(that: Any): Boolean = super[Record].equals(that)
 }
