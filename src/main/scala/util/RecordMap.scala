af5fa3b3badfb5191284327bf6e8d1b619b2c01c
Megan Wachs <megan@sifive.com>
2020-06-06 06:20:54 -0700

    RecordMap: Add some convenient accessor functions, make eltMap publich

--- src/main/scala/util/RecordMap.scala
+++ src/main/scala/util/RecordMap.scala
@@ -2,23 +2,26 @@
 
 package freechips.rocketchip.util
 
-import Chisel._
-import chisel3.Record
+import chisel3._
 import scala.collection.immutable.ListMap
 import chisel3.internal.requireIsChiselType
 import chisel3.experimental.DataMirror.internal.chiselTypeClone
 
-final class RecordMap[T <: Data](eltMap: ListMap[String, T])
+final class RecordMap[T <: Data](val eltMap: ListMap[String, T])
     extends Record {
   
   eltMap.foreach { case (name, elt) => requireIsChiselType(elt, name) }
 
-  val elements = ListMap() ++ eltMap.mapValues(chiselTypeClone) // mapValues return value is lazy
-
-  override def cloneType: this.type = (new RecordMap(eltMap)).asInstanceOf[this.type]
 
   def apply(x: Int) = eltMap.values.toSeq(x)
   def apply(x: String) = eltMap.get(x)
+  def size = eltMap.size
+  def data = eltMap.values
+
+  // This is needed for Record, and doesn't give the actual elements
+  val elements = ListMap[String, T]() ++ eltMap.mapValues(chiselTypeClone(_))  // mapValues return value is lazy
+
+  override def cloneType: this.type = (new RecordMap(eltMap)).asInstanceOf[this.type]
 
 }
 
@@ -27,6 +30,6 @@ object RecordMap {
   def apply[T <: Data](eltMap: ListMap[String, T]) = new RecordMap(eltMap)
 
   def apply[T <: Data](elements: (String, T)*) {
-    new RecordMap[T](ListMap(elements:_*))
+    new RecordMap[T](ListMap[String, T](elements:_*))
   }
 }
