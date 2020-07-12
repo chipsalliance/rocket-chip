5e116dd4288077b62daa5574b80fa841db9f711c
Bradley Morrell <brrmorre@gmail.com>
2020-07-11 17:06:36 -0700

    added hellacache parameters

--- src/main/scala/tilelink/Parameters.scala
+++ src/main/scala/tilelink/Parameters.scala
@@ -1051,7 +1051,7 @@ object TLMasterParameters {
       sourceId          = sourceId)
   }
 }
-  
+
 object TLClientParameters {
   @deprecated("Use TLMasterParameters.v1 instead of TLClientParameters","")
   def apply(
