4d257ffe25816e6d98ac6151ea9523d80d85ac3b
John Ingalls <john.ingalls@sifive.com>
2020-07-15 14:58:11 -0700

    plusarg default bit width

--- src/main/resources/vsrc/plusarg_reader.v
+++ src/main/resources/vsrc/plusarg_reader.v
@@ -4,7 +4,7 @@
 
 // No default parameter values are intended, nor does IEEE 1800-2012 require them (clause A.2.4 param_assignment),
 // but Incisive demands them. These default values should never be used.
-module plusarg_reader #(parameter FORMAT="borked=%d", DEFAULT=0, WIDTH=1) (
+module plusarg_reader #(parameter FORMAT="borked=%d", WIDTH=1, bit [WIDTH-1:0] DEFAULT=0) (
    output [WIDTH-1:0] out
 );
 
