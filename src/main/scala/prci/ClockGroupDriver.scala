6435f2ab02bb712be467ab979442a8a701863b5f
Henry Cook <henry@sifive.com>
2020-07-15 17:30:21 -0700

    prci: ClockGroupDriver uses RecordMap

--- src/main/scala/prci/ClockGroupDriver.scala
+++ src/main/scala/prci/ClockGroupDriver.scala
@@ -2,36 +2,46 @@
 package freechips.rocketchip.prci
 
 import chisel3._
+import chisel3.experimental.IO
 import freechips.rocketchip.config.Parameters
 import freechips.rocketchip.diplomacy.{InModuleBody, ModuleValue, ValName}
-import freechips.rocketchip.util.{HeterogeneousBag}
+import freechips.rocketchip.util.{RecordMap}
 
 /** Used to parameterize the creation of simple clock group drivers */
 case class ClockGroupDriverParameters(
   num: Int = 1,
   driveFn: ClockGroupDriver.DriveFn = ClockGroupDriver.driveFromImplicitClock
 ) {
-  def drive(node: ClockGroupEphemeralNode)(implicit p: Parameters, vn: ValName): ModuleValue[HeterogeneousBag[ClockGroupBundle]] = {
+  def drive(node: ClockGroupEphemeralNode)(implicit p: Parameters, vn: ValName): ModuleValue[RecordMap[ClockBundle]] = {
     driveFn(node, num, p, vn)
   }
 }
 
 object ClockGroupDriver {
-  type DriveFn = (ClockGroupEphemeralNode, Int, Parameters, ValName) => ModuleValue[HeterogeneousBag[ClockGroupBundle]]
+  type DriveFn = (ClockGroupEphemeralNode, Int, Parameters, ValName) => ModuleValue[RecordMap[ClockBundle]]
 
   /** Drive all members of all groups from the Chisel implicit clock */
   def driveFromImplicitClock: DriveFn = { (groups, num, p, vn) =>
     implicit val pp = p
     val dummyClockGroupSourceNode: ClockGroupSourceNode = SimpleClockGroupSource(num)
     groups :*= dummyClockGroupSourceNode
-    InModuleBody { HeterogeneousBag[ClockGroupBundle](Nil) }
+    InModuleBody { RecordMap[ClockBundle]() }
   }
 
-  /** Drive all members of all groups from cloned IOs */
+  /** Drive all members of all groups from a flattened IO representation */
   def driveFromIOs: DriveFn = { (groups, num, p, vn) =>
     implicit val pp = p
     val ioClockGroupSourceNode = ClockGroupSourceNode(List.fill(num) { ClockGroupSourceParameters() })
     groups :*= ioClockGroupSourceNode
-    InModuleBody { ioClockGroupSourceNode.makeIOs()(vn) }
+    InModuleBody {
+      val bundles = ioClockGroupSourceNode.out.map(_._1)
+      val elements =  bundles.map(_.member.elements).flatten
+      val io = IO(Flipped(RecordMap(elements.map { case (name, data) =>
+        name -> data.cloneType
+      }:_*)))
+
+      elements.foreach { case (name, data) => io(name).foreach { data := _ } }
+      io.suggestName(vn.name)
+    }
   }
 }
