a14034a098d5a56c6903e0e7c8082873d3278f85
Wesley W. Terpstra <wesley@sifive.com>
2020-07-31 12:57:21 -0700

    SRAM: repipeline the TLRAM into a 3 cycle RMW state machine
    Here is a picture of the change to the pipeline:
      https://app.lucidchart.com/invitations/accept/da44b89c-a93c-45e9-ba5e-cb6f9140d84e
    
    Compared to the old pipeline, occupancy is increased from 2 cycles to 3 for:
     - atomics
     - sub-ECC-granularity writes
     - repaired ECC values
    
    In exchange for this occupancy increase, a new register (REG) was added:
      sram data output => *REG* => ecc-correction => ALU => sram write setup
    This path was sufficiently long that it limited fMAX on many designs.
    In designs without ECC and without atomics, this pipeline is optimized away.
    
    Compared to the old pipeline, response latency is unchanged (by default) for:
     - reads   (1)
     - atomics (1)
     - writes  (1)
     - ECC-repaired reads   (2)
     - ECC-repaired atomics (2)
    
    Added a knob (sramReg) to set latency for all operations to 2.
    With this knob disabled (the default), as in the old pipeline:
      - output data can flow uncorrected from the SRAM
      - output valid depends on correct ECC decode of SRAM output
    With the knob enabled:
      - data flows from an ECC correction fed by registers
      - valid is a register

--- src/main/scala/unittest/Configs.scala
+++ src/main/scala/unittest/Configs.scala
@@ -44,9 +44,9 @@ class WithTLSimpleUnitTests extends Config((site, here, up) => {
     val txns = 100 * site(TestDurationMultiplier)
     val timeout = 50000 * site(TestDurationMultiplier)
     Seq(
-      Module(new TLRAMSimpleTest(1,        txns=15*txns, timeout=timeout)),
-      Module(new TLRAMSimpleTest(4,        txns=15*txns, timeout=timeout)),
-      Module(new TLRAMSimpleTest(16,       txns=15*txns, timeout=timeout)),
+      Module(new TLRAMSimpleTest(1,  true, txns=15*txns, timeout=timeout)),
+      Module(new TLRAMSimpleTest(4,  false,txns=15*txns, timeout=timeout)),
+      Module(new TLRAMSimpleTest(16, true, txns=15*txns, timeout=timeout)),
       Module(new TLRAMZeroDelayTest(4,     txns=15*txns, timeout=timeout)),
       Module(new TLRAMHintHandlerTest(     txns=15*txns, timeout=timeout)),
       Module(new TLFuzzRAMTest(            txns= 3*txns, timeout=timeout)),
@@ -56,9 +56,12 @@ class WithTLSimpleUnitTests extends Config((site, here, up) => {
       Module(new TLRAMAsyncCrossingTest(   txns= 5*txns, timeout=timeout)),
       Module(new TLRAMCreditedCrossingTest(txns= 5*txns, timeout=timeout)),
       Module(new TLRAMAtomicAutomataTest(  txns=10*txns, timeout=timeout)),
-      Module(new TLRAMECCTest(8, 4,        txns=15*txns, timeout=timeout)),
-      Module(new TLRAMECCTest(4, 1,        txns=15*txns, timeout=timeout)),
-      Module(new TLRAMECCTest(1, 1,        txns=15*txns, timeout=timeout)) ) }
+      Module(new TLRAMECCTest(8, 4, true,  txns=15*txns, timeout=timeout)),
+      Module(new TLRAMECCTest(4, 1, true,  txns=15*txns, timeout=timeout)),
+      Module(new TLRAMECCTest(1, 1, true,  txns=15*txns, timeout=timeout)),
+      Module(new TLRAMECCTest(8, 4, false, txns=15*txns, timeout=timeout)),
+      Module(new TLRAMECCTest(4, 1, false, txns=15*txns, timeout=timeout)),
+      Module(new TLRAMECCTest(1, 1, false, txns=15*txns, timeout=timeout)) ) }
 })
 
 class WithTLWidthUnitTests extends Config((site, here, up) => {
