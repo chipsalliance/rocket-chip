9aae2881a26c7f5b79b6a2c4a445ea55b7efe1d7
Henry Cook <henry@sifive.com>
2020-09-14 15:08:04 -0700

    block during reset supports blocking for a stretched amount of cycles

--- src/main/scala/interrupts/BlockDuringReset.scala
+++ src/main/scala/interrupts/BlockDuringReset.scala
@@ -8,20 +8,22 @@ import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.util.BlockDuringReset
 
 /** BlockDuringReset ensures that no interrupt is raised while reset is raised. */
-class IntBlockDuringReset()(implicit p: Parameters) extends LazyModule
+class IntBlockDuringReset(stretchResetCycles: Int = 0)(implicit p: Parameters) extends LazyModule
 {
   val intnode = IntAdapterNode()
   override def shouldBeInlined = true
   lazy val module = new LazyModuleImp(this) {
     intnode.in.zip(intnode.out).foreach { case ((in, _), (out, _)) =>
-      in.zip(out).foreach { case (i, o) => o := BlockDuringReset(i) }
+      in.zip(out).foreach { case (i, o) => o := BlockDuringReset(i, stretchResetCycles) }
     }
   }
 }
 
 object IntBlockDuringReset {
-  def apply()(implicit p: Parameters): IntNode = {
-    val block_during_reset = LazyModule(new IntBlockDuringReset)
-    block_during_reset.intnode
+  def apply(shouldBlockAndStretch: Option[Int] = Some(0))(implicit p: Parameters): IntNode = {
+    shouldBlockAndStretch.map { c =>
+      val block_during_reset = LazyModule(new IntBlockDuringReset(c))
+      block_during_reset.intnode
+    } .getOrElse(IntTempNode())
   }
 }
--- src/main/scala/util/BlockDuringReset.scala
+++ src/main/scala/util/BlockDuringReset.scala
@@ -3,13 +3,18 @@
 package freechips.rocketchip.util
 
 import chisel3._
-import chisel3.util.DecoupledIO
+import chisel3.util.{Counter, DecoupledIO}
 
 /** Blocks transactions until the cycle after reset. */
 object BlockDuringReset
 {
-  def apply[T <: Data](enq: DecoupledIO[T]): DecoupledIO[T] = {
-    val out_of_reset = RegNext(true.B, false.B)
+  private def outOfReset(stretchCycles: Int): Bool = stretchCycles match {
+    case 0 => RegNext(true.B, false.B)
+    case i => Counter(true.B, i)._2
+  }
+
+  def apply[T <: Data](enq: DecoupledIO[T], stretchCycles: Int = 0): DecoupledIO[T] = {
+    val out_of_reset = outOfReset(stretchCycles)
     val res = Wire(enq.cloneType)
     res.valid := enq.valid
     enq.ready := res.ready
@@ -21,21 +26,21 @@ object BlockDuringReset
     res
   }
 
-  def apply(valid: Bool): Bool = {
-    val out_of_reset = RegNext(true.B, false.B)
+  def apply(valid: Bool, stretchCycles: Int): Bool = {
+    val out_of_reset = outOfReset(stretchCycles)
     valid && out_of_reset
   }
 
-  def apply[T <: DataCanBeValid](data: T): T = {
-    val out_of_reset = RegNext(true.B, false.B)
+  def apply[T <: DataCanBeValid](data: T, stretchCycles: Int): T = {
+    val out_of_reset = outOfReset(stretchCycles)
     val res = Wire(data.cloneType)
     res := data
     res.valid := data.valid && out_of_reset
     res
   }
 
-  def apply[T <: DataCanBeValid](data: Vec[T]): Vec[T] = {
-    val out_of_reset = RegNext(true.B, false.B)
+  def apply[T <: DataCanBeValid](data: Vec[T], stretchCycles: Int): Vec[T] = {
+    val out_of_reset = outOfReset(stretchCycles)
     val res = Wire(data.cloneType)
     res.zip(data)foreach { case (r, d) =>
       r := d
