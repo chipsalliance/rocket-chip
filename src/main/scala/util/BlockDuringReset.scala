3bccd8533a2fc33b11b4f69b0c9961670b84cf23
Henry Cook <henry@sifive.com>
2020-09-25 17:31:30 -0700

    util: type class implicit Blockable

--- src/main/scala/util/BlockDuringReset.scala
+++ src/main/scala/util/BlockDuringReset.scala
@@ -3,7 +3,7 @@
 package freechips.rocketchip.util
 
 import chisel3._
-import chisel3.util.{Counter, DecoupledIO, RegEnable}
+import chisel3.util.{Counter, RegEnable}
 
 /** Blocks transactions until the cycle after reset. */
 object BlockDuringReset
@@ -13,39 +13,7 @@ object BlockDuringReset
     case i => RegEnable(true.B, false.B, Counter(true.B, i)._2)
   }
 
-  def apply[T <: Data](enq: DecoupledIO[T], stretchCycles: Int = 0): DecoupledIO[T] = {
-    val out_of_reset = outOfReset(stretchCycles)
-    val res = Wire(enq.cloneType)
-    res.valid := enq.valid
-    enq.ready := res.ready
-    res.bits := enq.bits
-    when (!out_of_reset) {
-      res.valid := false.B
-      enq.ready := false.B
-    }
-    res
-  }
-
-  def apply(valid: Bool, stretchCycles: Int): Bool = {
-    val out_of_reset = outOfReset(stretchCycles)
-    valid && out_of_reset
-  }
-
-  def apply[T <: DataCanBeValid](data: T, stretchCycles: Int): T = {
-    val out_of_reset = outOfReset(stretchCycles)
-    val res = Wire(data.cloneType)
-    res := data
-    res.valid := data.valid && out_of_reset
-    res
-  }
-
-  def apply[T <: DataCanBeValid](data: Vec[T], stretchCycles: Int): Vec[T] = {
-    val out_of_reset = outOfReset(stretchCycles)
-    val res = Wire(data.cloneType)
-    res.zip(data)foreach { case (r, d) =>
-      r := d
-      r.valid := d.valid && out_of_reset
-    }
-    res
+  def apply[T <: Data : Blockable](data: T, stretchCycles: Int = 0): T = {
+    implicitly[Blockable[T]].blockWhile(!outOfReset(stretchCycles), data)
   }
 }
