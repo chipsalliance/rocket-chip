0671f5cd13c0db05c8b99657c6731b9868ff193e
Henry Cook <henry@sifive.com>
2020-10-15 12:50:54 -0700

    interrupts: implict SeqIntEdge helpers

--- src/main/scala/interrupts/package.scala
+++ src/main/scala/interrupts/package.scala
@@ -17,4 +17,23 @@ package object interrupts
     def cross(n: IntInwardNode) (implicit valName: ValName) = crossIn(n)
     def cross(n: IntOutwardNode)(implicit valName: ValName) = crossOut(n)
   }
+
+  implicit class SeqIntEdge(private val edges: Seq[IntEdge]) {
+    /** Scan over interrupt edges calculating a running total of the number of interrupts. */
+    def runningTotal: Seq[Int] = {
+      edges.map(_.num).scanLeft(0)(_+_).init
+    }
+
+    /** Take a sequence of interrupts from several edges and combine them
+      * into one flat sequence with their ranges stacked on top of each other.
+      */
+    def flattenSources(offset: Int): Seq[IntSourceParameters] = {
+      edges
+        .map(_.resolvedSources)
+        .zip(edges.runningTotal)
+        .map { case (sources, edgeOffset) =>
+          sources.map(z => z.copy(rangeOpt = z.rangeOpt.map(_.offset(edgeOffset+offset))))
+        }.flatten
+    }
+  }
 }
