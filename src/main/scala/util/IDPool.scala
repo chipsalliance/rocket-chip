951c06b30d3842d9a25362ed63074905b9fd357b
John Ingalls <43973001+ingallsj@users.noreply.github.com>
2020-10-21 18:34:38 -0700

    IDPool: synthesis timing enhancement argument options (#2673)
    * IDPool: simplify select1 PriorityEncoder
    
    * IDPool arguments for pre-calculating valid,select
    
    * Chisel width interference
    
    * IDPool argument revocableSelect to downgrade 'alloc' interface from Irrevocable to ReadyValid

--- src/main/scala/util/IDPool.scala
+++ src/main/scala/util/IDPool.scala
@@ -5,32 +5,33 @@ package freechips.rocketchip.util
 import chisel3._
 import chisel3.util._
 
-class IDPool(numIds: Int) extends Module {
+class IDPool(numIds: Int, lateValid: Boolean = false, revocableSelect: Boolean = false) extends Module {
   require (numIds > 0)
   val idWidth = log2Up(numIds)
 
   val io = IO(new Bundle {
     val free = Flipped(Valid(UInt(idWidth.W)))
-    val alloc = Irrevocable(UInt(idWidth.W))
+    val alloc = if (revocableSelect) Decoupled(UInt(idWidth.W)) else Irrevocable(UInt(idWidth.W))
   })
 
   // True indicates that the id is available
-  val bitmap = RegInit(-1.S(numIds.W).asUInt)
+  val bitmap = RegInit(-1.S(numIds.W).asUInt()(numIds-1, 0))
   val select = RegInit(0.U(idWidth.W))
   val valid  = RegInit(true.B)
 
-  io.alloc.valid := valid
-  io.alloc.bits  := select
+  io.alloc.valid := (if (lateValid)       bitmap.orR                           else valid)
+  io.alloc.bits  := (if (revocableSelect) PriorityEncoder(bitmap(numIds-1, 0)) else select)
 
-  val taken  = Mux(io.alloc.ready, (1.U << io.alloc.bits)(numIds-1, 0), 0.U)
-  val given  = Mux(io.free .valid, (1.U << io.free .bits)(numIds-1, 0), 0.U)
+  val taken  = Mux(io.alloc.ready, UIntToOH(io.alloc.bits, numIds), 0.U)
+  val given  = Mux(io.free .valid, UIntToOH(io.free .bits, numIds), 0.U)
   val bitmap1 = (bitmap & ~taken) | given
-  val select1 = OHToUInt(~(leftOR(bitmap1, numIds) << 1) & bitmap1, numIds)
-  val valid1  = bitmap1.orR
+  val select1 = PriorityEncoder(bitmap1(numIds-1, 0))
+  val valid1  = (  (bitmap.orR && !((PopCount(bitmap(numIds-1, 0)) === 1.U) && io.alloc.ready))  // bitmap not zero, and not allocating last bit
+                || io.free.valid)
 
   // Clock gate the bitmap
   when (io.alloc.ready || io.free.valid) {
-    bitmap := bitmap1
+    bitmap := bitmap1(numIds-1, 0)
     valid  := valid1
   }
 
@@ -41,4 +42,14 @@ class IDPool(numIds: Int) extends Module {
 
   // No double freeing
   assert (!io.free.valid || !(bitmap & ~taken)(io.free.bits))
+
+  // pre-calculations for timing
+  if (!lateValid) {
+    assert (valid === bitmap.orR)
+  }
+  if (!revocableSelect) {
+    when (io.alloc.valid && RegNext(io.alloc.ready || (!io.alloc.valid && io.free.valid))) {
+      assert (select === PriorityEncoder(bitmap(numIds-1, 0)))
+    }
+  }
 }
