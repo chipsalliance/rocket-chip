a275aa9e95002fc314789d40bcc7cf72f82f182a
Jiuyang Liu <liu@jiuyang.me>
2021-04-10 09:04:11 +0000

    deprecate GenericParameterizedBundle.

--- src/main/scala/util/GenericParameterizedBundle.scala
+++ src/main/scala/util/GenericParameterizedBundle.scala
@@ -4,18 +4,5 @@ package freechips.rocketchip.util
 
 import Chisel._
 
+@deprecated("GenericParameterizedBundle is useless anymore after autoclonetype2 is on.", "Rocket Chip 2021.04")
 abstract class GenericParameterizedBundle[+T <: Object](val params: T) extends Bundle
-{
-  override def cloneType = {
-    try {
-      this.getClass.getConstructors.head.newInstance(params).asInstanceOf[this.type]
-    } catch {
-      case e: java.lang.IllegalArgumentException =>
-        throw new Exception("Unable to use GenericParameterizedBundle.cloneType on " +
-                       this.getClass + ", probably because " + this.getClass +
-                       "() takes more than one argument.  Consider overriding " +
-                       "cloneType() on " + this.getClass, e)
-    }
-  }
-}
-
