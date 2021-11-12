a87ef0d6283c7ea618cc9d9cae054e04cbf24cfe
John Ingalls <john.ingalls@sifive.com>
2021-11-12 13:03:40 -0800

    do not take virtual instruction fault on write access to read-only CSR

--- src/main/scala/rocket/RocketCore.scala
+++ src/main/scala/rocket/RocketCore.scala
@@ -314,9 +314,9 @@ class Rocket(tile: RocketTile)(implicit p: Parameters) extends CoreModule()(p)
     id_ctrl.scie && !(id_scie_decoder.unpipelined || id_scie_decoder.pipelined) ||
     id_csr_en && (csr.io.decode(0).read_illegal || !id_csr_ren && csr.io.decode(0).write_illegal) ||
     !ibuf.io.inst(0).bits.rvc && (id_system_insn && csr.io.decode(0).system_illegal)
-  val id_virtual_insn = id_ctrl.legal && (
-    id_csr_en && csr.io.decode(0).virtual_access_illegal ||
-    !ibuf.io.inst(0).bits.rvc && (id_system_insn && csr.io.decode(0).virtual_system_illegal))
+  val id_virtual_insn = id_ctrl.legal &&
+    ((id_csr_en && !(!id_csr_ren && csr.io.decode(0).write_illegal) && csr.io.decode(0).virtual_access_illegal) ||
+     (!ibuf.io.inst(0).bits.rvc && id_system_insn && csr.io.decode(0).virtual_system_illegal))
   // stall decode for fences (now, for AMO.rl; later, for AMO.aq and FENCE)
   val id_amo_aq = id_inst(0)(26)
   val id_amo_rl = id_inst(0)(25)
