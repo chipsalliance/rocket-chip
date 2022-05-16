85a118306f7f56d8ee220a4599c103318f60685c
Jack Koenig <koenig@sifive.com>
2022-05-16 14:58:27 -0700

    Make Decode logic width-safe
    This involves ensuring all BitPats that correspond to a particular
    portion of the decoded result are of the same width. BitPats with no
    "don't cares" can be zero padded to the common width. BitPats with "don't
    cares" that are too small are an error.

--- src/main/scala/rocket/Decode.scala
+++ src/main/scala/rocket/Decode.scala
@@ -7,12 +7,46 @@ import chisel3.util.experimental.decode._
 
 object DecodeLogic
 {
+  // TODO This should be a method on BitPat
+  private def hasDontCare(bp: BitPat): Boolean = bp.mask.bitCount != bp.width
+  // Pads BitPats that are safe to pad (no don't cares), errors otherwise
+  private def padBP(bp: BitPat, width: Int): BitPat = {
+    if (bp.width == width) bp
+    else {
+      require(!hasDontCare(bp), s"Cannot pad '$bp' to '$width' bits because it has don't cares")
+      val diff = width - bp.width
+      require(diff > 0, s"Cannot pad '$bp' to '$width' because it is already '${bp.width}' bits wide!")
+      BitPat(0.U(diff.W)) ## bp
+    }
+  }
+
   def apply(addr: UInt, default: BitPat, mapping: Iterable[(BitPat, BitPat)]): UInt =
     chisel3.util.experimental.decode.decoder(QMCMinimizer, addr, TruthTable(mapping, default))
   def apply(addr: UInt, default: Seq[BitPat], mappingIn: Iterable[(BitPat, Seq[BitPat])]): Seq[UInt] = {
-    val elementIndexes = default.map(_.getWidth).scan(default.map(_.getWidth).sum - 1) { case (l, r) => l - r }
-    val decoded = apply(addr, default.reduce(_ ## _), mappingIn.map { case (in, out) => (in, out.reduce(_ ## _)) })
-    elementIndexes.zip(elementIndexes.drop(1)).map { case (msb, lsb) => decoded(msb, lsb + 1)}
+    val nElts = default.size
+    require(mappingIn.forall(_._2.size == nElts),
+      s"All Seq[BitPat] must be of the same length, got $nElts vs. ${mappingIn.find(_._2.size != nElts).get}"
+    )
+
+    val elementsGrouped = mappingIn.map(_._2).transpose
+    val elementWidths = elementsGrouped.zip(default).map { case (elts, default) =>
+      (default :: elts.toList).map(_.getWidth).max
+    }
+    val resultWidth = elementWidths.sum
+
+    val elementIndices = elementWidths.scan(resultWidth - 1) { case (l, r) => l - r }
+
+    // All BitPats that correspond to a given element in the result must have the same width in the
+    // chisel3 decoder. We will zero pad any BitPats that are too small so long as they dont have
+    // any don't cares. If there are don't cares, it is an error and the user needs to pad the
+    // BitPat themselves
+    val defaultsPadded = default.zip(elementWidths).map { case (bp, w) => padBP(bp, w) }
+    val mappingInPadded = mappingIn.map { case (in, elts) =>
+      in -> elts.zip(elementWidths).map { case (bp, w) => padBP(bp, w) }
+    }
+    val decoded = apply(addr, defaultsPadded.reduce(_ ## _), mappingInPadded.map { case (in, out) => (in, out.reduce(_ ## _)) })
+
+    elementIndices.zip(elementIndices.tail).map { case (msb, lsb) => decoded(msb, lsb + 1) }.toList
   }
   def apply(addr: UInt, default: Seq[BitPat], mappingIn: List[(UInt, Seq[BitPat])]): Seq[UInt] =
     apply(addr, default, mappingIn.map(m => (BitPat(m._1), m._2)).asInstanceOf[Iterable[(BitPat, Seq[BitPat])]])
