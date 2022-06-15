196c37282524c9e67bf6f8abb92234d1910c1080
Zenithal <i@zenithal.me>
2022-06-15 14:52:18 +0800

    BitManipCrypto: only instantiate clmul when not usingBitManipCrypto

--- src/main/scala/rocket/BitManipCrypto.scala
+++ src/main/scala/rocket/BitManipCrypto.scala
@@ -4,6 +4,7 @@ package freechips.rocketchip.rocket
 
 import chisel3._
 import chisel3.util._
+import freechips.rocketchip.config.Parameters
 
 object ZBK {
   val SZ_FN     = 5
@@ -22,7 +23,7 @@ class BitManipCryptoInterface(xLen: Int) extends Bundle {
   val rd  = Output(UInt(xLen.W))
 }
 
-class BitManipCrypto(xLen: Int) extends Module {
+class BitManipCrypto(xLen: Int)(implicit val p: Parameters) extends Module with HasRocketCoreParameters {
   val io = IO(new BitManipCryptoInterface(xLen))
 
   // note that it is reversed
@@ -39,16 +40,17 @@ class BitManipCrypto(xLen: Int) extends Module {
   val rs2_bytes = asBytes(io.rs2)
   val rs1_nibbles = asNibbles(io.rs1)
   val rs2_nibbles = asNibbles(io.rs2)
-  val xperm8 = VecInit(rs2_bytes.map(
+  // only instantiate clmul when usingBitManip && !usingBitManipCrypto
+  val xperm8 = if (usingBitManipCrypto) VecInit(rs2_bytes.map(
     x => Mux(x(7,log2Ceil(xLen/8)).orR, 0.U(8.W), rs1_bytes(x)) // return 0 when x overflow
-  ).toSeq).asUInt
-  val xperm4 = VecInit(rs2_nibbles.map(
+  ).toSeq).asUInt else 0.U
+  val xperm4 = if (usingBitManipCrypto) VecInit(rs2_nibbles.map(
     x => if (xLen == 32) Mux(x(3,log2Ceil(xLen/4)).orR, 0.U(4.W), rs1_nibbles(x)) // return 0 when x overflow
     else {
       require(xLen == 64)
       rs1_nibbles(x)
     }
-  ).toSeq).asUInt
+  ).toSeq).asUInt else 0.U
 
   // clmul
   val clmul_rs1 = Mux(isClmul, io.rs1, Reverse(io.rs1))
