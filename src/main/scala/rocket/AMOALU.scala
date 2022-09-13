279a87faa47d419c107c5877748dfec643da87b8
SingularityKChen <chency_singularity@163.com>
2022-09-13 19:14:34 +0800

    refactor AMOALU to chisel3. (#3040)

--- src/main/scala/rocket/AMOALU.scala
+++ src/main/scala/rocket/AMOALU.scala
@@ -3,19 +3,20 @@
 
 package freechips.rocketchip.rocket
 
-import Chisel._
+import chisel3._
+import chisel3.util._
 import freechips.rocketchip.config.Parameters
 
 class StoreGen(typ: UInt, addr: UInt, dat: UInt, maxSize: Int) {
   val size = typ(log2Up(log2Up(maxSize)+1)-1,0)
-  def misaligned =
-    (addr & ((UInt(1) << size) - UInt(1))(log2Up(maxSize)-1,0)).orR
+  def misaligned: Bool =
+    (addr & ((1.U << size) - 1.U)(log2Up(maxSize)-1,0)).orR
 
   def mask = {
-    var res = UInt(1)
+    var res = 1.U
     for (i <- 0 until log2Up(maxSize)) {
-      val upper = Mux(addr(i), res, UInt(0)) | Mux(size >= UInt(i+1), UInt((BigInt(1) << (1 << i))-1), UInt(0))
-      val lower = Mux(addr(i), UInt(0), res)
+      val upper = Mux(addr(i), res, 0.U) | Mux(size >= (i+1).U, ((BigInt(1) << (1 << i))-1).U, 0.U)
+      val lower = Mux(addr(i), 0.U, res)
       res = Cat(upper, lower)
     }
     res
@@ -23,7 +24,7 @@ class StoreGen(typ: UInt, addr: UInt, dat: UInt, maxSize: Int) {
 
   protected def genData(i: Int): UInt =
     if (i >= log2Up(maxSize)) dat
-    else Mux(size === UInt(i), Fill(1 << (log2Up(maxSize)-i), dat((8 << i)-1,0)), genData(i+1))
+    else Mux(size === i.U, Fill(1 << (log2Up(maxSize)-i), dat((8 << i)-1,0)), genData(i+1))
 
   def data = genData(0)
   def wordData = genData(2)
@@ -37,9 +38,9 @@ class LoadGen(typ: UInt, signed: Bool, addr: UInt, dat: UInt, zero: Bool, maxSiz
     for (i <- log2Up(maxSize)-1 to logMinSize by -1) {
       val pos = 8 << i
       val shifted = Mux(addr(i), res(2*pos-1,pos), res(pos-1,0))
-      val doZero = Bool(i == 0) && zero
-      val zeroed = Mux(doZero, UInt(0), shifted)
-      res = Cat(Mux(size === UInt(i) || doZero, Fill(8*maxSize-pos, signed && zeroed(pos-1)), res(8*maxSize-1,pos)), zeroed)
+      val doZero = (i == 0).B && zero
+      val zeroed = Mux(doZero, 0.U, shifted)
+      res = Cat(Mux(size === i.U || doZero, Fill(8*maxSize-pos, signed && zeroed(pos-1)), res(8*maxSize-1,pos)), zeroed)
     }
     res
   }
@@ -52,14 +53,14 @@ class AMOALU(operandBits: Int)(implicit p: Parameters) extends Module {
   val minXLen = 32
   val widths = (0 to log2Ceil(operandBits / minXLen)).map(minXLen << _)
 
-  val io = new Bundle {
-    val mask = UInt(INPUT, operandBits/8)
-    val cmd = Bits(INPUT, M_SZ)
-    val lhs = Bits(INPUT, operandBits)
-    val rhs = Bits(INPUT, operandBits)
-    val out = Bits(OUTPUT, operandBits)
-    val out_unmasked = Bits(OUTPUT, operandBits)
-  }
+  val io = IO(new Bundle {
+    val mask = Input(UInt((operandBits / 8).W))
+    val cmd = Input(UInt(M_SZ.W))
+    val lhs = Input(UInt(operandBits.W))
+    val rhs = Input(UInt(operandBits.W))
+    val out = Output(UInt(operandBits.W))
+    val out_unmasked = Output(UInt(operandBits.W))
+  })
 
   val max = io.cmd === M_XA_MAX || io.cmd === M_XA_MAXU
   val min = io.cmd === M_XA_MIN || io.cmd === M_XA_MINU
@@ -69,7 +70,7 @@ class AMOALU(operandBits: Int)(implicit p: Parameters) extends Module {
 
   val adder_out = {
     // partition the carry chain to support sub-xLen addition
-    val mask = ~(UInt(0, operandBits) +: widths.init.map(w => !io.mask(w/8-1) << (w-1))).reduce(_|_)
+    val mask = ~(0.U(operandBits.W) +: widths.init.map(w => !io.mask(w/8-1) << (w-1))).reduce(_|_)
     (io.lhs & mask) + (io.rhs & mask)
   }
 
