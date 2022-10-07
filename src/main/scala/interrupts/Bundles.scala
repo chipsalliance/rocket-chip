6ee7565f94c27f196bf63461e0b45e48e2d7461e
Liu Xiaoyi <circuitcoder0@gmail.com>
2022-10-07 17:30:16 +0800

    Some renaming in interrupts/* and util/SynchronizerReg for Chisel3 (#3124)
    * Some renaming in interrupts/* and util/SynchronizerReg for Chisel3

--- src/main/scala/interrupts/Bundles.scala
+++ src/main/scala/interrupts/Bundles.scala
@@ -2,7 +2,7 @@
 
 package freechips.rocketchip.interrupts
 
-import Chisel._
+import chisel3._
 import freechips.rocketchip.util._
 
 class SyncInterrupts(params: IntEdge) extends GenericParameterizedBundle(params)
