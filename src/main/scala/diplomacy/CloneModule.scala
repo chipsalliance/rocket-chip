70ed8b63bd3780b2c6091a9e9fbfd12ab9145200
SingularityKChen <chency_singularity@163.com>
2023-01-10 23:19:31 +0800

    Remove cloneModule

--- src/main/scala/diplomacy/CloneModule.scala
+++ src/main/scala/diplomacy/CloneModule.scala
@@ -7,7 +7,6 @@ package chisel3.shim
 
 import Chisel._
 import chisel3.experimental.BaseModule
-import chisel3.reflect.DataMirror
 import chisel3.{RawModule, Module}
 import chisel3.internal.Builder
 import chisel3.internal.firrtl.{Command, DefInstance}
@@ -19,40 +18,4 @@ class ClonePorts protected[shim](elts: Data*) extends Record
   val elements = ListMap(elts.map(d => d.instanceName -> d.chiselCloneType): _*)
   def apply(field: String) = elements(field)
   override def cloneType = (new ClonePorts(elts: _*)).asInstanceOf[this.type]
-}
-
-class CloneModule private (model: RawModule) extends BlackBox
-{
-  override def desiredName = model.name
-  val io = IO(new ClonePorts(DataMirror.modulePorts(model).map(_._2): _*))
-}
-
-object CloneModule
-{
-  def apply(model: BaseModule): ClonePorts = {
-    // Create the 'BlackBox' stand-in
-    val mod = Module(new CloneModule(model.asInstanceOf[RawModule]))
-    // Rewrite the instance definition to be the original module
-    // (this is needed because the original module gets clobbered by DCE + constant prop)
-    val method = classOf[RawModule].getDeclaredMethod("_commands")
-    method.setAccessible(true)
-    val commands = method.invoke(Builder.forcedUserModule).asInstanceOf[ArrayBuffer[Command]]
-    val victimIdx = commands.lastIndexWhere {
-      case DefInstance(_, kill, _) => mod eq kill
-      case _ => false
-    }
-    val victim = commands(victimIdx).asInstanceOf[DefInstance]
-    val standin = new DefInstance(victim.sourceInfo, model, victim.ports) {
-      override def name = victim.name
-    }
-    commands.update(victimIdx, standin)
-    // Wire it up
-    model match {
-      case _: Module =>
-        mod.io("clock") := Module.clock
-        mod.io("reset") := Module.reset
-      case _: RawModule => // Do nothing
-    }
-    mod.io
-  }
 }
