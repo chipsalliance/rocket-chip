4d7c582bbdd60fcc55953ea242997229bdc43129
singularity <chency_singularity@163.com>
2023-02-24 10:32:16 +0800

    fix: remove unittest/Generator.scala

--- src/main/scala/unittest/Generator.scala
+++
@@ -1,8 +0,0 @@
-// See LICENSE.SiFive for license details.
-
-package freechips.rocketchip.unittest
-
-import firrtl.options.StageMain
-import freechips.rocketchip.system.RocketChipStage
-
-object Generator extends StageMain(new RocketChipStage)
