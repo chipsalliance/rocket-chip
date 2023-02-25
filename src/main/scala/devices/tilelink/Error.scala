87ae9ed7773a85759e180ad58372ef7e2fabd712
singularity <chency_singularity@163.com>
2023-02-25 10:47:01 +0800

    feat: port Chisel2 to Chisel3 devices/
    (cherry picked from commit 1ff0db373de88bcb98dd0e1a1e8f0e1d44b2b35a)

--- src/main/scala/devices/tilelink/Error.scala
+++ src/main/scala/devices/tilelink/Error.scala
@@ -2,7 +2,8 @@
 
 package freechips.rocketchip.devices.tilelink
 
-import Chisel._
+import chisel3._
+import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
@@ -22,7 +23,7 @@ class TLError(params: DevNullParams, buffer: Boolean = true, beatBytes: Int = 4)
     val a = if (buffer) {Queue(in.a, 1)} else in.a
 
     val da = Wire(in.d)
-    val idle = RegInit(Bool(true))
+    val idle = RegInit(true.B)
 
     val a_last = edge.last(a)
     val (da_first, da_last, _) = edge.firstlast(da)
@@ -32,12 +33,12 @@ class TLError(params: DevNullParams, buffer: Boolean = true, beatBytes: Int = 4)
     da.valid := a.valid && a_last && idle
 
     da.bits.opcode  := TLMessages.adResponse(a.bits.opcode)
-    da.bits.param   := UInt(0) // toT, but error grants must be handled transiently (ie: you don't keep permissions)
+    da.bits.param   := 0.U // toT, but error grants must be handled transiently (ie: you don't keep permissions)
     da.bits.size    := a.bits.size
     da.bits.source  := a.bits.source
-    da.bits.sink    := UInt(0)
-    da.bits.denied  := Bool(true)
-    da.bits.data    := UInt(0)
+    da.bits.sink    := 0.U
+    da.bits.denied  := true.B
+    da.bits.data    := 0.U
     da.bits.corrupt := edge.hasData(da.bits)
 
     if (params.acquire) {
@@ -48,21 +49,21 @@ class TLError(params: DevNullParams, buffer: Boolean = true, beatBytes: Int = 4)
       val dc_last = edge.last(dc)
 
       // Only allow one Grant in-flight at a time
-      when (da.fire() && da.bits.opcode === Grant) { idle := Bool(false) }
-      when (in.e.fire()) { idle := Bool(true) }
+      when (da.fire && da.bits.opcode === Grant) { idle := false.B }
+      when (in.e.fire) { idle := true.B }
 
       c.ready := (dc.ready && dc_last) || !c_last
       dc.valid := c.valid && c_last
 
       // ReleaseAck is not allowed to report failure
       dc.bits.opcode  := ReleaseAck
-      dc.bits.param   := Vec(toB, toN, toN)(c.bits.param)
+      dc.bits.param   := VecInit(toB, toN, toN)(c.bits.param)
       dc.bits.size    := c.bits.size
       dc.bits.source  := c.bits.source
-      dc.bits.sink    := UInt(0)
-      dc.bits.denied  := Bool(false)
-      dc.bits.data    := UInt(0)
-      dc.bits.corrupt := Bool(false)
+      dc.bits.sink    := 0.U
+      dc.bits.denied  := false.B
+      dc.bits.data    := 0.U
+      dc.bits.corrupt := false.B
 
       // Combine response channels
       TLArbiter.lowest(edge, in.d, dc, da)
@@ -71,9 +72,9 @@ class TLError(params: DevNullParams, buffer: Boolean = true, beatBytes: Int = 4)
     }
 
     // We never probe or issue B requests
-    in.b.valid := Bool(false)
+    in.b.valid := false.B
 
     // Sink GrantAcks
-    in.e.ready := Bool(true)
+    in.e.ready := true.B
   }
 }
