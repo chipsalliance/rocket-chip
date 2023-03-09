c4bf4a948bc86f5fff12d0fa0e4bd1ddba3be514
Zenithal <i@zenithal.me>
2023-03-09 16:13:16 +0000

    [Do not merge] patches

---
+++ src/main/scala/package.scala
@@ -0,0 +1,12 @@
+ // See LICENSE.SiFive for license details.
+
+package freechips {
+  package object rocketchip {
+    val config = org.chipsalliance.cde.config
+  }
+}
+package chipsalliance {
+  package object rocketchip {
+    val config = org.chipsalliance.cde.config
+  }
+}
