fff691493e52770970994bc0081cafab8aa777f7
Jerry Zhao <jerryz123@berkeley.edu>
2023-05-10 00:47:08 -0700

    Update HeterogeneousBag to chisel3.6

--- src/main/scala/util/HeterogeneousBag.scala
+++ src/main/scala/util/HeterogeneousBag.scala
@@ -2,16 +2,17 @@
 
 package freechips.rocketchip.util
 
-import Chisel._
-import chisel3.Record
+import chisel3._
+import chisel3.experimental.DataMirror
 import scala.collection.immutable.ListMap
 
 final case class HeterogeneousBag[T <: Data](elts: Seq[T]) extends Record with collection.IndexedSeq[T] {
-  def apply(x: Int) = elts(x)
+  val elements = ListMap(elts.zipWithIndex.map { case (n,i) => (i.toString, DataMirror.internal.chiselTypeClone(n)) }:_*)
+
+  def apply(x: Int): T = elements.values.toSeq(x)
   def length = elts.length
 
   override def className: String = super.className
-  val elements = ListMap(elts.zipWithIndex.map { case (n,i) => (i.toString, n) }:_*)
   // IndexedSeq has its own hashCode/equals that we must not use
   override def hashCode: Int = super[Record].hashCode
   override def equals(that: Any): Boolean = super[Record].equals(that)
