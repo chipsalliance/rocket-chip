71eddde4e65479c32688239b8a1602c0d6a235e4
Jiuyang Liu <liu@jiuyang.me>
2023-09-18 02:30:26 +0800

    finish LSUTokenizer

--- src/main/scala/tile/LSUTokenizer.scala
+++ src/main/scala/tile/LSUTokenizer.scala
@@ -2,61 +2,58 @@ package freechips.rocketchip.tile
 
 import chisel3._
 import chisel3.experimental.SourceInfo
-import chisel3.util.BitPat
-import chisel3.util.experimental.decode.TruthTable
+import chisel3.util.{BitPat, Decoupled, DecoupledIO, Queue, RegEnable, Valid, ValidIO, log2Ceil}
+import chisel3.util.experimental.decode.{TruthTable, decoder}
 import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.util.TwoWayCounter
 import org.chipsalliance.cde.config.Parameters
 
-case class LSUTokenizerSourceParameter()
-case class LSUTokenizerSinkParameter(outstandingSize: Int,
-                                     giveTokenToMe: TruthTable) {
+case class LSUTokenizerDiplomaticParameter(nodeName: String, outstandingSize: Int, giveTokenToMe: TruthTable) {
   require(giveTokenToMe.default.value == 0, "doesn't allow default to 1.")
 }
-case class LSUTokenizerEdgeParameter(outstandingSize: Int,
-                                     giveTokenToMe: TruthTable)
 class LSUTokenizerBundle extends Bundle {
+
   /** Grant token from Tokenizer to LSU. */
   val grant: Bool = Bool()
+
   /** Release token from LSU to Tokenizer. */
   val release: Bool = Flipped(Bool())
 }
 
-object LSUTokenizerNodeImp extends SimpleNodeImp[
-  LSUTokenizerSourceParameter,
-  LSUTokenizerSinkParameter,
-  LSUTokenizerEdgeParameter,
-  LSUTokenizerBundle] {
+object LSUTokenizerNodeImp
+    extends SimpleNodeImp[
+      LSUTokenizerDiplomaticParameter,
+      LSUTokenizerDiplomaticParameter,
+      LSUTokenizerDiplomaticParameter,
+      LSUTokenizerBundle
+    ] {
 
-  override def edge(pd: LSUTokenizerSourceParameter,
-                    pu: LSUTokenizerSinkParameter,
-                    p: Parameters,
-                    sourceInfo: SourceInfo): LSUTokenizerEdgeParameter = LSUTokenizerEdgeParameter(pu.outstandingSize, pu.giveTokenToMe)
+  override def edge(
+    pd:         LSUTokenizerDiplomaticParameter,
+    pu:         LSUTokenizerDiplomaticParameter,
+    p:          Parameters,
+    sourceInfo: SourceInfo
+  ): LSUTokenizerDiplomaticParameter =
+    LSUTokenizerDiplomaticParameter(pu.nodeName, pu.outstandingSize, pu.giveTokenToMe)
 
-  override def bundle(e: LSUTokenizerEdgeParameter): LSUTokenizerBundle = new LSUTokenizerBundle
+  override def bundle(e: LSUTokenizerDiplomaticParameter): LSUTokenizerBundle = new LSUTokenizerBundle
 
-  override def render(e: LSUTokenizerEdgeParameter) = RenderedEdge(colour = "#00cc00")
+  override def render(e: LSUTokenizerDiplomaticParameter) = RenderedEdge(colour = "#00cc00")
 }
 
-class LSUTokenizerSourceNode(parameters: Seq[LSUTokenizerSourceParameter]) extends SourceNode(LSUTokenizerNodeImp)(parameters) {
-  require(parameters.size == 1)
+class LSUTokenizerSinkNode(parameters: Seq[LSUTokenizerDiplomaticParameter])
+    extends SinkNode(LSUTokenizerNodeImp)(parameters) {
+  require(parameters.size == 1, "not allow request more then one LSU from one node.")
 }
 
-class LSUTokenizerSinkNode(parameters: Seq[LSUTokenizerSinkParameter]) extends SinkNode(LSUTokenizerNodeImp)(parameters)
+class LSUTokenizerCoreBundleBridge extends Bundle {
+  val executeInstruction: Valid[UInt] = Valid(UInt(32.W))
+  val flush = Bool()
+  val memoryHazard: Bool = Flipped(Bool())
+}
+class LSUTokenizerSourceNode extends SourceNode(LSUTokenizerNodeImp)(Seq())
 
-class LSUTokenizerNexusNode extends NexusNode(LSUTokenizerNodeImp)(
-  dFn = _ => LSUTokenizerSourceParameter(),
-  uFn = fns => LSUTokenizerSinkParameter(
-    // add all outstanding together
-    fns.map(_.outstandingSize).sum,
-    fns.map(_.giveTokenToMe).reduce((l, r) => TruthTable(
-      // Merge all table together by extending rhs value to 0
-      l.table.map(t => (t._1, t._2 ## BitPat.N(r.table.head._2.width))) ++ r.table.map(t => (t._1, BitPat.N(l.table.head._2.width) ## t._2)),
-      // Set all default to N
-      BitPat.N(l.table.head._2.width + r.table.head._2.width)))
-  )
-)
-
-/** A Component at Tile level to consume issued instructions from core,
+/** A Component at Tile level to consume instruction at execute stage,
   * using token to maintain memory order among different LSUs,
   * including scalar, vector, RoCCs.
   * The detail uArch of this module is:
@@ -65,13 +62,67 @@ class LSUTokenizerNexusNode extends NexusNode(LSUTokenizerNodeImp)(
   * - it will grant token to specific node if the current lsu type is the type register, in the same time, counter for this node will increase 1;
   * - LSUs from vector, RoCC, D$ etc should capture this grant and increase the token counter inside them;
   * - the counter inside LSUs should stall the TileLink outward transaction when token is zero;
-  * - for each LSU they should release token to [[LSUTokenizer]] when 
+  * - for each LSU they should release token to [[LSUTokenizer]] when LSU finished a memory instruction.(got memory ack)
   */
 class LSUTokenizer()(implicit p: Parameters) extends LazyModule {
-  lazy val module: LazyModuleImpLike = ???
-
-  val lsuMasterNode = new LSUTokenizerSourceNode(Seq(LSUTokenizerSourceParameter()))
-
+  lazy val module: LSUTokenizerImp = new LSUTokenizerImp(this)
+  val coreBundle = BundleBridgeSink[LSUTokenizerCoreBundleBridge]
+  val sourceNode = new LSUTokenizerSourceNode()
 }
 
+class LSUTokenizerImp(outer: LSUTokenizer) extends LazyModuleImp(outer) {
+  val coreBundle = outer.coreBundle.in.headOption.map(_._1).get
 
+  /** for each lsu, it has its own counter to record how many token is already issued to which. */
+  val lsuCounters: Seq[UInt] = outer.sourceNode.in.map(_._2).map {
+    case LSUTokenizerDiplomaticParameter(nodeName: String, outstandingSize: Int, _) =>
+      RegInit(0.U(log2Ceil(outstandingSize).W)).suggestName(s"${nodeName}OutstandingCounter")
+  }
+
+  /** issue queue to store the current instruction, the element is the OH encode for LSU. */
+  val issueQueue = new Queue(UInt(outer.sourceNode.in.size.W), outer.sourceNode.in.map(_._2.outstandingSize).sum)
+
+  issueQueue.io.enq.valid := coreBundle.executeInstruction.valid
+  issueQueue.io.enq.bits := decoder(
+    coreBundle.executeInstruction.bits,
+    outer.sourceNode.in
+      .map(_._2.giveTokenToMe)
+      .reduce((l, r) =>
+        TruthTable(
+          // Merge all table together by extending rhs value to 0
+          l.table.map(t => (t._1, t._2 ## BitPat.N(r.table.head._2.width))) ++ r.table
+            .map(t => (t._1, BitPat.N(l.table.head._2.width) ## t._2)),
+          // Set all default to N
+          BitPat.N(l.table.head._2.width + r.table.head._2.width)
+        )
+      )
+  )
+  assert(issueQueue.io.enq.ready, "issueQueue should always ready.")
+
+  /** OH encode to show which lsu is occupied by LSU. */
+  val currentLSU: UInt = RegEnable(issueQueue.io.deq.bits, 0.U(outer.sourceNode.in.size.W), issueQueue.io.deq.fire)
+
+  val outstandingCounters: Seq[UInt] = outer.sourceNode.out.zip(issueQueue.io.deq.bits.asBools).map {
+    case ((dstBundle, dstParameter), src) =>
+      val grant: Bool = WireDefault(src && issueQueue.io.deq.fire).suggestName(s"${dstParameter.nodeName}Grant")
+      dstBundle.grant := grant
+
+      val release: Bool = WireDefault(dstBundle.release).suggestName(s"${dstParameter.nodeName}Release")
+
+      val outstandingCounter = RegInit(0.U(log2Ceil(dstParameter.outstandingSize).W)).suggestName(s"${dstParameter.nodeName}OutstandingCounter")
+      when(grant && !release) {
+        outstandingCounter := outstandingCounter + 1.U
+      }
+      when(release && !grant) {
+        outstandingCounter := outstandingCounter - 1.U
+      }
+      outstandingCounter
+  }
+
+  val allLSUFree: Bool = VecInit(outstandingCounters.map(_ === 0.U)).asUInt.andR
+
+  issueQueue.io.deq.ready := issueQueue.io.deq.bits === currentLSU && allLSUFree
+
+  coreBundle.memoryHazard := allLSUFree && !issueQueue.io.deq.valid
+
+}
