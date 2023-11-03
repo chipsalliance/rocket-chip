5a7bfc78a5a873d84c8748a2d72c9c57e62a083d
abejgonzalez <abe.j.gonza@gmail.com>
2023-11-03 15:56:12 -0700

    Match Queue.apply interface | Move into single Object

--- src/main/scala/util/DelayQueue.scala
+++ src/main/scala/util/DelayQueue.scala
@@ -6,7 +6,7 @@ import chisel3._
 import chisel3.util._
 
 object DelayQueue {
-  def apply[T <: Data](sink: DecoupledIO[T], source: DecoupledIO[T], timer: UInt, delay: UInt, depth: Int): Unit = {
+  private def apply[T <: Data](source: DecoupledIO[T], timer: UInt, delay: UInt, depth: Int): DecoupledIO[T] = {
     val q = Module(new Queue(new Bundle {
       val data = source.bits.cloneType
       val time = UInt(timer.getWidth.W)
@@ -23,24 +23,23 @@ object DelayQueue {
     q.io.enq.valid := source.fire
     source.ready := q.io.enq.ready
 
+    val sink = Wire(new DecoupledIO(source.bits.cloneType))
     sink.bits := q.io.deq.bits.data
     sink.valid := q.io.deq.valid && ((timer - q.io.deq.bits.time) >= delay_r)
     q.io.deq.ready := sink.fire
-  }
-}
 
-object FixedDelayQueue {
-  def apply[T <: Data](sink: DecoupledIO[T], source: DecoupledIO[T], delay: Int, maxLatency: Int = 4096): Unit = {
+    sink
+  }
+
+  def apply[T <: Data](source: DecoupledIO[T], delay: Int, maxLatency: Int = 4096): DecoupledIO[T] = {
     val timer = RegInit(0.U((log2Ceil(delay.max(1)) + log2Ceil(maxLatency)).W))
     timer := timer + 1.U
-    DelayQueue(sink, source, timer, delay.U, delay.max(1))
+    apply(sink, source, timer, delay.U, delay.max(1))
   }
-}
 
-object VariableDelayQueue {
-  def apply[T <: Data](sink: DecoupledIO[T], source: DecoupledIO[T], delay: UInt, depth: Int = 4096, maxLatency: Int = 4096): Unit = {
+  def apply[T <: Data](source: DecoupledIO[T], delay: UInt, depth: Int = 4096, maxLatency: Int = 4096): DecoupledIO[T] = {
     val timer = RegInit(0.U((1 + log2Ceil(maxLatency)).W))
     timer := timer + 1.U
-    DelayQueue(sink, source, timer, delay, depth)
+    apply(sink, source, timer, delay, depth)
   }
 }
